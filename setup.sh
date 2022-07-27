89c8f4cc5996c2c7a3ba3fe123178b8ac4052d0e
Yuto Tanaka <karaage_nennen@icloud.com>
2022-07-27 20:17:53 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
+#!/bin/sh
+
+# Create vim colors directory
 mkdir -p ~/.config/nvim/colors
+mkdir -p ~/.vim/colors/
+
+# Install vim-monokai
+wget https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim -O ~/.vim/colors/monokai.vim
 wget https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim -O ~/.config/nvim/colors/monokai.vim
 
-mkdir -p ~/.vim/colors/
-wget https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim -O ~/.vim/colors/monokai.vim
+# Install vim-molokai
+wget https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim -O ~/.vim/colors/molokai.vim
+wget https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim -O ~/.config/nvim/colors/molokai.vim
