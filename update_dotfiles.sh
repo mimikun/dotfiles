ac271fbcf81507a1e7a7dbd8e7175d1e2b2a3a09
Yuto Tanaka <karaage_nennen@icloud.com>
2022-09-05 21:57:28 +0900

    Update update_dotfiles.sh

--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -4,4 +4,5 @@ cp ~/asdf_plugin_list.txt ./asdf_plugin_list.txt
 cp ~/cargo_packages.txt ./cargo_packages.txt
 cp ~/.config/fish/completions/* ./dot_config/fish/completions/
 cp ~/.config/fish/fish_plugins ./dot_config/fish/fish_plugins
+chezmoi add ~/.tool-versions
 
