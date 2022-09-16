632c00e1707f837ded49a694b8e01b7aabd88af4
Yuto Tanaka <karaage_nennen@icloud.com>
2022-09-16 20:41:08 +0900

    Fix miniZ mining batfile

--- windows/utilities/miniz-flux-mining.bat
+++ windows/utilities/miniz-flux-mining.bat
@@ -2,9 +2,12 @@ REM Mining Mode IN!
 gsudo nvidia-smi -pl 150
 REM Mining start!!
 miniZ.exe ^
---templimit=75C ^
---url=t1gwCcY1KRASJjPexzmxhcRzy5D4gXjTFSy@sg.flux.herominers.com:1200 ^
---coolui
+--templimit=75 ^
+--nvidia ^
+-u t1gwCcY1KRASJjPexzmxhcRzy5D4gXjTFSy.worker ^
+--server sg.flux.herominers.com ^
+--port 1200 ^
+--pers="Zelproof"
 REM GAMING MODE IN!!!
 gsudo nvidia-smi -pl 250
 pause
