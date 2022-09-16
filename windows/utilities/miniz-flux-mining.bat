ddca17c267afe8435ab8bfd3d3c89e082d6d9768
Yuto Tanaka <karaage_nennen@icloud.com>
2022-09-16 20:17:36 +0900

    Add miniZ mining batfile

---
+++ windows/utilities/miniz-flux-mining.bat
@@ -0,0 +1,10 @@
+REM Mining Mode IN!
+gsudo nvidia-smi -pl 150
+REM Mining start!!
+miniZ.exe ^
+--templimit=75C ^
+--url=t1gwCcY1KRASJjPexzmxhcRzy5D4gXjTFSy@sg.flux.herominers.com:1200 ^
+--coolui
+REM GAMING MODE IN!!!
+gsudo nvidia-smi -pl 250
+pause
