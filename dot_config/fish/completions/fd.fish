23b912280e591bbc62c70e5e097105db51e90067
Yuto Tanaka <karaage_nennen@icloud.com>
2022-11-17 19:27:02 +0900

    Update fd fish completion

--- dot_config/fish/completions/fd.fish
+++ dot_config/fish/completions/fd.fish
@@ -21,8 +21,11 @@ complete -c fd -l path-separator -d 'Set path separator when printing file paths
 complete -c fd -l search-path -d 'Provides paths to search as an alternative to the positional <path> argument' -r -F
 complete -c fd -l gen-completions -r -f -a "{bash	,elvish	,fish	,powershell	,zsh	}"
 complete -c fd -s H -l hidden -d 'Search hidden files and directories'
+complete -c fd -l no-hidden -d 'Overrides --hidden'
 complete -c fd -s I -l no-ignore -d 'Do not respect .(git|fd)ignore files'
+complete -c fd -l ignore -d 'Overrides --no-ignore'
 complete -c fd -l no-ignore-vcs -d 'Do not respect .gitignore files'
+complete -c fd -l ignore-vcs -d 'Overrides --no-ignore-vcs'
 complete -c fd -l no-ignore-parent -d 'Do not respect .(git|fd)ignore files in parent directories'
 complete -c fd -l no-global-ignore-file -d 'Do not respect the global ignore file'
 complete -c fd -s u -l unrestricted -d 'Unrestricted search, alias for \'--no-ignore --hidden\''
@@ -32,8 +35,10 @@ complete -c fd -s g -l glob -d 'Glob-based search (default: regular expression)'
 complete -c fd -l regex -d 'Regular-expression based search (default)'
 complete -c fd -s F -l fixed-strings -d 'Treat pattern as literal string stead of regex'
 complete -c fd -s a -l absolute-path -d 'Show absolute instead of relative paths'
+complete -c fd -l relative-path -d 'Overrides --absolute-path'
 complete -c fd -s l -l list-details -d 'Use a long listing format with file metadata'
 complete -c fd -s L -l follow -d 'Follow symbolic links'
+complete -c fd -l no-follow -d 'Overrides --follow'
 complete -c fd -s p -l full-path -d 'Search full abs. path (default: filename only)'
 complete -c fd -s 0 -l print0 -d 'Separate search results by the null character'
 complete -c fd -l prune -d 'Do not traverse into directories that match the search criteria. If you want to exclude specific directories, use the \'--exclude=…\' option'
@@ -42,10 +47,5 @@ complete -c fd -s q -l quiet -d 'Print nothing, exit code 0 if match found, 1 ot
 complete -c fd -l show-errors -d 'Show filesystem errors'
 complete -c fd -l strip-cwd-prefix -d 'By default, relative paths are prefixed with \'./\' when -x/--exec, -X/--exec-batch, or -0/--print0 are given, to reduce the risk of a path starting with \'-\' being treated as a command line option. Use this flag to disable this behaviour'
 complete -c fd -l one-file-system
-complete -c fd -l no-hidden
-complete -c fd -l ignore
-complete -c fd -l ignore-vcs
-complete -c fd -l relative-path
-complete -c fd -l no-follow
 complete -c fd -s h -l help -d 'Print help information (use `--help` for more detail)'
 complete -c fd -s V -l version -d 'Print version information'
