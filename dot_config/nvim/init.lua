3d20b2b4d7ad427b6ba65827a07f71aa4e10ba63
Yuto Tanaka <karaage_nennen@icloud.com>
2023-03-28 18:00:39 +0900

    Neovim Lua config loading speedup

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,3 +1,7 @@
+-- ref: https://zenn.dev/kawarimidoll/articles/19bfc63e1c218c
+-- 読み込み高速化?
+if vim.loader then vim.loader.enable() end
+
 -- ファイルの種類毎に設定ファイルを記述できるようにする
 vim.cmd('filetype plugin indent off')
 
