2075b5e656be317249fa23e7640feaef3c385afb
Yuto Tanaka <karaage_nennen@icloud.com>
2023-04-10 19:27:49 +0900

    Add some comment

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -108,6 +108,8 @@ return require("packer").startup(function(use)
         "nvim-lualine/lualine.nvim",
         requires = { "kyazdani42/nvim-web-devicons", opt = true },
     })
+
+    -- other color theme
     use("tanvirtin/monokai.nvim")
     use("Allianaab2m/penumbra.nvim")
 end)
