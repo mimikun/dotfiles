f9efcfa17467b673506f716d618ad2093a1df922
Yuto Tanaka <karaage_nennen@icloud.com>
2023-06-06 19:38:40 +0900

    Add folke/which-key.nvim

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -86,6 +86,14 @@ return require("packer").startup(function(use)
         requires = { "nvim-lua/plenary.nvim" },
     })
     use("vim-skk/skkeleton")
+    use({
+        "folke/which-key.nvim",
+        config = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+            require("which-key").setup({})
+        end,
+    })
 
     ---- denops.vim
     use("vim-denops/denops.vim")
