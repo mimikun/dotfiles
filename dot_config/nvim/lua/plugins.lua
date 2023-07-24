7143e32d3eb87aa142cb2557be0aff7384e2e843
Yuto Tanaka <karaage_nennen@icloud.com>
2023-07-24 19:59:43 +0900

    Add Packer.nvim auto install script

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,3 +1,16 @@
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+        vim.cmd([[packadd packer.nvim]])
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 vim.cmd([[packadd packer.nvim]])
 
 return require("packer").startup(function(use)
@@ -108,4 +121,8 @@ return require("packer").startup(function(use)
     use("tanvirtin/monokai.nvim")
     use("Allianaab2m/penumbra.nvim")
     use("folke/tokyonight.nvim")
+
+    if packer_bootstrap then
+        require("packer").sync()
+    end
 end)
