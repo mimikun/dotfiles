a14a4591b5233f12ba9c81d32bfe42145bf3b2ac
Yuto Tanaka <karaage_nennen@icloud.com>
2023-08-23 19:09:54 +0900

    Fix stylua error

--- dot_config/nvim/lua/func.lua
+++ dot_config/nvim/lua/func.lua
@@ -1,12 +1,8 @@
 -- User functions
-vim.api.nvim_create_user_command(
-    "Vup",
-    function()
-        vim.cmd(":Lazy update")
-        vim.cmd(":Lazy restore")
-        vim.cmd(":Lazy sync")
-        vim.cmd(":TSUpdate")
-        vim.cmd(":CocUpdate")
-    end,
-    {}
-)
+vim.api.nvim_create_user_command("Vup", function()
+    vim.cmd(":Lazy update")
+    vim.cmd(":Lazy restore")
+    vim.cmd(":Lazy sync")
+    vim.cmd(":TSUpdate")
+    vim.cmd(":CocUpdate")
+end, {})
