11258ee64ae2fc3b1f0da71e6b45e594db0e394f
Yuto Tanaka <karaage_nennen@icloud.com>
2023-08-30 18:11:02 +0900

    Add nvim plugin: scrollbar.nvim

---
+++ dot_config/nvim/lua/plugins/scrollbar-nvim.lua
@@ -0,0 +1,27 @@
+return {
+    "Xuyuanp/scrollbar.nvim",
+    event = {
+        "WinScrolled",
+        "VimResized",
+        "QuitPre",
+        "WinEnter",
+        "FocusGained",
+    },
+    config = function()
+        -- TODO: refactor using lua
+        vim.cmd([[
+            augroup ScrollbarInit
+                autocmd!
+                autocmd WinScrolled,VimResized,QuitPre * silent! lua require('scrollbar').show()
+                autocmd WinEnter,FocusGained           * silent! lua require('scrollbar').show()
+                autocmd WinLeave,BufLeave,BufWinLeave,FocusLost            * silent! lua require('scrollbar').clear()
+            augroup end
+        ]])
+        vim.g.scrollbar_shape = {
+            head = "▲",
+            body = "█",
+            tail = "▼",
+        }
+    end,
+    cond = false,
+}
