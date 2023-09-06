0f963080fa435c1edff80a7c40ad9bb973e70633
Yuto Tanaka <karaage_nennen@icloud.com>
2023-09-06 18:59:52 +0900

    Update nvim plugin config: coc.nvim
    Fix #18

--- dot_config/nvim/lua/plugins/coc-nvim.lua
+++ dot_config/nvim/lua/plugins/coc-nvim.lua
@@ -30,49 +30,38 @@ local coc_extensions = {
 
 return {
     "neoclide/coc.nvim",
-    lazy = false,
     branch = "release",
+    --lazy = false,
+    event = "BufRead",
     build = ":CocUpdate",
     config = function()
-        -- <tab> と<Shift-tab> でナビゲートするやつ
-        vim.cmd([[
-                function! CheckBackspace() abort
-                    let col = col('.') - 1
-                    return !col || getline('.')[col - 1]  =~ '\s'
-                endfunction
-            ]])
-
-        -- Insert <tab> when previous text is space, refresh completion if not.
-        vim.cmd([[
-                inoremap <silent><expr> <TAB>
-                    \ coc#pum#visible() ? coc#pum#next(1):
-                    \ CheckBackspace() ? "\<Tab>" :
-                    \ coc#refresh()
-            ]])
-        vim.cmd([[inoremap <expr><S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]])
-
-        -- VSCodeライクなタブ補完
-        --vim.cmd([[
-        --[[
-            inoremap <silent><expr> <TAB>
-                \ coc#pum#visible() ? coc#_select_confirm() :
-                \ coc#expandableOrJumpable() ?
-                \ "\<C-r>=coc#rpc#request('doKeymap', ['snippets-expand-jump',''])\<CR>" :
-                \ CheckBackspace() ? "\<TAB>" :
-                \ coc#refresh()
-            ]]
-        --]])
-        --vim.cmd([[
-        --[[
-            function! CheckBackspace() abort
-                let col = col('.') - 1
-                return !col || getline('.')[col - 1]  =~# '\s'
-            endfunction
-            ]]
-        --]])
-        --vim.g.coc_snippet_next = '<tab>'
-
         vim.g.coc_global_extensions = coc_extensions
+
+        -- Autocomplete
+        function _G.check_back_space()
+            local col = vim.fn.col(".") - 1
+            return col == 0 or vim.fn.getline("."):sub(col, col):match("%s") ~= nil
+        end
+
+        -- vim.keymap.set のoptions
+        local opts = { silent = true, noremap = true, expr = true, replace_keycodes = false }
+
+        -- <TAB> と<S-TAB> で候補選択するやつ
+        vim.keymap.set(
+            "i",
+            "<TAB>",
+            'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()',
+            opts
+        )
+        vim.keymap.set("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
+
+        -- <CR> つまり Enter で候補決定するやつ
+        vim.keymap.set(
+            "i",
+            "<cr>",
+            [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]],
+            opts
+        )
     end,
     --cond = false,
 }
