864bf9cef5b31a4a0032235a66925dba49fb564d
Yuto Tanaka <karaage_nennen@icloud.com>
2023-09-08 23:20:12 +0900

    Add Invoke-FixEncode.ps1 (use for develop)

---
+++ OneDrive/Documents/PowerShell/Invoke-FixEncode.ps1
@@ -0,0 +1,3 @@
+﻿$pwsh_config_path = "Microsoft.PowerShell_profile.ps1.tmpl"
+$pwsh_config_file = Get-Content -Path $pwsh_config_path
+Set-Content -Path $pwsh_config_path -Value $pwsh_config_file -Encoding "utf8BOM"
