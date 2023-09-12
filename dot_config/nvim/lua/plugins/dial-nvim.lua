a3f8ace08a9db9497a5ea51180d20e6f00ca8eb3
Yuto Tanaka <karaage_nennen@icloud.com>
2023-09-12 18:42:55 +0900

    Update dial.nvim config
    Fix #43

--- dot_config/nvim/lua/plugins/dial-nvim.lua
+++ dot_config/nvim/lua/plugins/dial-nvim.lua
@@ -1,10 +1,20 @@
+local keymaps = {
+    { "<C-a>", desc = "<C-a> increment" },
+    { "<C-x>", desc = "<C-x> decrement" },
+}
+
 return {
     "monaqa/dial.nvim",
     --lazy = false,
     event = "VimEnter",
+    keys = keymaps,
     config = function()
-        -- TODO: Write config
-        -- https://github.com/monaqa/dial.nvim
+        vim.keymap.set("n", "<C-a>", function()
+            require("dial.map").manipulate("increment", "normal")
+        end)
+        vim.keymap.set("n", "<C-x>", function()
+            require("dial.map").manipulate("decrement", "normal")
+        end)
     end,
-    cond = false,
+    --cond = false,
 }
