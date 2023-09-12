09f8431db4d9adee0b79399fba44275016fd9cd9
Yuto Tanaka <karaage_nennen@icloud.com>
2023-09-12 18:43:17 +0900

    Update gitsigns.nvim config
    Fix #45

--- dot_config/nvim/lua/plugins/gitsigns-nvim.lua
+++ dot_config/nvim/lua/plugins/gitsigns-nvim.lua
@@ -2,25 +2,24 @@ return {
     "lewis6991/gitsigns.nvim",
     --lazy = false,
     event = "WinEnter",
+    cmd = "Gitsigns",
     config = function()
-        require("gitsigns").setup({
+        local gitsigns = require("gitsigns")
+        gitsigns.setup({
             signcolumn = false,
-            -- TODO: gitgutter再現
             -- g]で前の変更箇所へ移動する
-            --vim.keymap.set("n", "g[", ":GitGutterPrevHunk<CR>")
-
-            -- g[で次の変更箇所へ移動する
-            --vim.keymap.set("n", "g]", ":GitGutterNextHunk<CR>")
-
+            vim.keymap.set("n", "g[", ":Gitsigns prev_hunk<CR>"),
+            -- g[で次の変更箇所へ移動する,
+            vim.keymap.set("n", "g]", ":Gitsigns next_hunk<CR>"),
             -- ghでdiffをハイライトする
-            --vim.keymap.set("n", "gh", ":GitGutterLineHighlightsToggle<CR>")
-
+            vim.keymap.set("n", "gh", ":Gitsigns toggle_linehl<CR>"),
             -- gpでカーソル行のdiffを表示する
-            --vim.keymap.set("n", "gp", ":GitGutterPreviewHunk<CR>")
+            vim.keymap.set("n", "gp", ":Gitsigns preview_hunk_inline<CR>"),
+
             -- 記号の色を変更する
-            --vim.cmd([[highlight GitGutterAdd ctermfg=green]]
-            --vim.cmd([[highlight GitGutterChange ctermfg=blue]]
-            --vim.cmd([[highlight GitGutterDelete ctermfg=red]]
+            --vim.cmd.highlight("GitSignsAdd ctermfg=green"),
+            --vim.cmd.highlight("GitSignsChange ctermfg=blue"),
+            --vim.cmd.highlight("GitSignsDelete ctermfg=red"),
         })
     end,
     --cond = false,
