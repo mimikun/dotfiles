2b0cbc008e5b22cc1cdd869c078a8f849ecd82c2
Yuto Tanaka <karaage_nennen@icloud.com>
2023-09-12 18:43:35 +0900

    Update neo-tree.nvim config
    Fix #37

--- dot_config/nvim/lua/plugins/neo-tree-nvim.lua
+++ dot_config/nvim/lua/plugins/neo-tree-nvim.lua
@@ -1,15 +1,19 @@
 return {
-    -- TODO: setting
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
-    lazy = false,
-    --event = "VimEnter",
+    --lazy = false,
+    --event = "VeryLazy",
     cmd = "Neotree",
+    keys = {
+        { "<C-n>", ":Neotree . reveal toggle<CR>", desc = "Toggle Neotree" },
+    },
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons",
         "MunifTanjim/nui.nvim",
     },
-    config = function() end,
-    cond = false,
+    config = function()
+        vim.keymap.set("n", "<C-n>", ":Neotree . reveal toggle<CR>")
+    end,
+    --cond = false,
 }
