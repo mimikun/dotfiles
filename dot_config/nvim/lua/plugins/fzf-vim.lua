37d9cfa75b68fcdfbb7c2c4c2b39be2e1c4c1ccd
Yuto Tanaka <karaage_nennen@icloud.com>
2023-09-14 19:27:21 +0900

    Update fzf.vim config

--- dot_config/nvim/lua/plugins/fzf-vim.lua
+++ dot_config/nvim/lua/plugins/fzf-vim.lua
@@ -3,33 +3,36 @@ return {
     --lazy = false,
     --event = "VeryLazy",
     keys = {
-        { "<C-p>", ":call FzfOmniFiles()<CR>", desc = "Open file search" },
-        { "<C-g>", ":Rg<CR>", desc = "Open string search" },
+        { "<C-p>", desc = "Open file search" },
+        { "<C-g>", desc = "Open string search" },
     },
     dependencies = {
         "junegunn/fzf",
     },
     config = function()
-        -- TODO: Luaで書く
-        -- https://github.com/junegunn/fzf.vim/blob/master/README.md
+        -- うまく動かない
+        local is_git = function()
+            if vim.fn.system("git status"):find("fatal") == 1 then
+                return false
+            else
+                return true
+            end
+        end
 
         -- Ctrl+pでファイル検索を開く
         -- git管理されていれば:GFiles、そうでなければ:Filesを実行する
-        vim.cmd([[
-                fun! FzfOmniFiles()
-                    let is_git = system('git status')
-                    if v:shell_error
-                        :Files
-                    else
-                        :GFiles
-                    endif
-                endfun
-            ]])
-
-        vim.keymap.set("n", "<C-p>", ":call FzfOmniFiles()<CR>")
+        vim.keymap.set("n", "<C-p>", function()
+            if is_git() then
+                vim.cmd(":Files")
+            else
+                vim.cmd(":GFiles")
+            end
+        end, {})
 
         -- Ctrl+gで文字列検索を開く
         -- <S-?>でプレビューを表示/非表示する
+        -- TODO: Luaで書く
+        -- https://github.com/junegunn/fzf.vim/blob/master/README.md
         vim.cmd([[
                 command! -bang -nargs=* Rg
                 \ call fzf#vim#grep(
@@ -37,32 +40,50 @@ return {
                 \ <bang>0 ? fzf#vim#with_preview({'options': '--delimiter : --nth 3..'}, 'up:60%')
                 \ : fzf#vim#with_preview({'options': '--exact --delimiter : --nth 3..'}, 'right:50%:hidden', '?'),
                 \ <bang>0)
-            ]])
-        vim.keymap.set("n", "<C-g>", ":Rg<CR>")
+        ]])
+        vim.keymap.set("n", "<C-g>", function()
+            vim.cmd(":Rg<CR>")
+        end)
 
         -- frでカーソル位置の単語をファイル検索する
-        vim.keymap.set("n", "fr", 'vawy:Rg <C-R>"<CR>')
+        vim.keymap.set("n", "fr", function()
+            vim.cmd('vawy:Rg <C-R>"<CR>')
+        end)
 
         -- frで選択した単語をファイル検索する
-        vim.keymap.set("x", "fr y", ':Rg <C-R>"<CR>')
+        vim.keymap.set("x", "fr y", function()
+            vim.cmd(':Rg <C-R>"<CR>')
+        end)
 
         -- fbでバッファ検索を開く
-        vim.keymap.set("n", "fb", ":Buffers<CR>")
+        vim.keymap.set("n", "fb", function()
+            vim.cmd(":Buffers<CR>")
+        end)
 
         -- fpでバッファの中で1つ前に開いたファイルを開く
-        vim.keymap.set("n", "fp", ":Buffers<CR><CR>")
+        vim.keymap.set("n", "fp", function()
+            vim.cmd(":Buffers<CR><CR>")
+        end)
 
         -- flで開いているファイルの文字列検索を開く
-        vim.keymap.set("n", "fl", ":BLines<CR>")
+        vim.keymap.set("n", "fl", function()
+            vim.cmd(":BLines<CR>")
+        end)
 
         -- fmでマーク検索を開く
-        vim.keymap.set("n", "fm", ":Marks<CR>")
+        vim.keymap.set("n", "fm", function()
+            vim.cmd(":Marks<CR>")
+        end)
 
         -- fhでファイル閲覧履歴検索を開く
-        vim.keymap.set("n", "fh", ":History<CR>")
+        vim.keymap.set("n", "fh", function()
+            vim.cmd(":History<CR>")
+        end)
 
         -- fcでコミット履歴検索を開く
-        vim.keymap.set("n", "fc", ":Commits<CR>")
+        vim.keymap.set("n", "fc", function()
+            vim.cmd(":Commits<CR>")
+        end)
     end,
     --cond = false,
 }
