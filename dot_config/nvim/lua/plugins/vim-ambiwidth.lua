9a5050e33070101c2b980a128b6a8c96674f7b55
Yuto Tanaka <karaage_nennen@icloud.com>
2023-09-17 15:08:06 +0900

    Add new neovim plugin: vim-ambiwidth
    - https://github.com/rbtnn/vim-ambiwidth

---
+++ dot_config/nvim/lua/plugins/vim-ambiwidth.lua
@@ -0,0 +1,10 @@
+return {
+    "rbtnn/vim-ambiwidth",
+    lazy = false,
+    --event = "VeryLazy",
+    config = function()
+        --vim.cmd("let g:ambiwidth_cica_enabled = v:false")
+        --vim.cmd("let g:ambiwidth_add_list = [[0xfe566, 0xfe568, 2], [0xff500, 0xffd46, 2]]")
+    end,
+    --cond = false,
+}
