7c167e26f5453751ebcbf5963a490160ca7d570d
Yuto Tanaka <karaage_nennen@icloud.com>
2023-10-08 00:07:46 +0900

    Fix neovim plugin config: vimdoc-ja

--- dot_config/nvim/lua/plugins/vimdoc-ja.lua
+++ dot_config/nvim/lua/plugins/vimdoc-ja.lua
@@ -1,5 +1,8 @@
 return {
-    "vim-jp/vimdoc-ja",
+    -- NOTE: `doc/tags-ja` has a bug
+    --"vim-jp/vimdoc-ja",
+    "mimikun/vimdoc-ja",
+    branch = "ignore/doc_tags-ja",
     --lazy = false,
     --event = "VeryLazy",
     keys = {
