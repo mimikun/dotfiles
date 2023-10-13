2a8c15c1673a2240e661f3e6706c2d40502e23f7
Yuto Tanaka <karaage_nennen@icloud.com>
2023-10-13 22:30:38 +0900

    Fix lualine bug

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -1,3 +1,100 @@
+-- thx: https://raw.githubusercontent.com/numToStr/dotfiles/master/neovim/.config/nvim/lua/numToStr/plugins/lualine.lua
+
+local options = {
+    theme = "auto",
+    component_separators = "",
+    section_separators = "",
+    icons_enabled = true,
+    globalstatus = true,
+    refresh = {
+        statusline = 1000,
+        tabline = 1000,
+        winbar = 1000,
+    },
+}
+
+local sections = {
+    lualine_a = {
+        {
+            "mode",
+            color = {
+                gui = "bold",
+            },
+        },
+    },
+    lualine_b = {
+        {
+            "branch",
+        },
+        {
+            "diff",
+            colored = false,
+        },
+    },
+    lualine_c = {
+        {
+            -- NOTE: filename component has bug
+            -- https://github.com/nvim-lualine/lualine.nvim/issues/1097
+            -- NOTE: filename component document
+            -- https://github.com/nvim-lualine/lualine.nvim?tab=readme-ov-file#filename-component-options
+            "filename",
+            file_status = true,
+            path = function()
+                local file = vim.fn.expand("%:p")
+                if vim.fn.filereadable(file) == 1 then
+                    return 0
+                else
+                    return 0
+                end
+            end,
+        },
+        {
+            "diagnostics",
+        },
+    },
+    lualine_x = {
+        "filetype",
+        "encoding",
+        "fileformat",
+    },
+    lualine_y = {
+        "progress",
+    },
+    lualine_z = {
+        {
+            "location",
+            color = {
+                gui = "bold",
+            },
+        },
+    },
+}
+
+local tabline = {
+    lualine_a = {
+        {
+            "buffers",
+            buffers_color = {
+                active = "lualine_b_normal",
+            },
+        },
+    },
+    lualine_z = {
+        {
+            "tabs",
+            tabs_color = {
+                active = "lualine_b_normal",
+            },
+        },
+    },
+}
+
+local opts = {
+    options = options,
+    sections = sections,
+    tabline = tabline,
+}
+
 return {
     "nvim-lualine/lualine.nvim",
     --lazy = false,
@@ -9,58 +106,7 @@ return {
         { "nvim-tree/nvim-web-devicons", opt = true },
     },
     config = function()
-        --ref:
-        --https://raw.githubusercontent.com/numToStr/dotfiles/master/neovim/.config/nvim/lua/numToStr/plugins/lualine.lua
-        require("lualine").setup({
-            options = {
-                theme = "auto",
-                component_separators = "",
-                section_separators = "",
-                icons_enabled = true,
-                globalstatus = true,
-                refresh = {
-                    statusline = 1000,
-                    tabline = 1000,
-                    winbar = 1000,
-                },
-            },
-            sections = {
-                lualine_a = {
-                    { "mode", color = { gui = "bold" } },
-                },
-                lualine_b = {
-                    { "branch" },
-                    { "diff", colored = false },
-                },
-                lualine_c = {
-                    { "filename", file_status = true },
-                    { "diagnostics" },
-                },
-                lualine_x = {
-                    "filetype",
-                    "encoding",
-                    "fileformat",
-                },
-                lualine_y = { "progress" },
-                lualine_z = {
-                    { "location", color = { gui = "bold" } },
-                },
-            },
-            tabline = {
-                lualine_a = {
-                    {
-                        "buffers",
-                        buffers_color = { active = "lualine_b_normal" },
-                    },
-                },
-                lualine_z = {
-                    {
-                        "tabs",
-                        tabs_color = { active = "lualine_b_normal" },
-                    },
-                },
-            },
-        })
+        require("lualine").setup(opts)
     end,
     --cond = false,
 }
