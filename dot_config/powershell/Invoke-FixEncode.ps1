ddb9de0cd6ddaaabc117c10c469e9fb87185bb1a
Yuto Tanaka <karaage_nennen@icloud.com>
2023-10-27 22:33:39 +0900

    Improved Linux PowerShell config
    Fix #52

--- dot_config/powershell/Invoke-FixEncode.ps1
+++ dot_config/powershell/Invoke-FixEncode.ps1
@@ -1,3 +1,3 @@
-﻿$pwsh_config_path = "Microsoft.PowerShell_profile.ps1"
+﻿$pwsh_config_path = "Microsoft.PowerShell_profile.ps1.tmpl"
 $pwsh_config_file = Get-Content -Path $pwsh_config_path
 Set-Content -Path $pwsh_config_path -Value $pwsh_config_file -Encoding "utf8BOM"
