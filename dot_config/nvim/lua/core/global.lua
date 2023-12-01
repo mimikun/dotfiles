ec93ceaaa6bb7a799a7a6cd3184c3071bbebc9ba
Yuto Tanaka <karaage_nennen@icloud.com>
2023-12-01 21:23:25 +0900

    Improved human rights judgment process in neovim

--- dot_config/nvim/lua/core/global.lua
+++ dot_config/nvim/lua/core/global.lua
@@ -3,6 +3,14 @@
 local global = {}
 local os_name = vim.loop.os_uname().sysname
 local hostname = vim.loop.os_gethostname()
+local total_memory = vim.loop.get_total_memory()
+-- 7GB
+local human_rights_memory_size = 7516192768
+
+function global:is_human_rights()
+    -- total_memory > 7GB
+    return total_memory > human_rights_memory_size
+end
 
 function global:load_variables()
     self.is_mac = os_name == "Darwin"
@@ -10,10 +18,7 @@ function global:load_variables()
     self.is_windows = os_name == "Windows_NT"
     self.is_wsl = vim.fn.has("wsl") == 1
     self.is_unix = vim.fn.has("unix") == 1
-    -- TODO: need improve
-    -- e.g. Ryzen9 3900X, RAM 32GB, 1TB SSD
-    self.is_human_rights = hostname == "YutoWindows"
-    self.is_not_human_rights = hostname == "TanakaPC"
+    self.is_human_rights = global:is_human_rights()
     self.vim_path = vim.fn.stdpath("config")
     local path_sep = self.is_windows and "\\" or "/"
     local home = self.is_windows and os.getenv("USERPROFILE") or os.getenv("HOME")
