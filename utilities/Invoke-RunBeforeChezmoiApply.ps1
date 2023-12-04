529ff8a72d58bbc8cc3e6767e7eb0e84e5341b99
Yuto Tanaka <karaage_nennen@icloud.com>
2023-12-04 20:40:34 +0900

    Improved PowerShell config

--- utilities/Invoke-RunBeforeChezmoiApply.ps1
+++ utilities/Invoke-RunBeforeChezmoiApply.ps1
@@ -3,6 +3,11 @@
 function Invoke-RunBeforeChezmoiApply() {
     Write-Output "pre-apply-hook"
     # TODO: set $env:BW_SESSION
+
+    # Copy config file from Linux
+    $home_pwsh_profile = Join-Path $env:USERPROFILE -ChildPath "OneDrive\Documents\PowerShell\Microsoft.PowerShell_profile.ps1.tmpl"
+    $base_profile = Join-Path $env:CHEZMOI_DIR -ChildPath "dot_config/powershell/Microsoft.PowerShell_profile.ps1.tmpl"
+    Copy-Item -Path $base_profile -Destination $home_powershell_profile
 }
 
 Invoke-RunBeforeChezmoiApply
