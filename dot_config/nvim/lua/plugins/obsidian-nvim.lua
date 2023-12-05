8d7a5d09c8f2c6e36026c4be7bf39d2a03c058f9
Yuto Tanaka <karaage_nennen@icloud.com>
2023-12-05 19:48:09 +0900

    Update neovim config: obsidian.nvim

--- dot_config/nvim/lua/plugins/obsidian-nvim.lua
+++ dot_config/nvim/lua/plugins/obsidian-nvim.lua
@@ -35,6 +35,19 @@ local day_sec = 86400
 local month_sec = 2592000
 local year_sec = 31536000
 
+---@return table
+local today_helper = function ()
+    local tmp = os.date("*t", os.time())
+    local tbl = {
+        year = tmp.year,
+        month = tmp.month,
+        day = tmp.day,
+        z_month = string.format("%02d", tmp.month),
+        z_day = string.format("%02d", tmp.day),
+    }
+    return tbl
+end
+
 local opts = {
     workspaces = vaults,
     detect_cwd = false,
@@ -44,67 +57,140 @@ local opts = {
     -- Default save location for newly daily notes
     daily_notes = {
         folder = "001_DailyNotes",
-        date_format = "%Y年%m月%d日",
         template = "999_Templates/dailyNoteTemplateFromObsidianNvim.md",
     },
     disable_frontmatter = true,
     -- Obsidian templates folder
     templates = {
         subdir = "999_Templates",
-        date_format = "%Y年%m月%d日",
         substitutions = {
             -- 999_Templates/dailyNoteTemplate.md
             -- 今日
             today = function()
-                local tmp = os.date("*t", os.time())
-                local year = tmp.year
-                local month = tmp.month
-                local day = tmp.day
+                local today = today_helper()
+                local year = today.year
+                local month = today.z_month
+                local day = today.z_day
                 return (year .. "年" .. month .. "月" .. day .. "日")
+            end,
+            -- YYYY/MM/DD
+            today_fm_1 = function ()
+                local today = today_helper()
+                local year = today.year
+                local month = today.z_month
+                local day = today.z_day
+                return (year .. "/" .. month .. "/" .. day)
+            end,
+            -- YYYY/M/D
+            today_fm_2 = function ()
+                local today = today_helper()
+                local year = today.year
+                local month = today.month
+                local day = today.day
+                return (year .. "/" .. month .. "/" .. day)
+            end,
+            -- YYYY年M月D日
+            today_fm_3 = function ()
+                local today = today_helper()
+                local year = today.year
+                local month = today.month
+                local day = today.day
+                return (year .. "年" .. month .. "月" .. day .. "日")
+            end,
+            -- YYYY-MM-DD
+            today_fm_4 = function ()
+                local today = today_helper()
+                local year = today.year
+                local month = today.z_month
+                local day = today.z_day
+                return (year .. "-" .. month .. "-" .. day)
+            end,
+            -- YYYY-M-D
+            today_fm_5 = function ()
+                local today = today_helper()
+                local year = today.year
+                local month = today.month
+                local day = today.day
+                return (year .. "-" .. month .. "-" .. day)
             end,
             -- 翌日
             tomorrow = function()
                 local tmp = os.date("*t", os.time() + day_sec)
                 local year = tmp.year
-                local month = tmp.month
-                local day = tmp.day
+                local month = string.format("%02d", tmp.month)
+                local day = string.format("%02d", tmp.day)
                 return (year .. "年" .. month .. "月" .. day .. "日")
             end,
             -- 前日
             yesterday = function()
                 local tmp = os.date("*t", os.time() - day_sec)
                 local year = tmp.year
-                local month = tmp.month
-                local day = tmp.day
+                local month = string.format("%02d", tmp.month)
+                local day = string.format("%02d", tmp.day)
                 return (year .. "年" .. month .. "月" .. day .. "日")
             end,
             -- 999_Templates/monthlyNoteTemplate.md
             -- 今月
             current_month = function()
-                local tmp = os.date("*t", os.time())
-                local year = tmp.year
-                local month = tmp.month
+                local today = today_helper()
+                local year = today.year
+                local month = today.z_month
+                return (year .. "年" .. month .. "月")
+            end,
+            -- YYYY/MM
+            month_fm_1 = function()
+                local today = today_helper()
+                local year = today.year
+                local month = today.z_month
+                return (year .. "/" .. month)
+            end,
+            -- YYYY/M
+            month_fm_2 = function()
+                local today = today_helper()
+                local year = today.year
+                local month = today.month
+                return (year .. "/" .. month)
+            end,
+            -- YYYY-MM
+            month_fm_3 = function()
+                local today = today_helper()
+                local year = today.year
+                local month = today.z_month
+                return (year .. "-" .. month)
+            end,
+            -- YYYY-M
+            month_fm_4 = function()
+                local today = today_helper()
+                local year = today.year
+                local month = today.month
+                return (year .. "-" .. month)
+            end,
+            -- YYYY年M月
+            month_fm_5 = function()
+                local today = today_helper()
+                local year = today.year
+                local month = today.month
                 return (year .. "年" .. month .. "月")
             end,
             -- 翌月
             next_month = function()
                 local tmp = os.date("*t", os.time() + month_sec + day_sec)
                 local year = tmp.year
-                local month = tmp.month
+                local month = string.format("%02d", tmp.month)
                 return (year .. "年" .. month .. "月")
             end,
             -- 前月
             last_month = function()
                 local tmp = os.date("*t", os.time() - month_sec)
                 local year = tmp.year
-                local month = tmp.month
+                local month = string.format("%02d", tmp.month)
                 return (year .. "年" .. month .. "月")
             end,
             -- 999_Templates/yearlyNoteTemplate.md
             -- 今年
             current_year = function()
-                local tmp = os.date("*t", os.time())
-                local year = tmp.year
+                local today = today_helper()
+                local year = today.year
                 return (year .. "年")
             end,
             -- 翌年
