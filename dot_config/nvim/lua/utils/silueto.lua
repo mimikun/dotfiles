98b1d2f5afb16e498dbeb48636332a069a88a556
Yuto Tanaka <karaage_nennen@icloud.com>
2023-12-05 19:48:43 +0900

    Add neovim utils: silueto.lua

---
+++ dot_config/nvim/lua/utils/silueto.lua
@@ -0,0 +1,26 @@
+local M = {}
+
+local global = require("core.global")
+local path_sep = global.path_sep
+
+local file_name = vim.fn.expand("$obsidian_vault_path") .. path_sep .. "tasks.md"
+local task_file = io.open(file_name, "r")
+if not task_file then
+    return error("Unable to open" .. file_name)
+end
+for line in task_file:lines("L") do
+    -- 定義ファイルにおいてコメントアウト箇所は不要箇所を示すので削除
+    if not string.match(line, "^//") then
+        print(line)
+    end
+end
+task_file:close()
+
+return M
+
+--[[
+TODO: 
+
+繰り返しタスク
+
+]]
