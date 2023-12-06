af80233f48e05a16d9a7e25083b9c4f8896896ac
Yuto Tanaka <karaage_nennen@icloud.com>
2023-12-06 22:12:55 +0900

    Update neovim config: obsidian.nvim

--- dot_config/nvim/lua/plugins/obsidian-nvim.lua
+++ dot_config/nvim/lua/plugins/obsidian-nvim.lua
@@ -36,7 +36,7 @@ local month_sec = 2592000
 local year_sec = 31536000
 
 ---@return table
-local today_helper = function ()
+local today_helper = function()
     local tmp = os.date("*t", os.time())
     local tbl = {
         year = tmp.year,
@@ -54,18 +54,14 @@ local opts = {
     -- Default save location for newly created notes
     notes_subdir = "000_Inbox",
     log_level = vim.log.levels.DEBUG,
-    -- Default save location for newly daily notes
-    daily_notes = {
-        folder = "001_DailyNotes",
-        template = "999_Templates/dailyNoteTemplateFromObsidianNvim.md",
-    },
     disable_frontmatter = true,
     -- Obsidian templates folder
     templates = {
         subdir = "999_Templates",
+        date_format = "%Y年%m月%d日",
         substitutions = {
-            -- 999_Templates/dailyNoteTemplate.md
-            -- 今日
+            -- 999_Templates/dailyNoteTemplateFromObsidianNvim.md
+            -- YYYY年MM月DD日
             today = function()
                 local today = today_helper()
                 local year = today.year
@@ -74,7 +70,7 @@ local opts = {
                 return (year .. "年" .. month .. "月" .. day .. "日")
             end,
             -- YYYY/MM/DD
-            today_fm_1 = function ()
+            today_fm_1 = function()
                 local today = today_helper()
                 local year = today.year
                 local month = today.z_month
@@ -82,7 +78,7 @@ local opts = {
                 return (year .. "/" .. month .. "/" .. day)
             end,
             -- YYYY/M/D
-            today_fm_2 = function ()
+            today_fm_2 = function()
                 local today = today_helper()
                 local year = today.year
                 local month = today.month
@@ -90,7 +86,7 @@ local opts = {
                 return (year .. "/" .. month .. "/" .. day)
             end,
             -- YYYY年M月D日
-            today_fm_3 = function ()
+            today_fm_3 = function()
                 local today = today_helper()
                 local year = today.year
                 local month = today.month
@@ -98,7 +94,7 @@ local opts = {
                 return (year .. "年" .. month .. "月" .. day .. "日")
             end,
             -- YYYY-MM-DD
-            today_fm_4 = function ()
+            today_fm_4 = function()
                 local today = today_helper()
                 local year = today.year
                 local month = today.z_month
@@ -106,14 +102,14 @@ local opts = {
                 return (year .. "-" .. month .. "-" .. day)
             end,
             -- YYYY-M-D
-            today_fm_5 = function ()
+            today_fm_5 = function()
                 local today = today_helper()
                 local year = today.year
                 local month = today.month
                 local day = today.day
                 return (year .. "-" .. month .. "-" .. day)
             end,
-            -- 翌日
+            -- Tomorrow
             tomorrow = function()
                 local tmp = os.date("*t", os.time() + day_sec)
                 local year = tmp.year
@@ -121,7 +117,7 @@ local opts = {
                 local day = string.format("%02d", tmp.day)
                 return (year .. "年" .. month .. "月" .. day .. "日")
             end,
-            -- 前日
+            -- Yesterday
             yesterday = function()
                 local tmp = os.date("*t", os.time() - day_sec)
                 local year = tmp.year
@@ -129,8 +125,8 @@ local opts = {
                 local day = string.format("%02d", tmp.day)
                 return (year .. "年" .. month .. "月" .. day .. "日")
             end,
-            -- 999_Templates/monthlyNoteTemplate.md
-            -- 今月
+            -- 999_Templates/monthlyNoteTemplateFromObsidianNvim.md
+            -- YYYY年MM月
             current_month = function()
                 local today = today_helper()
                 local year = today.year
@@ -172,40 +168,47 @@ local opts = {
                 local month = today.month
                 return (year .. "年" .. month .. "月")
             end,
-            -- 翌月
+            -- Next month
             next_month = function()
                 local tmp = os.date("*t", os.time() + month_sec + day_sec)
                 local year = tmp.year
                 local month = string.format("%02d", tmp.month)
                 return (year .. "年" .. month .. "月")
             end,
-            -- 前月
+            -- Last month
             last_month = function()
                 local tmp = os.date("*t", os.time() - month_sec)
                 local year = tmp.year
                 local month = string.format("%02d", tmp.month)
                 return (year .. "年" .. month .. "月")
             end,
-            -- 999_Templates/yearlyNoteTemplate.md
-            -- 今年
+            -- 999_Templates/yearlyNoteTemplateFromObsidianNvim.md
+            -- YYYY年
             current_year = function()
                 local today = today_helper()
                 local year = today.year
                 return (year .. "年")
             end,
-            -- 翌年
+            -- Next year
             next_year = function()
                 local tmp = os.date("*t", os.time() + year_sec)
                 local year = tmp.year
                 return (year .. "年")
             end,
-            -- 前年
+            -- Last year
             last_year = function()
                 local tmp = os.date("*t", os.time() - year_sec)
                 local year = tmp.year
                 return (year .. "年")
             end,
         },
+    },
+    -- Default save location for newly daily notes
+    daily_notes = {
+        folder = "001_DailyNotes",
+        date_format = "%Y年%m月%d日",
+        -- NOTE: Should without `999_Templates/`
+        template = "dailyNoteTemplateFromObsidianNvim.md",
     },
 
     -- Set to true if you use the Obsidian Advanced URI plugin.
