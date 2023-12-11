650ba8c81423e52360ae568b8eb6841ef493ef57
Yuto Tanaka <karaage_nennen@icloud.com>
2023-12-11 19:24:19 +0900

    Update neovim config: telescope.nvim

--- dot_config/nvim/lua/plugins/telescope-nvim.lua
+++ dot_config/nvim/lua/plugins/telescope-nvim.lua
@@ -4,7 +4,7 @@ return {
     -- TODO: setting
     -- https://github.com/nvim-telescope/telescope.nvim
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.2",
+    tag = "0.1.5",
     lazy = false,
     --event = "VimEnter",
     --cmd = "",
@@ -31,46 +31,20 @@ return {
 
         -- Ctrl+gで文字列検索を開く
         vim.keymap.set("n", "<C-g>", builtin.live_grep)
-        --        -- Ctrl+gで文字列検索を開く
-        --        -- <S-?>でプレビューを表示/非表示する
-        --        -- TODO: Luaで書く
-        --        -- https://github.com/junegunn/fzf.vim/blob/master/README.md
-        --        vim.cmd([[
-        --                command! -bang -nargs=* Rg
-        --                \ call fzf#vim#grep(
-        --                \ 'rg --column --line-number --hidden --ignore-case --no-heading --color=always '.shellescape(<q-args>), 1,
-        --                \ <bang>0 ? fzf#vim#with_preview({'options': '--delimiter : --nth 3..'}, 'up:60%')
-        --                \ : fzf#vim#with_preview({'options': '--exact --delimiter : --nth 3..'}, 'right:50%:hidden', '?'),
-        --                \ <bang>0)
-        --        ]])
-        --        vim.keymap.set("n", "<C-g>", function()
-        --            vim.cmd(":Rg<CR>")
-        --        end)
-        --
 
         -- frでカーソル位置の単語をファイル検索する
         -- frで選択した単語をファイル検索する
         vim.keymap.set("n", "fr", builtin.grep_string)
-        --        vim.keymap.set("n", "fr", function()
-        --            vim.cmd('vawy:Rg <C-R>"<CR>')
-        --        end)
-        --        vim.keymap.set("x", "fr y", function()
-        --            vim.cmd(':Rg <C-R>"<CR>')
-        --        end)
 
         -- fbでバッファ検索を開く
         vim.keymap.set("n", "fb", builtin.buffers)
 
         -- fpでバッファの中で1つ前に開いたファイルを開く
-        --vim.keymap.set("n", "fp", ":Buffers<CR><CR>")
-
-        --        -- fpでバッファの中で1つ前に開いたファイルを開く
-        --        vim.keymap.set("n", "fp", function()
-        --            vim.cmd(":Buffers<CR><CR>")
-        --        end)
+        -- TODO:
 
         -- flで開いているファイルの文字列検索を開く
-        vim.keymap.set("n", "fl", ":BLines<CR>")
+        -- TODO:
+        -- vim.keymap.set("n", "fl", ":BLines<CR>")
 
         -- fmでマーク検索を開く
         vim.keymap.set("n", "fm", builtin.marks)
@@ -79,11 +53,8 @@ return {
         vim.keymap.set("n", "fh", builtin.oldfiles)
 
         -- fcでコミット履歴検索を開く
-        --vim.keymap.set("n", "fc", ":Commits<CR>")
-        --        -- fcでコミット履歴検索を開く
-        --        vim.keymap.set("n", "fc", function()
-        --            vim.cmd(":Commits<CR>")
-        --        end)
+        -- TODO:
+        -- vim.keymap.set("n", "fc", ":Commits<CR>")
     end,
     cond = false,
 }
