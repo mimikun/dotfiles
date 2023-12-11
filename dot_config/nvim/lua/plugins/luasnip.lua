173a45f18f932c17a90a9eb005db94548bd1068e
Yuto Tanaka <karaage_nennen@icloud.com>
2023-12-11 19:22:55 +0900

    Fix neovim plugin config: LuaSnip

--- dot_config/nvim/lua/plugins/luasnip.lua
+++ dot_config/nvim/lua/plugins/luasnip.lua
@@ -1,8 +1,11 @@
 local global = require("core.global")
+local is_windows = global.is_windows
 local data_dir = global.data_dir
 local path_sep = global.path_sep
 local my_snippets_dir = global.snippets_dir
 local friendly_snippets = data_dir .. path_sep .. "lazy" .. path_sep .. "friendly-snippets"
+
+local build = is_windows and "" or "make install_jsregexp"
 
 return {
     "L3MON4D3/LuaSnip",
@@ -12,7 +15,7 @@ return {
         "rafamadriz/friendly-snippets",
     },
     version = "v2.*",
-    build = "make install_jsregexp",
+    build = build,
     config = function()
         require("luasnip.loaders.from_vscode").lazy_load({
             paths = {
