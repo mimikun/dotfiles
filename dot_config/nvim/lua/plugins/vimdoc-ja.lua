cd320357d78905ac9024103e50107d2b7db23130
Yuto Tanaka <karaage_nennen@icloud.com>
2023-12-22 20:36:39 +0900

    Update neovim plugin config: vimdoc-ja

--- dot_config/nvim/lua/plugins/vimdoc-ja.lua
+++ dot_config/nvim/lua/plugins/vimdoc-ja.lua
@@ -1,6 +1,18 @@
+local global = require("core.global")
+local path_sep = global.path_sep
+local data_dir = global.data_dir
+-- ref: https://github.com/folke/lazy.nvim/issues/1220
+---@diagnostic disable
+local build_cmd = function()
+    local tags_ja = data_dir .. "lazy" .. path_sep .. "vimdoc-ja" .. path_sep .. "doc" .. path_sep .. "tags-ja"
+    return "git update-index --skip-worktree" .. tags_ja
+end
+
 return {
     "mimikun/vimdoc-ja",
     branch = "fixed",
+    --"vim-jp/vimdoc-ja",
+    --build = build_cmd,
     --lazy = false,
     --event = "VeryLazy",
     keys = {
