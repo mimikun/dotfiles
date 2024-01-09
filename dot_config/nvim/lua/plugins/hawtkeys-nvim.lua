1fe31ed469d9b2d981109c2b75e84903cda37b5e
Yuto Tanaka <karaage_nennen@icloud.com>
2024-01-09 21:40:31 +0900

    Add new neovim plugin: hawtkeys.nvim

---
+++ dot_config/nvim/lua/plugins/hawtkeys-nvim.lua
@@ -0,0 +1,13 @@
+return {
+    "tris203/hawtkeys.nvim",
+    --lazy = false,
+    --event = "VeryLazy",
+    cmd = {
+        "Hawtkeys",
+        "HawtkeysAll",
+        "HawtkeysDupes",
+    },
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = {},
+    --cond = false,
+}
