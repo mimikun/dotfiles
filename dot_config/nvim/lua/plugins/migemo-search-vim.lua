0cde08b18cd8767fc550537f258f55c77ba97c97
Yuto Tanaka <karaage_nennen@icloud.com>
2024-01-12 18:51:46 +0900

    Add new neovim plugin: migemo-search.vim

---
+++ dot_config/nvim/lua/plugins/migemo-search-vim.lua
@@ -0,0 +1,21 @@
+-- Enable if there are no human rights and if executable cmigemo command
+local is_not_human_rights = not require("core.global").is_human_rights
+local has_cmigemo = (1 == vim.fn.executable("cmigemo")) and true or false
+local cond = is_not_human_rights and has_cmigemo
+
+local keymaps = {
+    { "<C-j>", desc = "Toggle Kana-mode eskk.vim" },
+}
+
+return {
+    "rhysd/migemo-search.vim",
+    lazy = false,
+    --event = "VeryLazy",
+    --keys = keymaps,
+    config = function()
+        -- TODO: config use lua
+        -- sample:
+        -- vim.g["fern#default_hidden"] = 1
+    end,
+    cond = cond,
+}
