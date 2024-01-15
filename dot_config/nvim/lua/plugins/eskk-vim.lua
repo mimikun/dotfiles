7338782aae4383e6eef129fe05d9fde31f11eb87
Yuto Tanaka <karaage_nennen@icloud.com>
2024-01-15 19:27:20 +0900

    Update neovim plugin config: eskk.vim

--- dot_config/nvim/lua/plugins/eskk-vim.lua
+++ dot_config/nvim/lua/plugins/eskk-vim.lua
@@ -11,31 +11,35 @@ return {
     --event = "VeryLazy",
     keys = keymaps,
     config = function()
-        -- TODO: config use lua
-        -- sample:
-        -- vim.g["fern#default_hidden"] = 1
-        vim.cmd([[
-            let g:eskk#directory = "~/.config/eskk"
-            let g:eskk#dictionary = { 'path': "~/.config/eskk/my_jisyo", 'sorted': 1, 'encoding': 'utf-8',}
-            let g:eskk#large_dictionary = {'path': "~/.config/eskk/SKK-JISYO.L", 'sorted': 1, 'encoding': 'euc-jp',}
-
-            "漢字変換した時に候補が1つの場合、自動的に確定する
-            let g:eskk#kakutei_when_unique_candidate = 1
-            "neocompleteを入れないと、1にすると動作しなくなるため0推奨
-            let g:eskk#enable_completion = 0
-            "デフォルトのマッピングを削除
-            let g:eskk#no_default_mappings = 1
-            "ノーマルモードに戻るとeskkモードを初期値にする
-            let g:eskk#keep_state = 0
-            "漢字変換を確定しても改行しない。
-            let g:eskk#egg_like_newline = 1
-
-            "表示文字を変更
-            let g:eskk#marker_henkan = "[変換]"
-            let g:eskk#marker_henkan_select = "[選択]"
-            let g:eskk#marker_okuri = "[送り]"
-            let g:eskk#marker_jisyo_touroku = "[辞書]"
-        ]])
+        -- eskkのconfig directory
+        vim.g["eskk#directory"] = "~/.config/eskk"
+        -- ユーザー辞書
+        vim.g["eskk#dictionary"] = {
+            path = "~/.skk/SKK-JISYO.mimikun",
+            sorted = 1,
+            encoding = "utf-8",
+        }
+        -- SKK-JISYO.L
+        vim.g["eskk#large_dictionary"] = {
+            path = "~/.skk/SKK-JISYO.L",
+            sorted = 1,
+            encoding = "euc-jp",
+        }
+        -- 漢字変換した時に候補が1つの場合、自動的に確定する
+        vim.g["eskk#kakutei_when_unique_candidate"] = 1
+        -- neocompleteを入れないと、1にすると動作しなくなるため0推奨
+        vim.g["eskk#enable_completion"] = 0
+        -- デフォルトのマッピングを削除
+        vim.g["eskk#no_default_mappings"] = 1
+        -- ノーマルモードに戻るとeskkモードを初期値にする
+        vim.g["eskk#keep_state"] = 0
+        -- 漢字変換を確定しても改行しない。
+        vim.g["eskk#egg_like_newline"] = 1
+        -- 表示文字を変更
+        vim.g["eskk#marker_henkan"] = "[変換]"
+        vim.g["eskk#marker_henkan_select"] = "[選択]"
+        vim.g["eskk#marker_okuri"] = "[送り]"
+        vim.g["eskk#marker_jisyo_touroku"] = "[辞書]"
     end,
     cond = cond,
 }
