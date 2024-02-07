6087bb3d07a1a30d5b567a6fbff6ea69ceb37369
Yuto Tanaka <karaage_nennen@icloud.com>
2024-02-07 19:02:42 +0900

    Improved neovim plugin config: kensaku.vim

---
+++ dot_config/nvim/lua/plugins/kensaku-command.lua
@@ -0,0 +1,17 @@
+local global = require("core.global")
+local is_human_rights = global.is_human_rights
+
+return {
+    "lambdalisue/kensaku-command.vim",
+    --lazy = false,
+    --event = "VeryLazy",
+    cmd = "Kensaku",
+    dependencies = {
+        "vim-denops/denops.vim",
+        "lambdalisue/kensaku.vim",
+    },
+    config = true,
+    -- Disable if there are no human rights
+    --cond = is_human_rights,
+    cond = false,
+}
---
+++ dot_config/nvim/lua/plugins/kensaku-search.lua
@@ -0,0 +1,22 @@
+local global = require("core.global")
+local is_human_rights = global.is_human_rights
+local keymaps = {
+    { "<CR>", desc = "kensaku-search-replace"},
+}
+
+return {
+    "lambdalisue/kensaku-search.vim",
+    --lazy = false,
+    --event = "VeryLazy",
+    keys = keymaps,
+    dependencies = {
+        "vim-denops/denops.vim",
+        "lambdalisue/kensaku.vim",
+    },
+    config = function()
+        vim.set.keymap("c", "<CR>", "<Plug>(kensaku-search-replace)<CR>", { noremap = true })
+    end,
+    -- Disable if there are no human rights
+    --cond = is_human_rights,
+    cond = false,
+}
