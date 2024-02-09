3e323aa4422eea60d48f5862c9d7a62a20beebc8
Yuto Tanaka <karaage_nennen@icloud.com>
2024-02-09 22:18:16 +0900

    Workaround for a FXXING SHXT BUG caused by the combination of vimdoc-ja and lazy.nvim

--- dot_config/nvim/lua/plugins/vimdoc-ja.lua
+++ dot_config/nvim/lua/plugins/vimdoc-ja.lua
@@ -1,18 +1,27 @@
-local global = require("core.global")
-local path_sep = global.path_sep
-local data_dir = global.data_dir
--- ref: https://github.com/folke/lazy.nvim/issues/1220
----@diagnostic disable
-local build_cmd = function()
-    local tags_ja = data_dir .. "lazy" .. path_sep .. "vimdoc-ja" .. path_sep .. "doc" .. path_sep .. "tags-ja"
-    return "git update-index --skip-worktree" .. tags_ja
-end
+local vimdoc_repo = vim.fn.expand("$XDG_DATA_HOME/nvim/site/lazy/vimdoc-ja/")
+
+-- NOTE: Workaround for a FXXING SHXT BUG caused by the combination of vimdoc-ja and lazy.nvim
+-- thx: https://github.com/delphinus/dotfiles/blob/master/.config/nvim/lua/core/lazy/init.lua
+vim.api.nvim_create_autocmd("User", {
+    pattern = "LazyUpdatePre",
+    group = vim.api.nvim_create_augroup("lazy-update-pre", {}),
+    callback = function()
+        vim.system({ "git", "reset", "--hard" }, { cwd = tostring(vimdoc_repo) }, function(info)
+            if info.code == 0 then
+                vim.schedule(function()
+                    vim.notify("SUCCESS: git reset --hard", vim.log.levels.DEBUG)
+                end)
+            else
+                vim.schedule(function()
+                    vim.notify("FAILED: git reset --hard", vim.log.levels.ERROR)
+                end)
+            end
+        end):wait()
+    end,
+})
 
 return {
-    "mimikun/vimdoc-ja",
-    branch = "fixed",
-    --"vim-jp/vimdoc-ja",
-    --build = build_cmd,
+    "vim-jp/vimdoc-ja",
     --lazy = false,
     --event = "VeryLazy",
     keys = {
