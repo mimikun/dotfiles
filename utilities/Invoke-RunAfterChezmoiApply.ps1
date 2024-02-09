9f7728c1cf157ff8dd498aa21a8dadf40d92cc52
Yuto Tanaka <karaage_nennen@icloud.com>
2024-02-09 22:18:16 +0900

    Improved Invoke-RunAfterChezmoiApply.ps1
    Fix #223

--- utilities/Invoke-RunAfterChezmoiApply.ps1
+++ utilities/Invoke-RunAfterChezmoiApply.ps1
@@ -1,12 +1,14 @@
 ﻿# Run after chezmoi apply
 function Invoke-RunAfterChezmoiApply() {
     Write-Output "post-apply-hook"
+    $base_profile = Join-Path $env:USERPROFILE -ChildPath ".config\powershell\Microsoft.PowerShell_profile.ps1"
     $home_pwsh_profile = Join-Path $env:USERPROFILE -ChildPath "OneDrive\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
     $home_powershell_profile = Join-Path $env:USERPROFILE -ChildPath "OneDrive\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
     $work_pwsh_profile = Join-Path $env:USERPROFILE -ChildPath "Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
     $work_powershell_profile = Join-Path $env:USERPROFILE -ChildPath "Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
 
-    $base_profile = $home_pwsh_profile
+    Write-Output "Copy PowerShell profiles"
+    Copy-Item -Path $base_profile -Destination $home_pwsh_profile
     Copy-Item -Path $base_profile -Destination $home_powershell_profile
     Copy-Item -Path $base_profile -Destination $work_pwsh_profile
     Copy-Item -Path $base_profile -Destination $work_powershell_profile
