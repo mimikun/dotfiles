32b66e82f5d567f6453777f92ba338439d3d42dc
Yuto Tanaka <karaage_nennen@icloud.com>
2024-02-12 18:17:08 +0900

    Update neovim plugin config: nvim-surround

--- dot_config/nvim/lua/plugins/nvim-surround.lua
+++ dot_config/nvim/lua/plugins/nvim-surround.lua
@@ -3,10 +3,21 @@ return {
     version = "*",
     --lazy = false,
     event = "VeryLazy",
+    dependencies = {
+        "roobert/surround-ui.nvim",
+        "folke/which-key.nvim",
+    },
     config = function()
         -- TODO: setting
         -- https://github.com/kylechui/nvim-surround/blob/main/README.md
-        require("nvim-surround").setup({})
+        local surround = require("nvim-surround")
+        local surround_ui = require("surround-ui")
+
+        surround.setup({})
+        surround_ui.setup({
+            root_key = "S",
+        })
     end,
+    -- TODO: enable
     cond = false,
 }
