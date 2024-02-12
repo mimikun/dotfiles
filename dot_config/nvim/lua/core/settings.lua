f6fe0c3138297e629a61f71846e52e064cd15caf
Yuto Tanaka <karaage_nennen@icloud.com>
2024-02-12 17:18:43 +0900

    Remove unused neovim plugin: coc.nvim

--- dot_config/nvim/lua/core/settings.lua
+++ dot_config/nvim/lua/core/settings.lua
@@ -4,38 +4,6 @@ local settings = {}
 ---@type boolean
 settings["use_copilot"] = false
 
--- Required coc extensions
----@type table
-settings["coc_extensions"] = {
-    "coc-css",
-    "coc-deepl",
-    "coc-deno",
-    "coc-eslint",
-    "coc-explorer",
-    "coc-git",
-    "coc-highlight",
-    "coc-html",
-    "coc-html-css-support",
-    "coc-jest",
-    "coc-json",
-    "coc-lists",
-    "coc-lua",
-    "coc-marketplace",
-    "coc-pairs",
-    "@raidou/coc-prettier-v3",
-    "coc-pyright",
-    "coc-rust-analyzer",
-    "coc-snippets",
-    "coc-solargraph",
-    "coc-toml",
-    "coc-tsserver",
-    "coc-yaml",
-    "@yaegassy/coc-black-formatter",
-    "@yaegassy/coc-mypy",
-    "@yaegassy/coc-nginx",
-    "coc-zig",
-}
-
 -- Required lsp servers
 ---@type table
 settings["lsp_servers"] = {
