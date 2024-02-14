71346bc9390ecfe966339722aebdb697223784fd
Yuto Tanaka <karaage_nennen@icloud.com>
2024-02-14 20:11:19 +0900

    Improved utils/update.sh
    Run format dotfyle.json

--- utils/update.sh
+++ utils/update.sh
@@ -36,6 +36,7 @@ chezmoi add ~/.config/nvim/mason-lock.json
 
 # dotfyle.json
 chezmoi add ~/.config/nvim/dotfyle.json
+deno fmt dot_config/nvim/dotfyle.json
 
 # ~/.local/bin/*
 chezmoi add ~/.local/bin/chezmoi_post_apply_hook
