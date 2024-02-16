39be1649cba04ef958a430199ef7c7e64c91e95a
Yuto Tanaka <karaage_nennen@icloud.com>
2024-02-16 19:58:36 +0900

    Add new neovim plugin: human-rights.nvim

---
+++ dot_config/nvim/lua/plugins/human-rights.lua
@@ -0,0 +1,15 @@
+local cmds = {
+    "ShowCpu",
+    "ShowMemory",
+    "ShowParallelism",
+    "ShowDisk",
+}
+
+return {
+    "mimikun/human-rights.nvim",
+    --lazy = false,
+    --event = "VeryLazy",
+    cmd = cmds,
+    --config = true,
+    --cond = false,
+}
