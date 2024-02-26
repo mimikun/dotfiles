c913d968ca95eb23cc0e29d40f16afb02306b13f
Yuto Tanaka <karaage_nennen@icloud.com>
2024-02-26 20:27:04 +0900

    Improved utils/create-patch.sh

--- utils/create-patch.sh
+++ utils/create-patch.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 PRODUCT_NAME="dotfiles"
+DEFAULT_REMOTE="origin"
+DEFAULT_BRANCH="master"
 TODAY=$(date +'%Y%m%d')
 BRANCH_NAME=$(git branch --show-current)
 GPG_PUB_KEY="CCAA9E0638DF9088BB624BC37C0F8AD3FB3938FC"
 USE_GPG=false
 
-if [[ "$BRANCH_NAME" = "master" ]] || [[ "$BRANCH_NAME" = "patch-"* ]]; then
-    echo "This branch is master or patch branch"
+if [[ "$BRANCH_NAME" = "$DEFAULT_BRANCH" ]] || [[ "$BRANCH_NAME" = "patch-"* ]]; then
+    echo "This branch is $DEFAULT_BRANCH or patch branch"
 
     for p in "$PRODUCT_NAME" "." "$TODAY" "." "patch"; do
         PATCH_NAME+=$p
@@ -23,10 +25,10 @@ fi
 # if GPG patch
 if ! $USE_GPG; then
     echo "patch file name: $PATCH_NAME"
-    git diff origin/master >"$PATCH_NAME"
+    git diff "$DEFAULT_REMOTE/$DEFAULT_BRANCH" >"$PATCH_NAME"
 else
     GPG_PATCH_NAME+=$PATCH_NAME
     GPG_PATCH_NAME+=".gpg"
     echo "gpg patch file name: $GPG_PATCH_NAME"
-    git diff origin/master | gpg --encrypt --recipient "$GPG_PUB_KEY" >"$GPG_PATCH_NAME"
+    git diff "$DEFAULT_REMOTE/$DEFAULT_BRANCH" | gpg --encrypt --recipient "$GPG_PUB_KEY" >"$GPG_PATCH_NAME"
 fi
