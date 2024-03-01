a9c7df2d64d3a0b03f4785a39bfeeb49611eb282
Yuto Tanaka <karaage_nennen@icloud.com>
2024-03-02 01:07:10 +0900

    Add comment

--- dot_config/nvim/lua/plugins/ccc-nvim.lua
+++ dot_config/nvim/lua/plugins/ccc-nvim.lua
@@ -2,6 +2,8 @@ return {
     "uga-rosa/ccc.nvim",
     --lazy = false,
     event = "BufRead",
+    -- https://github.com/uga-rosa/ccc.nvim/issues/104
+    --commit = "392ef0640b96684e88b3965f32f3bc42530f66c3",
     opts = {
         highlighter = {
             auto_enable = true,
