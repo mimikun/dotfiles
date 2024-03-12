f523b2d1d86e22b5081dad5d78e31e6a3554e7be
Yuto Tanaka <karaage_nennen@icloud.com>
2024-03-12 20:35:31 +0900

    Update neovim plugin config: noice.nvim

--- dot_config/nvim/lua/plugins/noice.lua
+++ dot_config/nvim/lua/plugins/noice.lua
@@ -7,9 +7,33 @@ local spec = {
     dependencies = {
         "MunifTanjim/nui.nvim",
         "rcarriga/nvim-notify",
+        "hrsh7th/nvim-cmp",
     },
-    opts = {},
-    cond = false,
+    opts = {
+        lsp = {
+            -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+            override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+                -- requires hrsh7th/nvim-cmp
+                ["cmp.entry.get_documentation"] = true,
+            },
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+            -- use a classic bottom cmdline for search
+            bottom_search = true,
+            -- position the cmdline and popupmenu together
+            command_palette = true,
+            -- long messages will be sent to a split
+            long_message_to_split = true,
+            -- enables an input dialog for inc-rename.nvim
+            inc_rename = false,
+            -- add a border to hover docs and signature help
+            lsp_doc_border = false,
+        },
+    },
+    --cond = false,
 }
 
 return spec
