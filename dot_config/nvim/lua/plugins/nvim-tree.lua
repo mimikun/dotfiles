ec21ee48d0c6c6f14c6dd20a322b0c32928e38a3
Yuto Tanaka <karaage_nennen@icloud.com>
2024-03-18 18:09:25 +0900

    Update neovim plugin config: nvim-tree

--- dot_config/nvim/lua/plugins/nvim-tree.lua
+++ dot_config/nvim/lua/plugins/nvim-tree.lua
@@ -1,4 +1,9 @@
 ---@type table
+local keymaps = {
+    { "<C-n>", desc = "Toggle Fern" },
+}
+
+---@type table
 local cmds = {
     "NvimTreeOpen",
     "NvimTreeClose",
@@ -19,6 +24,7 @@ local spec = {
     "nvim-tree/nvim-tree.lua",
     version = "*",
     lazy = false,
+    keys = keymaps,
     cmd = cmds,
     --event = "VeryLazy",
     dependencies = {
@@ -28,56 +34,12 @@ local spec = {
         vim.g.loaded_netrw = 1
         vim.g.loaded_netrwPlugin = 1
     end,
-    -- TODO: settings
-    opts = {},
-    cond = false,
-}
-
-return spec
-
---[[
-fern configs
-
----@type table
-local keymaps = {
-    { "<C-n>", desc = "Toggle Fern" },
-}
-
----@type LazySpec
-local spec = {
-    "lambdalisue/fern.vim",
-    branch = "main",
-    --lazy = false,
-    --event = "VeryLazy",
-    keys = keymaps,
-    dependencies = {
-        "nvim-tree/nvim-web-devicons",
-        "TheLeoP/fern-renderer-web-devicons.nvim",
-        "lambdalisue/glyph-palette.vim",
-        "lambdalisue/fern-git-status.vim",
-    },
     config = function()
-        -- アイコン表示を有効にする
-        vim.g["fern#renderer"] = "nvim-web-devicons"
-
-        -- アイコンに色をつける
-        vim.api.nvim_create_augroup("my_glyph_palette", {})
-        vim.api.nvim_create_autocmd("FileType", {
-            group = "my_glyph_palette",
-            pattern = "fern",
-            command = "call glyph_palette#apply()",
-        })
-        vim.api.nvim_create_autocmd("FileType", {
-            group = "my_glyph_palette",
-            pattern = { "nerdtree", "startify" },
-            command = "call glyph_palette#apply()",
-        })
-
-        -- Ctrl+nでファイルツリーを表示/非表示する
-        vim.keymap.set("n", "<C-n>", ":Fern . -reveal=% -drawer -toggle -width=40<CR>")
-        -- ドットファイルを表示させる
-        vim.g["fern#default_hidden"] = 1
+        local nvim_tree = require("nvim-tree")
+        nvim_tree.setup({})
+        vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>")
     end,
     --cond = false,
 }
-]]
+
+return spec
