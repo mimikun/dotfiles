01b40321330dd6088529671f4713e137ac2d85a3
Yuto Tanaka <karaage_nennen@icloud.com>
2024-03-21 19:43:51 +0900

    Add plugin spec copy file: spec-template.lua

---
+++ dot_config/nvim/lua/plugins/spec-template.lua
@@ -0,0 +1,15 @@
+---@type LazySpec
+local spec = {
+    "mimikun/spec-template",
+    --lazy = false,
+    enabled = false,
+    --cmd = "CMDNAME",
+    --keys = "",
+    --event = "VeryLazy",
+    --dependencies = { "nvim-tree/nvim-web-devicons" },
+    --opts = {},
+    --config = true,
+    cond = false,
+}
+
+return spec
