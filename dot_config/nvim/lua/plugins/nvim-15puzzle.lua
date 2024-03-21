aa43da07a819bbf2f03a95bc372addb91cb56ea2
Yuto Tanaka <karaage_nennen@icloud.com>
2024-03-21 19:43:51 +0900

    Add new neovim plugin: 15puzzle.nvim

---
+++ dot_config/nvim/lua/plugins/nvim-15puzzle.lua
@@ -0,0 +1,11 @@
+---@type LazySpec
+local spec = {
+    "NStefan002/15puzzle.nvim",
+    --lazy = false,
+    --event = "VeryLazy",
+    cmd = "Play15puzzle",
+    config = true,
+    --cond = false,
+}
+
+return spec
