0ef20278c55ebd955b0d3364ce160cd9f9c80f29
Yuto Tanaka <karaage_nennen@icloud.com>
2024-03-27 20:13:33 +0900

    refactor!: improved colorscheme plugins config

--- dot_config/nvim/lua/plugins/themes.lua
+++ dot_config/nvim/lua/plugins/themes.lua
@@ -1,203 +1,400 @@
+local themery_config = require("core.global").themery_config
+
 ---@type LazySpec
-local spec = {
+local github_theme = {
+    "projekt0n/github-nvim-theme",
+    lazy = false,
+    --event = "VeryLazy",
+    priority = 1000,
+    --cond = false,
+}
+
+---@type LazySpec
+local cyberdream = {
+    "scottmckendry/cyberdream.nvim",
+    --lazy = false,
+    --event = "VeryLazy",
+    --priority = 1000,
+    opts = {
+        -- Enable transparent background
+        transparent = true,
+        -- Enable italics comments
+        italic_comments = true,
+        -- Replace all fillchars with ' ' for the ultimate clean look
+        hide_fillchars = true,
+        -- Modern borderless telescope theme
+        borderless_telescope = true,
+    },
+    --cond = false,
+}
+
+---@type LazySpec
+local monokai = {
+    "tanvirtin/monokai.nvim",
+    --lazy = false,
+    --event = "VeryLazy",
+    --priority = 1000,
+    config = true,
+    --cond = false,
+}
+
+---@type LazySpec
+local penumbra = {
+    "Allianaab2m/penumbra.nvim",
+    --lazy = false,
+    --event = "VeryLazy",
+    --priority = 1000,
+    config = true,
+    --cond = false,
+}
+
+---@type LazySpec
+local tokyonight = {
+    "folke/tokyonight.nvim",
+    --lazy = false,
+    --event = "VeryLazy",
+    --priority = 1000,
+    opts = {},
+    --cond = false,
+}
+
+---@type LazySpec
+local yash = {
+    "kihachi2000/yash.nvim",
+    --lazy = false,
+    --event = "VeryLazy",
+    --priority = 1000,
+    --cond = false,
+}
+
+---@type LazySpec
+local nightfox = {
+    "EdenEast/nightfox.nvim",
+    --lazy = false,
+    --event = "VeryLazy",
+    --priority = 1000,
+    config = true,
+    --cond = false,
+}
+
+---@type LazySpec
+local tokyodark = {
+    "tiagovla/tokyodark.nvim",
+    --lazy = false,
+    --event = "VeryLazy",
+    --priority = 1000,
+    config = true,
+    --cond = false,
+}
+
+---@type LazySpec
+local monokai_pro = {
+    "loctvl842/monokai-pro.nvim",
+    --lazy = false,
+    --event = "VeryLazy",
+    --priority = 1000,
+    config = true,
+    --cond = false,
+}
+
+---@type LazySpec
+local nord = {
+    "shaunsingh/nord.nvim",
+    --lazy = false,
+    --event = "VeryLazy",
+    --priority = 1000,
+    --cond = false,
+}
+
+---@type LazySpec
+local catppuccin = {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    --lazy = false,
+    --event = "VeryLazy",
+    priority = 1000,
+    config = true,
+    --cond = false,
+}
+
+---@type LazySpec
+local pastelnight = {
+    "pauchiner/pastelnight.nvim",
+    --lazy = false,
+    --event = "VeryLazy",
+    priority = 1000,
+    opts = {},
+    --cond = false,
+}
+
+---@type LazySpec
+local kurayami = {
+    "kevinm6/kurayami.nvim",
+    --lazy = false,
+    --event = "VimEnter",
+    priority = 1000,
+    --cond = false,
+}
+
+---@type LazySpec
+local lavi = {
+    "b0o/lavi.nvim",
+    --lazy = false,
+    --event = "VimEnter",
+    dependencies = { "rktjmp/lush.nvim" },
+    --priority = 1000,
+    --cond = false,
+}
+
+---@type LazySpec
+local evergarden = {
+    "comfysage/evergarden",
+    --lazy = false,
+    --event = "VimEnter",
+    opts = {
+        transparent_background = true,
+        -- 'hard'|'medium'|'soft'
+        contrast_dark = "medium",
+        -- add custom overrides
+        overrides = {},
+    },
+    --priority = 1000,
+    --cond = false,
+}
+
+---@type LazySpec
+local sunburn = {
+    "loganswartz/sunburn.nvim",
+    --lazy = false,
+    --event = "VimEnter",
+    dependencies = { "loganswartz/polychrome.nvim" },
+    --priority = 1000,
+    --cond = false,
+}
+
+---@type LazySpec
+local sonokai = {
+    "sainnhe/sonokai",
+    --lazy = false,
+    --event = "VeryLazy",
+    --priority = 1000,
+    config = function()
+        vim.g.sonokai_style = "default"
+    end,
+    --cond = false,
+}
+
+---@type LazySpec
+local kyotonight = {
+    "voidekh/kyotonight.vim",
+    --lazy = false,
+    --event = "VeryLazy",
+    --priority = 1000,
+    --cond = false,
+}
+
+---@type LazySpec
+local momiji = {
+    "kyoh86/momiji",
+    --lazy = false,
+    --event = "VeryLazy",
+    --priority = 1000,
+    --cond = false,
+}
+
+---@type LazySpec
+local edge = {
+    "sainnhe/edge",
+    --lazy = false,
+    --event = "VeryLazy",
+    --priority = 1000,
+    config = function()
+        vim.g.edge_style = "default"
+        vim.g.edge_better_performance = 1
+    end,
+    --cond = false,
+}
+
+---@type LazySpec[]
+local theme_specs = {
     -- Lua themes
-    {
-        "projekt0n/github-nvim-theme",
-        lazy = false,
-        --event = "VeryLazy",
-        priority = 1000,
-        --cond = false,
-    },
-    {
-        "scottmckendry/cyberdream.nvim",
-        --lazy = false,
-        --event = "VeryLazy",
-        --priority = 1000,
-        opts = {
-            -- Enable transparent background
-            transparent = true,
-            -- Enable italics comments
-            italic_comments = true,
-            -- Replace all fillchars with ' ' for the ultimate clean look
-            hide_fillchars = true,
-            -- Modern borderless telescope theme
-            borderless_telescope = true,
-        },
-        --cond = false,
-    },
-    {
-        "crispybaccoon/evergarden",
-        --lazy = false,
-        --event = "VeryLazy",
-        --priority = 1000,
-        opts = {
-            transparent_background = true,
-            -- 'hard'|'medium'|'soft'
-            contrast_dark = "medium",
-            override_terminal = true,
-            style = {
-                tabline = { reverse = true, color = "green" },
-                search = { reverse = false, inc_reverse = true },
-                types = { italic = true },
-                keyword = { italic = true },
-                comment = { italic = false },
-            },
-            -- add custom overrides
-            overrides = {},
-        },
-        --cond = false,
-    },
-    {
-        "tanvirtin/monokai.nvim",
-        --lazy = false,
-        --event = "VeryLazy",
-        --priority = 1000,
-        config = true,
-        --cond = false,
-    },
-    {
-        "Allianaab2m/penumbra.nvim",
-        --lazy = false,
-        --event = "VeryLazy",
-        --priority = 1000,
-        config = true,
-        --cond = false,
-    },
-    {
-        "folke/tokyonight.nvim",
-        --lazy = false,
-        --event = "VeryLazy",
-        --priority = 1000,
-        opts = {},
-        --cond = false,
-    },
-    {
-        "kihachi2000/yash.nvim",
-        --lazy = false,
-        --event = "VeryLazy",
-        --priority = 1000,
-        --cond = false,
-    },
-    {
-        "EdenEast/nightfox.nvim",
-        --lazy = false,
-        --event = "VeryLazy",
-        --priority = 1000,
-        config = true,
-        --cond = false,
-    },
-    {
-        "tiagovla/tokyodark.nvim",
-        --lazy = false,
-        --event = "VeryLazy",
-        --priority = 1000,
-        config = true,
-        --cond = false,
-    },
-    {
-        "loctvl842/monokai-pro.nvim",
-        --lazy = false,
-        --event = "VeryLazy",
-        --priority = 1000,
-        config = true,
-        --cond = false,
-    },
-    {
-        "shaunsingh/nord.nvim",
-        --lazy = false,
-        --event = "VeryLazy",
-        --priority = 1000,
-        --cond = false,
-    },
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        --lazy = false,
-        --event = "VeryLazy",
-        priority = 1000,
-        config = true,
-        --cond = false,
-    },
-    {
-        "pauchiner/pastelnight.nvim",
-        --lazy = false,
-        --event = "VeryLazy",
-        priority = 1000,
-        opts = {},
-        --cond = false,
-    },
-    {
-        "kevinm6/kurayami.nvim",
-        --lazy = false,
-        --event = "VimEnter",
-        priority = 1000,
-        --cond = false,
-    },
-    {
-        "b0o/lavi.nvim",
-        --lazy = false,
-        --event = "VimEnter",
-        dependencies = { "rktjmp/lush.nvim" },
-        --priority = 1000,
-        --cond = false,
-    },
-    {
-        -- XXX: name duplicate
-        "comfysage/evergarden",
-        --lazy = false,
-        --event = "VimEnter",
-        opts = {
-            transparent_background = true,
-            contrast_dark = "medium", -- 'hard'|'medium'|'soft'
-            overrides = {}, -- add custom overrides
-        },
-        --priority = 1000,
-        cond = false,
-    },
-    {
-        "loganswartz/sunburn.nvim",
-        --lazy = false,
-        --event = "VimEnter",
-        dependencies = { "loganswartz/polychrome.nvim" },
-        --priority = 1000,
-        --cond = false,
-    },
+    github_theme,
+    cyberdream,
+    monokai,
+    penumbra,
+    tokyonight,
+    yash,
+    nightfox,
+    tokyodark,
+    monokai_pro,
+    nord,
+    catppuccin,
+    pastelnight,
+    kurayami,
+    lavi,
+    evergarden,
+    sunburn,
     -- VimScript themes
+    sonokai,
+    kyotonight,
+    momiji,
+    edge,
+}
+
+---@type table
+local huez_omit_themes = {
+    "default",
+    "desert",
+    "evening",
+    "industry",
+    "koehler",
+    "morning",
+    "murphy",
+    "pablo",
+    "peachpuff",
+    "ron",
+    "shine",
+    "slate",
+    "torte",
+    "zellner",
+    "blue",
+    "darkblue",
+    "delek",
+    "quiet",
+    "elflord",
+    "habamax",
+    "lunaperche",
+}
+
+---@type LazySpec
+local huez = {
+    "vague2k/huez.nvim",
+    --lazy = false,
+    cmd = "Huez",
+    --event = "VeryLazy",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    config = function()
+        local huez = require("huez")
+        local huez_api = require("huez.api")
+
+        huez.setup({
+            file_path = vim.fs.normalize(vim.fn.stdpath("config")) .. "/.nvim.huez.lua",
+            fallback = "default",
+            omit = huez_omit_themes,
+            -- "vim" or "telescope"
+            picker = "telescope",
+            picker_opts = require("telescope.themes").get_dropdown({}),
+        })
+        local colorscheme = huez_api.get_colorscheme()
+        vim.cmd.colorscheme(colorscheme)
+    end,
+    cond = false,
+}
+
+---@type table
+local themery_selectables = {
     {
-        "sainnhe/sonokai",
-        --lazy = false,
-        --event = "VeryLazy",
-        --priority = 1000,
-        config = function()
-            vim.g.sonokai_style = "default"
-        end,
-        --cond = false,
+        name = "github_dark",
+        colorscheme = "github_dark",
     },
     {
-        "voidekh/kyotonight.vim",
-        --lazy = false,
-        --event = "VeryLazy",
-        --priority = 1000,
-        --cond = false,
+        name = "cyberdream",
+        colorscheme = "cyberdream",
     },
     {
-        "kyoh86/momiji",
-        --lazy = false,
-        --event = "VeryLazy",
-        --priority = 1000,
-        --cond = false,
+        name = "monokai",
+        colorscheme = "monokai",
     },
     {
-        "sainnhe/edge",
-        --lazy = false,
-        --event = "VeryLazy",
-        --priority = 1000,
-        config = function()
-            vim.g.edge_style = "default"
-            vim.g.edge_better_performance = 1
-        end,
-        --cond = false,
+        name = "penumbra",
+        colorscheme = "penumbra",
+    },
+    {
+        name = "tokyonight",
+        colorscheme = "tokyonight",
+    },
+    {
+        name = "yash",
+        colorscheme = "yash",
+    },
+    {
+        name = "nightfox",
+        colorscheme = "nightfox",
+    },
+    {
+        name = "tokyodark",
+        colorscheme = "tokyodark",
+    },
+    {
+        name = "monokai_pro",
+        colorscheme = "monokai-pro",
+    },
+    {
+        name = "nord",
+        colorscheme = "nord",
+    },
+    {
+        name = "catppuccin",
+        colorscheme = "catppuccin",
+    },
+    {
+        name = "pastelnight",
+        colorscheme = "pastelnight",
+    },
+    {
+        name = "kurayami",
+        colorscheme = "kurayami",
+    },
+    {
+        name = "lavi",
+        colorscheme = "lavi",
+    },
+    -- TODO: evergarden_2
+    {
+        name = "",
+        colorscheme = "",
+    },
+    {
+        name = "sunburn",
+        colorscheme = "sunburn",
+    },
+    {
+        name = "sonokai",
+        colorscheme = "sonokai",
+    },
+    {
+        name = "kyotonight",
+        colorscheme = "kyotonight",
+    },
+    {
+        name = "momiji",
+        colorscheme = "momiji",
+    },
+    {
+        name = "edge",
+        colorscheme = "edge",
     },
 }
 
-return spec
+local themery = {
+    "zaldih/themery.nvim",
+    --lazy = false,
+    --event = "VeryLazy",
+    cmd = "Themery",
+    opts = {
+        themes = themery_selectables,
+        themeConfigFile = themery_config,
+        livePreview = true,
+    },
+    --cond = false,
+}
+
+---@type LazySpec[]
+local specs = {
+    --github_theme,
+    theme_specs,
+    huez,
+    themery,
+}
+
+return specs
