a351c3e11dced7ab694e53d39f828bebeaef4561
Yuto Tanaka <karaage_nennen@icloud.com>
2024-04-05 21:47:20 +0900

    chore(fish): update gh completion

--- dot_config/fish/completions/gh.fish
+++ dot_config/fish/completions/gh.fish
@@ -55,6 +55,60 @@ function __gh_perform_completion
     printf "%s\n" "$directiveLine"
 end
 
+# this function limits calls to __gh_perform_completion, by caching the result behind $__gh_perform_completion_once_result
+function __gh_perform_completion_once
+    __gh_debug "Starting __gh_perform_completion_once"
+
+    if test -n "$__gh_perform_completion_once_result"
+        __gh_debug "Seems like a valid result already exists, skipping __gh_perform_completion"
+        return 0
+    end
+
+    set --global __gh_perform_completion_once_result (__gh_perform_completion)
+    if test -z "$__gh_perform_completion_once_result"
+        __gh_debug "No completions, probably due to a failure"
+        return 1
+    end
+
+    __gh_debug "Performed completions and set __gh_perform_completion_once_result"
+    return 0
+end
+
+# this function is used to clear the $__gh_perform_completion_once_result variable after completions are run
+function __gh_clear_perform_completion_once_result
+    __gh_debug ""
+    __gh_debug "========= clearing previously set __gh_perform_completion_once_result variable =========="
+    set --erase __gh_perform_completion_once_result
+    __gh_debug "Successfully erased the variable __gh_perform_completion_once_result"
+end
+
+function __gh_requires_order_preservation
+    __gh_debug ""
+    __gh_debug "========= checking if order preservation is required =========="
+
+    __gh_perform_completion_once
+    if test -z "$__gh_perform_completion_once_result"
+        __gh_debug "Error determining if order preservation is required"
+        return 1
+    end
+
+    set -l directive (string sub --start 2 $__gh_perform_completion_once_result[-1])
+    __gh_debug "Directive is: $directive"
+
+    set -l shellCompDirectiveKeepOrder 32
+    set -l keeporder (math (math --scale 0 $directive / $shellCompDirectiveKeepOrder) % 2)
+    __gh_debug "Keeporder is: $keeporder"
+
+    if test $keeporder -ne 0
+        __gh_debug "This does require order preservation"
+        return 0
+    end
+
+    __gh_debug "This doesn't require order preservation"
+    return 1
+end
+
+
 # This function does two things:
 # - Obtain the completions and store them in the global __gh_comp_results
 # - Return false if file completion should be performed
@@ -65,17 +119,17 @@ function __gh_prepare_completions
     # Start fresh
     set --erase __gh_comp_results
 
-    set -l results (__gh_perform_completion)
-    __gh_debug "Completion results: $results"
+    __gh_perform_completion_once
+    __gh_debug "Completion results: $__gh_perform_completion_once_result"
 
-    if test -z "$results"
+    if test -z "$__gh_perform_completion_once_result"
         __gh_debug "No completion, probably due to a failure"
         # Might as well do file completion, in case it helps
         return 1
     end
 
-    set -l directive (string sub --start 2 $results[-1])
-    set --global __gh_comp_results $results[1..-2]
+    set -l directive (string sub --start 2 $__gh_perform_completion_once_result[-1])
+    set --global __gh_comp_results $__gh_perform_completion_once_result[1..-2]
 
     __gh_debug "Completions are: $__gh_comp_results"
     __gh_debug "Directive is: $directive"
@@ -171,7 +225,11 @@ end
 # Remove any pre-existing completions for the program since we will be handling all of them.
 complete -c gh -e
 
+# this will get called after the two calls below and clear the $__gh_perform_completion_once_result global
+complete -c gh -n '__gh_clear_perform_completion_once_result'
 # The call to __gh_prepare_completions will setup __gh_comp_results
 # which provides the program's completion choices.
-complete -c gh -n '__gh_prepare_completions' -f -a '$__gh_comp_results'
-
+# If this doesn't require order preservation, we don't use the -k flag
+complete -c gh -n 'not __gh_requires_order_preservation && __gh_prepare_completions' -f -a '$__gh_comp_results'
+# otherwise we use the -k flag
+complete -k -c gh -n '__gh_requires_order_preservation && __gh_prepare_completions' -f -a '$__gh_comp_results'
