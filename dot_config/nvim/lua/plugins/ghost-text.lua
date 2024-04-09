3c25ef082a0dd19aaaac63a510c847e12f18720b
Yuto Tanaka <karaage_nennen@icloud.com>
2024-04-09 22:40:02 +0900

    fix(nvim)!: ghost-text plugin config

--- dot_config/nvim/lua/plugins/ghost-text.lua
+++ dot_config/nvim/lua/plugins/ghost-text.lua
@@ -3,13 +3,14 @@ local global = require("core.global")
 local is_human_rights = global.is_human_rights
 
 ---@type LazySpec
-local nvim_ghost = {
+local spec = {
     "subnut/nvim-ghost.nvim",
     --lazy = false,
     cmd = "GhostTextStart",
     --event = "VeryLazy",
-    config = function()
-        vim.g.nvim_ghost_server_port = 4001
+    init = function()
+        -- Set port number
+        --vim.g.nvim_ghost_server_port = 4001
         -- Start manually
         vim.g.nvim_ghost_autostart = 0
         -- Suppressing all messages
@@ -17,41 +18,7 @@ local nvim_ghost = {
         -- Disable the plugin
         --vim.g.nvim_ghost_disabled = 1
     end,
-    --cond = false,
-}
-
----@type LazySpec
-local dps_ghosttext = {
-    "gamoutatsumi/dps-ghosttext.vim",
-    --lazy = false,
-    cmd = "GhostStart",
-    --event = "VeryLazy",
-    dependencies = {
-        "vim-denops/denops.vim",
-        "yuki-yano/denops-lazy.nvim",
-    },
-    config = function()
-        local denops_lazy = require("denops-lazy")
-        denops_lazy.load("dps-ghosttext.vim")
-
-        --[[
-        g:dps_ghosttext#ftmap
-            Dictionary of filetype mappings.
-            Normally, key is domain name.
-            (e.g. If in "www.github.com" so "github.com") Value is generally vim filetypes.
-            (e.g. "markdown")
-            Default: {"github.com": "markdown"}
-        ]]
-        vim.g["dps_ghosttext#disable_defaultmap"] = 0
-        vim.g["dps_ghosttext#enable_autostart"] = 0
-    end,
     cond = is_human_rights,
 }
 
----@type LazySpec[]
-local specs = {
-    nvim_ghost,
-    dps_ghosttext,
-}
-
-return specs
+return spec
