1f2d1c0b51a28267b4336d33563b43f8d4b8af78
Yuto Tanaka <karaage_nennen@icloud.com>
2024-04-16 19:22:29 +0900

    chore(utilities): update Invoke-RunAfterChezmoiApply.ps1

--- utilities/Invoke-RunAfterChezmoiApply.ps1
+++ utilities/Invoke-RunAfterChezmoiApply.ps1
@@ -1,17 +1,28 @@
 ﻿# Run after chezmoi apply
 function Invoke-RunAfterChezmoiApply() {
     Write-Output "post-apply-hook"
+
+    Write-Output "Copy PowerShell profiles"
     $base_profile = Join-Path $env:USERPROFILE -ChildPath ".config\powershell\Microsoft.PowerShell_profile.ps1"
     $home_pwsh_profile = Join-Path $env:USERPROFILE -ChildPath "OneDrive\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
     $home_powershell_profile = Join-Path $env:USERPROFILE -ChildPath "OneDrive\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
     $work_pwsh_profile = Join-Path $env:USERPROFILE -ChildPath "Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
     $work_powershell_profile = Join-Path $env:USERPROFILE -ChildPath "Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
 
-    Write-Output "Copy PowerShell profiles"
     Copy-Item -Path $base_profile -Destination $home_pwsh_profile
     Copy-Item -Path $base_profile -Destination $home_powershell_profile
     Copy-Item -Path $base_profile -Destination $work_pwsh_profile
     Copy-Item -Path $base_profile -Destination $work_powershell_profile
+
+    Write-Output "Copy vim (paleovim) configuration"
+    $windows_vim_config = Join-Path -Path $env:USERPROFILE -ChildPath "vimfiles"
+    $home_vim_config = Join-Path -Path $env:USERPROFILE -ChildPath ".config\vim"
+    $work_vim_config = Join-Path -Path $env:USERPROFILE -ChildPath ".vim"
+    if ($env:COMPUTERNAME -eq "TANAKAPC") {
+        Copy-Item -Path $work_vim_config -Destination $windows_vim_config -Recurse
+    } else {
+        Copy-Item -Path $home_vim_config -Destination $windows_vim_config -Recurse
+    }
 }
 
 Invoke-RunAfterChezmoiApply
