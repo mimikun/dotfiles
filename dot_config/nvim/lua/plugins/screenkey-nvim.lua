eae7438becbc41b7d32dc6b4524586c8005bc198
Yuto Tanaka <karaage_nennen@icloud.com>
2024-04-18 21:15:41 +0900

    feat(nvim)!: add screenkey.nvim

---
+++ dot_config/nvim/lua/plugins/screenkey-nvim.lua
@@ -0,0 +1,36 @@
+---@type table
+local opts = {
+    -- see :h nvim_open_win
+    win_opts = {
+        relative = "editor",
+        anchor = "SE",
+        width = 40,
+        height = 3,
+        border = "single",
+    },
+    -- compress input when repeated <compress_after> times
+    compress_after = 3,
+    -- clear the input after <clear_after> seconds of inactivity
+    clear_after = 3,
+    -- temporarily disable screenkey (for example when inside of the terminal)
+    disable = {
+        -- for example: "toggleterm"
+        filetypes = { "toggleterm" },
+        -- :h 'buftype'
+        -- for example: "terminal"
+        buftypes = { "terminal" },
+    },
+}
+
+---@type LazySpec
+local spec = {
+    "NStefan002/screenkey.nvim",
+    --lazy = false,
+    cmd = "Screenkey",
+    --event = "VeryLazy",
+    opts = opts,
+    config = true,
+    --cond = false,
+}
+
+return spec
