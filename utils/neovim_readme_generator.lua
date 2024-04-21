abb24bf3e35ee4000913134929e828d380c8aff6
Yuto Tanaka <karaage_nennen@icloud.com>
2024-04-21 22:04:16 +0900

    chore(utils): update neovim_readme_generator.lua

--- utils/neovim_readme_generator.lua
+++ utils/neovim_readme_generator.lua
@@ -1,37 +1,132 @@
---thx: dotfyle-metadata.nvim
 local uv = vim.uv
 
-local data = {}
+local readme = {}
 
-local install_plugins = function()
-    local plugins = {}
+local lazy_plugins = require("lazy").plugins()
 
-    for _, plugin in pairs(require("lazy").plugins()) do
+-- Get markdown reference links
+---@return table
+local markdown_reflinks = function()
+    local data = {}
+
+    for _, plugin in pairs(lazy_plugins) do
         local name = plugin.name
-        local url = plugin.url
-        local deps = plugin.dependencies
-        local repo = string.format("- [%s](%s)", name, url)
-        table.insert(data, repo)
-        if deps then
-            table.insert(data, "dependencies")
-            for _, d_plugin in pairs(deps) do
-                table.insert(data, d_plugin)
+        local url = plugin.url:gsub(".git$", "")
+
+        table.insert(data, string.format("[%s]:%s", name, url))
+    end
+
+    table.sort(data)
+
+    return data
+end
+
+--- Get Plugin lists
+---@return table
+local plugin_full_list = function()
+    local data = {}
+
+    for _, plugin in pairs(lazy_plugins) do
+        local name = plugin.name
+        local dependencies = plugin.dependencies
+        table.insert(data, string.format("- [%s]", name))
+        if dependencies then
+            for _, dp in pairs(dependencies) do
+                table.insert(data, string.format("    - [%s]", dp))
             end
         end
     end
 
-    table.sort(plugins)
-    return plugins
+    return data
 end
 
-table.insert(data, "## Plugins")
-for _, value in pairs(install_plugins()) do
-    table.insert(data, string.format("- %s", value))
+---Get lsp servers
+---@return table
+local lsp_servers = function()
+    local lspconfig = require("lspconfig")
+    local servers = lspconfig.util.available_servers()
+
+    table.sort(servers)
+
+    return servers
 end
+
+---Get the installed treesitter parsers by nvim-treesitter
+---@return table
+local treesitter_parsers = function()
+    local ts = require("nvim-treesitter.configs")
+    local parsers = ts.get_ensure_installed_parsers()
+
+    table.sort(parsers)
+
+    return parsers
+end
+
+---Get the installed lsp servers and tools by mason.nvim
+---@return table
+local mason_tools = function()
+    local mason = require("mason")
+
+    local install_dir = string.format("%s/bin", vim.env.MASON)
+
+    if vim.fn.isdirectory(install_dir) == 0 then
+        return {}
+    end
+
+    local files
+    files = vim.split(vim.fn.globpath(install_dir, "*"), "\n", {})
+    files = vim.tbl_map(function(file)
+        return vim.fn.fnamemodify(file, ":t")
+    end, files)
+
+    table.sort(files)
+
+    return files
+end
+
+table.insert(readme, "# My neovim configuration")
+table.insert(readme, "")
+table.insert(readme, "## Plugins")
+table.insert(readme, "")
+table.insert(readme, "手動で配置してください")
+table.insert(readme, "")
+table.insert(readme, table.concat(plugin_full_list(), "\n"))
+table.insert(readme, "")
+table.insert(readme, "### Denops Plugins")
+table.insert(readme, "")
+table.insert(readme, "Disable when [human rights] are violated.")
+table.insert(readme, "[denops.vim] needs [human rights].")
+table.insert(readme, "")
+table.insert(readme, "手動で配置してください")
+table.insert(readme, "")
+table.insert(readme, "### Vim Script Plugins")
+table.insert(readme, "")
+table.insert(readme, "手動で配置してください")
+table.insert(readme, "")
+table.insert(readme, "#### Colorscheme")
+table.insert(readme, "")
+table.insert(readme, "手動で配置してください")
+table.insert(readme, "")
+table.insert(readme, "### Lua Plugins")
+table.insert(readme, "")
+table.insert(readme, "手動で配置してください")
+table.insert(readme, "")
+table.insert(readme, "#### Colorscheme")
+table.insert(readme, "")
+table.insert(readme, "手動で配置してください")
+table.insert(readme, "")
+table.insert(readme, "## nvim-treesitter parsers")
+table.insert(readme, "")
+table.insert(readme, "## Mason managed tools")
+table.insert(readme, "")
+table.insert(readme, "## LSP servers")
+table.insert(readme, "")
+-- reference links
+table.insert(readme, "[human rights]:https://github.com/mimikun/dotfiles/blob/master/docs/GLOSSARY.md#human-rights")
+table.insert(readme, table.concat(markdown_reflinks(), "\n"))
 
 -- OUTPUT
-
-local metadata = table.concat(data, "\n")
+local metadata = table.concat(readme, "\n")
 
 local readme_path = string.format("%s/PLUG.md", vim.fn.stdpath("config"))
 local fd = assert(uv.fs_open(readme_path, "w", 438))
