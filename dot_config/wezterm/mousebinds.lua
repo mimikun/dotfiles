65ccaf982d8d327dce0b3678f2fd098866f7097d
Yuto Tanaka <karaage_nennen@icloud.com>
2024-04-23 19:25:18 +0900

    chore(wezterm): improved mousebinds.lua

--- dot_config/wezterm/mousebinds.lua
+++ dot_config/wezterm/mousebinds.lua
@@ -2,11 +2,5 @@ local wezterm = require("wezterm")
 local action = wezterm.action
 
 return {
-    mouse_bindings = {
-        {
-            event = { Down = { streak = 1, button = "Left" } },
-            mods = "NONE",
-            action = action.EmitEvent("show-title-bar"),
-        },
-    },
+    mouse_bindings = {},
 }
