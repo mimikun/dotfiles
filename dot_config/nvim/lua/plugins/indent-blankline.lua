84554550dfff46588727c2cf4d7237b820f5813e
Yuto Tanaka <karaage_nennen@icloud.com>
2024-04-24 16:20:19 +0900

    feat(nvim)!: add indent-blankline.nvim plugin

---
+++ dot_config/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,15 @@
+---@type LazySpec
+local spec = {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    lazy = false,
+    --cmd = "CMDNAME",
+    --keys = "",
+    --event = "VeryLazy",
+    --dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {},
+    --config = true,
+    --cond = false,
+}
+
+return spec
