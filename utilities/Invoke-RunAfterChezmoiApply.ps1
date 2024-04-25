8d40959af9700515db94e7053f1ed3ee7ce36ab3
Yuto Tanaka <karaage_nennen@icloud.com>
2024-04-25 18:37:13 +0900

    fix(windows)!: copy process for pvim config

--- utilities/Invoke-RunAfterChezmoiApply.ps1
+++ utilities/Invoke-RunAfterChezmoiApply.ps1
@@ -2,6 +2,10 @@
 function Invoke-RunAfterChezmoiApply() {
     Write-Output "post-apply-hook"
 
+    ############################
+    # Copy PowerShell profiles #
+    ############################
+
     Write-Output "Copy PowerShell profiles"
     $base_profile = Join-Path $env:USERPROFILE -ChildPath ".config\powershell\Microsoft.PowerShell_profile.ps1"
     $home_pwsh_profile = Join-Path $env:USERPROFILE -ChildPath "OneDrive\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
@@ -14,14 +18,25 @@ function Invoke-RunAfterChezmoiApply() {
     Copy-Item -Path $base_profile -Destination $work_pwsh_profile
     Copy-Item -Path $base_profile -Destination $work_powershell_profile
 
+    #####################################
+    # Copy vim (paleovim) configuration #
+    #####################################
+
     Write-Output "Copy vim (paleovim) configuration"
-    $windows_vim_config = Join-Path -Path $env:USERPROFILE -ChildPath "vimfiles"
-    $home_vim_config = Join-Path -Path $env:USERPROFILE -ChildPath ".config\vim"
-    $work_vim_config = Join-Path -Path $env:USERPROFILE -ChildPath ".vim"
+    $windows_pvim_config = Join-Path -Path $env:USERPROFILE -ChildPath "vimfiles\"
+    $home_pvim_config = Join-Path -Path $env:CHEZMOI_DIR -ChildPath "dot_config\vim\*"
+    $work_pvim_config = Join-Path -Path $env:CHEZMOI_DIR -ChildPath "dot_vim\*"
+
+    # folder exist check
+    if (-not (Test-Path -Path $windows_pvim_config)) {
+        # mkdir $windows_pvim_config
+        New-Item -Path $windows_pvim_config -ItemType "directory" > $null
+    }
+
     if ($env:COMPUTERNAME -eq "TANAKAPC") {
-        Copy-Item -Path $work_vim_config -Destination $windows_vim_config -Recurse
+        Copy-Item -Path $work_pvim_config -Destination $windows_pvim_config -Recurse -Force
     } else {
-        Copy-Item -Path $home_vim_config -Destination $windows_vim_config -Recurse
+        Copy-Item -Path $home_pvim_config -Destination $windows_pvim_config -Recurse -Force
     }
 }
 
