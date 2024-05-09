9de6cf38346c22fad68e2ceab093dc8e0a3a8522
Yuto Tanaka <karaage_nennen@icloud.com>
2024-05-09 19:21:28 +0900

    feat(nvim-mini)!: add plugin rainbowhxch/accelerated-jk.nvim

--- dot_config/nvim-mini/init.lua
+++ dot_config/nvim-mini/init.lua
@@ -460,6 +460,13 @@ require("jetpack.packer").add({
             oil_git_status.setup({})
         end,
     },
+    {
+        "rainbowhxch/accelerated-jk.nvim",
+        config = function()
+            vim.keymap.set("n", "j", "<Plug>(accelerated_jk_gj)", {})
+            vim.keymap.set("n", "k", "<Plug>(accelerated_jk_gk)", {})
+        end,
+    },
 })
 
 -- Automatic plugin installation on startup
