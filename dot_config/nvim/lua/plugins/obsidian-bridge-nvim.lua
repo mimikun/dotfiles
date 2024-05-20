c7fc5f801066ec04bd92e060e8a2b913f03fe27d
Yuto Tanaka <karaage_nennen@icloud.com>
2024-05-20 22:36:44 +0900

    feat(nvim)!: add obsidian-bridge.nvim

---
+++ dot_config/nvim/lua/plugins/obsidian-bridge-nvim.lua
@@ -0,0 +1,37 @@
+---@type table
+local dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+}
+
+---@type table
+local cmds = {
+    "ObsidianBridgeDailyNote",
+    "ObsidianBridgeOpenGraph",
+    "ObsidianBridgeOpenVaultMenu",
+    "ObsidianBridgeTelescopeCommand",
+}
+
+local events = {
+    "BufReadPre *.md",
+    "BufNewFile *.md",
+}
+
+---@type LazySpec
+local spec = {
+    "oflisback/obsidian-bridge.nvim",
+    --lazy = false,
+    cmd = cmds,
+    event = events,
+    dependencies = dependencies,
+    opts = {
+        obsidian_server_address = "http://localhost:27123",
+        -- See "Sync of buffer scrolling" section below
+        scroll_sync = false,
+    },
+    --config = true,
+    --config = function() require("obsidian-bridge").setup({}) end,
+    --cond = false,
+}
+
+return spec
