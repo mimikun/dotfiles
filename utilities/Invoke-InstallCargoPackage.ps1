c08b9e2836c8fc1cf312523e25bf419b3e9aeea1
Yuto Tanaka <karaage_nennen@icloud.com>
2024-05-30 20:15:05 +0900

    feat!: add Invoke-InstallCargoPackage.ps1

---
+++ utilities/Invoke-InstallCargoPackage.ps1
@@ -0,0 +1,23 @@
+﻿function Invoke-InstallCargoPackage {
+    function Invoke-ExistsCmd() {
+        Param($cmdName)
+        Get-Command -Name $cmdName > $null 2>&1
+        $result = $?
+        return $result
+    }
+
+    $tmp = Join-Path -Path $env:USERPROFILE -ChildPath ".mimikun-pkglists"
+    $pkglist = Join-Path -Path $tmp -ChildPath "windows_cargo_packages.txt"
+
+    Get-Content -Path $pkglist |
+    ForEach-Object {
+        $pkg = $_
+        $cond = Invoke-ExistsCmd $pkg
+        if (-not $cond) {
+            Write-Output "$pkg is not found"
+            cargo install $pkg
+        }
+    }
+}
+
+Set-Alias -Name install_cargo_package -Value Invoke-InstallCargoPackage
