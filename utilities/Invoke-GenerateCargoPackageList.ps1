a8c251b5af825e79fc79ef3916a4528aa955cbad
Yuto Tanaka <karaage_nennen@icloud.com>
2024-05-30 20:14:40 +0900

    feat!: add Invoke-GenerateCargoPackageList.ps1

---
+++ utilities/Invoke-GenerateCargoPackageList.ps1
@@ -0,0 +1,12 @@
+﻿$tmp = Join-Path -Path $env:USERPROFILE -ChildPath ".mimikun-pkglists"
+$filepath = Join-Path -Path $tmp -ChildPath "windows_cargo_packages.txt"
+
+function Invoke-GenerateCargoPackageList() {
+    cargo install-update --list |
+    Select-Object -Skip 3 |
+    ForEach-Object { $_.Split(" ")[0] } |
+    Where-Object { $_ -ne "" } |
+    Out-File -FilePath $filepath -Encoding utf8
+}
+
+Set-Alias -Name generate_cargo_package_list -Value Invoke-GenerateCargoPackageList
