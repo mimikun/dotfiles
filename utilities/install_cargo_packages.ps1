739ac2dcc6ec5029db9018b74ed91e76bff4001b
Yuto Tanaka <karaage_nennen@icloud.com>
2024-05-30 20:03:01 +0900

    chore(utilities): update some windows utilities

--- utilities/install_cargo_packages.ps1
+++
@@ -1,9 +0,0 @@
-Write-Output "Please edit this PowerShell script file!!!"
-pause
-
-$file = "\path\to\cargo_packages.txt"
-
-foreach ($crate in Get-Content $file) {
-    cargo install $crate
-    Start-Sleep -Seconds 5
-}
