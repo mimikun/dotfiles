94d27b88c945643dd1451d720f97219b5ec898c8
Yuto Tanaka <karaage_nennen@icloud.com>
2024-06-06 21:32:13 +0900

    chore(nvim): update tree-sitter configs

---
+++ dot_config/nvim/lua/plugins/treesitter/init.lua
@@ -0,0 +1,10 @@
+---@type table
+local specs = {
+    { import = "plugins.treesitter.tree-sitter" },
+    { import = "plugins.treesitter.treesj" },
+    { import = "plugins.treesitter.ts-comments-nvim" },
+    { import = "plugins.treesitter.render-markdown" },
+    { import = "plugins.treesitter.orgmode" },
+}
+
+return specs
