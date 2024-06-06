bd60aa9c152a693340f1e6ae7ef86f306109611f
Yuto Tanaka <karaage_nennen@icloud.com>
2024-06-06 21:16:16 +0900

    chore(nvim): update lsp configs

---
+++ dot_config/nvim/lua/plugins/lsp/init.lua
@@ -0,0 +1,9 @@
+---@type table
+local specs = {
+    { import = "plugins.lsp.garbage-day" },
+    { import = "plugins.lsp.lspsaga" },
+    { import = "plugins.lsp.mason" },
+    { import = "plugins.lsp.nvim-lspconfig" },
+}
+
+return specs
