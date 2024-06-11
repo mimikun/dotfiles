9732e4c891eac7f48da3b0697a830cc09e0cc481
Yuto Tanaka <karaage_nennen@icloud.com>
2024-06-11 20:17:26 +0900

    feat(nvim)!: add new plugin: ivy.nvim

---
+++ dot_config/nvim/lua/plugins/ivy-nvim.lua
@@ -0,0 +1,27 @@
+---@type table
+local keys = {
+    { "<leader>p", desc = "Find files in your project with a custom rust file finder" },
+    { "<leader>/", desc = "Find content in files using the silver searcher" },
+    { "<leader>b", desc = "Search though open buffers" },
+}
+
+---@type table
+local cmds = {
+    "IvyFd",
+    "IvyAg",
+    "IvyBuffers",
+    "IvyLines",
+    "IvyWorkspaceSymbol",
+}
+
+---@type LazySpec
+local spec = {
+    "AdeAttwood/ivy.nvim",
+    lazy = false,
+    build = "cargo build --release",
+    cmd = cmds,
+    keys = keys,
+    --cond = false,
+}
+
+return spec
