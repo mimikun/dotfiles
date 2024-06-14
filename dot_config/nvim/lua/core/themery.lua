ac255f3ab82e316acc16508e99d0ac936b9fc331
Yuto Tanaka <karaage_nennen@icloud.com>
2024-06-14 22:39:33 +0900

    feat(nvim)!: use github_dark_dimmed colorscheme

--- dot_config/nvim/lua/core/themery.lua
+++ dot_config/nvim/lua/core/themery.lua
@@ -1,5 +1,5 @@
 -- Themery block
 -- This block will be replaced by Themery.
-vim.cmd("colorscheme github_dark")
-vim.g.theme_id = 1
+vim.cmd("colorscheme github_dark_dimmed")
+vim.g.theme_id = 4
 -- end themery block
