c4fa174c914824419501da533354a1b4a4dbcd05
Yuto Tanaka <karaage_nennen@icloud.com>
2024-06-14 23:13:00 +0900

    chore(nvim): split lua-ls lsp  config

--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -47,32 +47,6 @@ local spec = {
         require("neoconf").setup({})
         require("neodev").setup({})
 
-        ---@param names string[]
-        ---@return string[]
-        local function get_plugin_paths(names)
-            local plugins = require("lazy.core.config").plugins
-            local paths = {}
-            for _, name in ipairs(names) do
-                if plugins[name] then
-                    table.insert(paths, plugins[name].dir .. "/lua")
-                else
-                    vim.notify("Invalid plugin name" .. name)
-                end
-            end
-            return paths
-        end
-
-        ---@param plugins string[]
-        ---@return string[]
-        local function library(plugins)
-            local paths = get_plugin_paths(plugins)
-            table.insert(paths, vim.fn.stdpath("config") .. "/lua")
-            table.insert(paths, vim.env.VIMRUNTIME .. "/lua")
-            table.insert(paths, "${3rd}/luv/library")
-            table.insert(paths, "${3rd}/busted/library")
-            table.insert(paths, "${3rd}/luassert/library")
-            return paths
-        end
 
         local handlers = {
             function(server_name)
@@ -82,29 +56,7 @@ local spec = {
             end,
 
             ["lua_ls"] = function()
-                lspconfig.lua_ls.setup({
-                    settings = {
-                        Lua = {
-                            --diagnostics = {},
-                            format = {
-                                -- Use stylua
-                                enable = false,
-                            },
-                            runtime = {
-                                version = "LuaJIT",
-                                pathStrict = true,
-                                path = { "?.lua", "?/init.lua" },
-                            },
-                            semantic = {
-                                enable = false,
-                            },
-                            workspace = {
-                                checkThirdParty = "Disable",
-                                library = library({ "lazy.nvim" }),
-                            },
-                        },
-                    },
-                })
+                lspconfig.lua_ls.setup(require("plugins.lsp.lua-ls"))
             end,
         }
 
