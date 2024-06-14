1df656975dc277069eb1539d5afddcc403d2ea2d
Yuto Tanaka <karaage_nennen@icloud.com>
2024-06-14 22:39:33 +0900

    fix(nvim): sqlite3.lua config

--- dot_config/nvim/lua/plugins/sqlite.lua
+++ dot_config/nvim/lua/plugins/sqlite.lua
@@ -1,10 +1,12 @@
+local global = require("core.global")
+
 ---@type LazySpec
 local spec = {
     "kkharji/sqlite.lua",
     --lazy = false,
     config = function()
-        if require("core.global").is_windows then
-            vim.g.sqlite_clib_path = table.concat({ home, "utilities", "sqlite3.dll" }, "/")
+        if global.is_windows then
+            vim.g.sqlite_clib_path = table.concat({ global.home, "utilities", "sqlite3.dll" }, "/")
         end
     end,
     --cond = false,
