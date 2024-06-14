7ebb6f53df985c74179840474c6413e0cb59b654
Yuto Tanaka <karaage_nennen@icloud.com>
2024-06-14 23:14:20 +0900

    chore(nvim): split fish-lsp  config

--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -60,36 +60,8 @@ local spec = {
             end,
         }
 
-        ---@type table
-        local fish_lsp_root_files = {
-            "config.fish",
-            "$HOME/.config/fish",
-            "/usr/share/fish",
-        }
-
-        -- TODO: fish-lsp
-        -- NOTE: about :h lspconfig-setup
-        -- NOTE: ref: https://github.com/ndonfris/fish-lsp/issues/22
         if not configs.fish_lsp then
-            configs.fish_lsp = {
-                default_config = {
-                    root_dir = function(fname)
-                        local root_files = lspconfig.util.root_pattern(unpack(fish_lsp_root_files))(fname)
-                        return root_files
-                    end,
-                    name = "fish-lsp",
-                    filetypes = { "fish" },
-                    autostart = true,
-                    single_file_support = true,
-                    --on_new_config = function(new_config, new_root_dir) end,
-                    --capabilities = {},
-                    cmd = { "fish-lsp", "start" },
-                    --handlers = {},
-                    --init_options = {},
-                    --on_attach = function(client, bufnr)end,
-                    settings = {},
-                },
-            }
+            configs.fish_lsp = require("plugins.lsp.fish-lsp")
         end
 
         -- TODO: aiscript-lsp
