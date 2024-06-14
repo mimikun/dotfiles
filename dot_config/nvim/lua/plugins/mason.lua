45395c7d4f53ba0137589d1c76af0571eda912d5
Yuto Tanaka <karaage_nennen@icloud.com>
2024-06-14 23:15:19 +0900

    chore(nvim): split aiscript-lsp config

--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -64,25 +64,8 @@ local spec = {
             configs.fish_lsp = require("plugins.lsp.fish-lsp")
         end
 
-        -- TODO: aiscript-lsp
-        -- NOTE: about :h lspconfig-setup
         if not configs.aiscript_lsp then
-            configs.aiscript_lsp = {
-                default_config = {
-                    root_dir = lspconfig.util.root_pattern("*.is", "*.ais"),
-                    name = "aiscript-lsp",
-                    filetypes = { "is", "ais" },
-                    autostart = true,
-                    single_file_support = true,
-                    --on_new_config = function(new_config, new_root_dir) end,
-                    --capabilities = {},
-                    cmd = { "aiscript-languageserver", "--stdio" },
-                    --handlers = {},
-                    --init_options = {},
-                    --on_attach = function(client, bufnr)end,
-                    settings = {},
-                },
-            }
+            configs.aiscript_lsp = require("plugins.lsp.aiscript-lsp")
         end
 
         require("mason").setup({
