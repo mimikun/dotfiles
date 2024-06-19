92c3ac289e72d21a503448f4848f67736aead73e
Yuto Tanaka <karaage_nennen@icloud.com>
2024-06-19 22:29:48 +0900

    feat(nvim)!: add new plugin: package-info.nvim

---
+++ dot_config/nvim/lua/plugins/package-info-nvim.lua
@@ -0,0 +1,36 @@
+-- Can be `npm`, `yarn`, or `pnpm`
+---@type string
+local use_pm = "pnpm"
+
+---@type table
+local opts = {
+    colors = {
+        up_to_date = "#3C4048",
+        outdated = "#d19a66",
+    },
+    icons = {
+        enable = true,
+        style = {
+            -- TODO: Use utils/icons.lua
+            up_to_date = "|  ",
+            -- TODO: Use utils/icons.lua
+            outdated = "|  ",
+        },
+    },
+    autostart = true,
+    package_manager = use_pm,
+    hide_up_to_date = false,
+    hide_unstable_versions = false,
+}
+
+---@type LazySpec
+local spec = {
+    "vuki656/package-info.nvim",
+    --lazy = false,
+    event = { "BufRead package.json" },
+    dependencies = { "MunifTanjim/nui.nvim" },
+    opts = opts,
+    --cond = false,
+}
+
+return spec
