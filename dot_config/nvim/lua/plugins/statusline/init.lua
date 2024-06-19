6a345c607a390cce08ecee69074aabf86ed709c3
Yuto Tanaka <karaage_nennen@icloud.com>
2024-06-19 22:29:48 +0900

    feat(nvim)!: add new plugin: windline.nvim

--- dot_config/nvim/lua/plugins/statusline/init.lua
+++ dot_config/nvim/lua/plugins/statusline/init.lua
@@ -2,6 +2,7 @@ local specs = {
     { import = "plugins.statusline.lualine" },
     { import = "plugins.statusline.bufferline" },
     { import = "plugins.statusline.staline-nvim" },
+    { import = "plugins.statusline.windline-nvim" },
 }
 
 return specs
---
+++ dot_config/nvim/lua/plugins/statusline/windline-nvim.lua
@@ -0,0 +1,15 @@
+---@type table
+local events = { "BufReadPre", "BufNewFile" }
+
+---@type LazySpec
+local spec = {
+    "windwp/windline.nvim",
+    --lazy = false,
+    event = events,
+    config = function()
+        require("wlsample.vscode")
+    end,
+    --cond = false,
+}
+
+return spec
