d73d502b82380bced185a0c210e208341420c35e
Yuto Tanaka <karaage_nennen@icloud.com>
2024-06-20 22:23:20 +0900

    chore(nvim): improved spec-template.nvim

--- dot_config/nvim/lua/plugins/spec-template.lua
+++ dot_config/nvim/lua/plugins/spec-template.lua
@@ -1,15 +1,34 @@
 ---@type LazySpec
 local spec = {
     "mimikun/spec-template",
+    --dir = ""
+    --url = ""
+    --name = ""
+    --dev = false
+    --build = "",
+    --branch = "",
+    --tag = "",
+    --commit = "",
+    --version = "",
     --lazy = false,
     enabled = false,
+    --ft = "",
     --cmd = "CMDNAME",
     --keys = "",
     --event = "VeryLazy",
     --dependencies = { "nvim-tree/nvim-web-devicons" },
+    --init = function()
+    --    INIT
+    --end,
     --opts = {},
     --config = true,
     cond = false,
+    --main = ""
+    --pin = false,
+    --submodules = false,
+    --module = false,
+    --priority = 1000,
+    --optional = false,
 }
 
 return spec
