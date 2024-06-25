3116783bad643947820b41fa1b4dbf5fd3c890eb
Yuto Tanaka <karaage_nennen@icloud.com>
2024-06-25 20:53:02 +0900

    feat(nvim)!: add new plugin: headlines.nvim

---
+++ dot_config/nvim/lua/plugins/headlines-nvim.lua
@@ -0,0 +1,10 @@
+---@type LazySpec
+local spec = {
+    "lukas-reineke/headlines.nvim",
+    --lazy = false,
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    opts = {},
+    --cond = false,
+}
+
+return spec
