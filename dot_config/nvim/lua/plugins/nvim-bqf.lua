69886a4c9faf67645a2eb79155b022b1b87055bf
Yuto Tanaka <karaage_nennen@icloud.com>
2024-07-03 22:45:25 +0900

    feat(nvim)!: add new plugin: nvim-bqf

---
+++ dot_config/nvim/lua/plugins/nvim-bqf.lua
@@ -0,0 +1,13 @@
+---@type LazySpec
+local spec = {
+    "kevinhwang91/nvim-bqf",
+    --lazy = false,
+    ft = "qf",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {},
+    --cond = false,
+}
+
+return spec
