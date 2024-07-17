4d1728276f3bb7072a132e250ab65c9b51798a2d
Yuto Tanaka <karaage_nennen@icloud.com>
2024-07-17 23:20:56 +0900

    feat(nvim)!: add new neovim plugin: lazysql.nvim

---
+++ dot_config/nvim/lua/plugins/lazysql-nvim.lua
@@ -0,0 +1,11 @@
+---@type LazySpec
+local spec = {
+    "LostbBlizzard/lazysql.nvim",
+    --lazy = false,
+    cmd = "LazySql",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    opt = {},
+    --cond = false,
+}
+
+return spec
