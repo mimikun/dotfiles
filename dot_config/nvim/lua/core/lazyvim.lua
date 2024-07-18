5ca3ce23562c92dd0f9901c97dab37d85ee57ef3
Yuto Tanaka <karaage_nennen@icloud.com>
2024-07-18 20:49:59 +0900

    feat(nvim): update lazy.nvim config

--- dot_config/nvim/lua/core/lazyvim.lua
+++ dot_config/nvim/lua/core/lazyvim.lua
@@ -34,90 +34,102 @@ local icons = {
 
 local Lazy = {}
 
-function Lazy:load_lazy()
-    -- Lazy.nvimでのプラグイン管理
-    if not vim.uv.fs_stat(lazy_path) then
-        vim.fn.system({
-            "git",
-            "clone",
-            "--filter=blob:none",
-            "https://github.com/folke/lazy.nvim.git",
-            "--branch=stable",
-            lazy_path,
-        })
-    end
-
-    ---@type table
-    local lazy_settings = {
-        root = lazy_root,
-        defaults = {
-            lazy = true,
-            cond = true,
-        },
-        concurrency = concurrency,
-        git = {
-            timeout = 300,
-        },
-        rocks = {
-            enabled = falase,
-        },
-        dev = {
-            path = table.concat({ global.home, "ghq", "github.com", "mimikun", "dev-plugins" }, global.path_sep),
-            fallback = true,
-        },
-        ui = {
-            -- a number <1 is a percentage., >1 is a fixed size
-            size = { width = 0.88, height = 0.8 },
-            -- wrap the lines in the ui
-            wrap = true,
-            -- The border to use for the UI window. Accepts same border values as |nvim_open_win()|.
-            border = "rounded",
-            icons = {
-                cmd = icons.misc.Code,
-                config = icons.ui.Gear,
-                event = icons.kind.Event,
-                -- NOTE: utils/icons.lua has not this icon
-                favorite = " ",
-                ft = icons.documents.Files,
-                init = icons.misc.ManUp,
-                import = icons.documents.Import,
-                keys = icons.ui.Keyboard,
-                -- NOTE: utils/icons.lua has not this icon
-                lazy = "󰒲 ",
-                loaded = icons.ui.Check,
-                not_loaded = icons.misc.Ghost,
-                plugin = icons.ui.Package,
-                runtime = icons.misc.Vim,
-                require = icons.cmp_sep.nvim_lua,
-                source = icons.kind.StaticMethod,
-                start = icons.ui.Play,
-                task = icons.ui_sep.Accepted,
-                list = {
-                    icons.ui_sep.BigCircle,
-                    icons.ui_sep.BigUnfilledCircle,
-                    icons.ui_sep.Square,
-                    icons.ui_sep.ChevronRight,
-                },
-            },
-        },
-        performance = {
-            cache = {
-                enabled = true,
-            },
-            -- reset the package path to improve startup time
-            reset_packpath = true,
-            rtp = {
-                -- reset the runtime path to $VIMRUNTIME and the config directory
-                reset = true,
-                -- add any custom paths here that you want to include in the rtp
-                ---@type string[]
-                paths = {},
-            },
-        },
-    }
-
-    vim.opt.rtp:prepend(lazy_path)
-    require("lazy").setup("plugins", lazy_settings)
+if not vim.uv.fs_stat(lazy_path) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazy_path,
+    })
 end
 
-Lazy:load_lazy()
+---@type table
+local lazy_settings = {
+    root = lazy_root,
+    defaults = {
+        lazy = true,
+        cond = true,
+    },
+    concurrency = concurrency,
+    git = {
+        timeout = 300,
+        -- rate of network related git operations (clone, fetch, checkout)
+        throttle = {
+            enabled = false, -- not enabled by default
+            -- max 2 ops every 5 seconds
+            rate = 2,
+            duration = 5 * 1000, -- in ms
+        },
+    },
+    rocks = {
+        enabled = falase,
+    },
+    dev = {
+        path = table.concat({ global.home, "ghq", "github.com", "mimikun", "dev-plugins" }, global.path_sep),
+        fallback = true,
+    },
+    install = {
+        colorscheme = { vim.cmd.colorscheme() },
+    },
+    ui = {
+        size = { width = 0.88, height = 0.8 },
+        wrap = true,
+        border = "rounded",
+        -- The backdrop opacity. 0 is fully opaque, 100 is fully transparent.
+        backdrop = 60,
+        icons = {
+            cmd = icons.misc.Code,
+            config = icons.ui.Gear,
+            event = icons.kind.Event,
+            -- NOTE: utils/icons.lua has not this icon
+            -- nf-md-star
+            favorite = " ",
+            ft = icons.documents.Files,
+            init = icons.misc.ManUp,
+            import = icons.documents.Import,
+            keys = icons.ui.Keyboard,
+            -- NOTE: utils/icons.lua has not this icon
+            -- nf-md-sleep
+            lazy = "󰒲 ",
+            loaded = icons.ui.Check,
+            not_loaded = icons.misc.Ghost,
+            plugin = icons.ui.Package,
+            runtime = icons.misc.Vim,
+            require = icons.cmp_sep.nvim_lua,
+            source = icons.kind.StaticMethod,
+            start = icons.ui.Play,
+            task = icons.ui_sep.Accepted,
+            list = {
+                icons.ui_sep.BigCircle,
+                icons.ui_sep.BigUnfilledCircle,
+                icons.ui_sep.Square,
+                icons.ui_sep.ChevronRight,
+            },
+        },
+    },
+    performance = {
+        cache = {
+            enabled = true,
+        },
+        reset_packpath = true,
+        rtp = {
+            reset = true,
+            paths = {},
+        },
+        disabled_plugins = {
+            -- "gzip",
+            -- "matchit",
+            -- "matchparen",
+            -- "netrwPlugin",
+            -- "tarPlugin",
+            -- "tohtml",
+            -- "tutor",
+            -- "zipPlugin",
+        },
+    },
+}
+
+vim.opt.rtp:prepend(lazy_path)
+require("lazy").setup("plugins", lazy_settings)
