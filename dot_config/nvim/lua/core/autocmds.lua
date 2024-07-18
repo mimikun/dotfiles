94e4563b359a90ddd2c371c9503221bcb471ca22
Yuto Tanaka <karaage_nennen@icloud.com>
2024-07-18 20:49:59 +0900

    perf(nvim): improved startup speed

--- dot_config/nvim/lua/core/autocmds.lua
+++ dot_config/nvim/lua/core/autocmds.lua
@@ -1,11 +1,3 @@
--- Show stdpaths
-vim.api.nvim_create_user_command("ShowStdPaths", function()
-    print(vim.fn.stdpath("cache"))
-    print(vim.fn.stdpath("config"))
-    print(vim.fn.stdpath("data"))
-    print(vim.fn.stdpath("state"))
-end, {})
-
 -- TODO:
 -- Conceal all Special Characters with 💩
 local function conceal_all_special_chars()
