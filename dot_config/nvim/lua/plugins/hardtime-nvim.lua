3ff4906f8031aac39675cf7cd4c4042d85403d5c
Yuto Tanaka <karaage_nennen@icloud.com>
2024-07-19 23:00:34 +0900

    feat(nvim): add new newovim plugin: hardtime.nvim

---
+++ dot_config/nvim/lua/plugins/hardtime-nvim.lua
@@ -0,0 +1,27 @@
+---@type table
+local dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+}
+
+---@type table
+local opts = {
+    disable_mouse = false,
+    hint = true,
+    notification = true,
+    allow_different_key = false,
+    enabled = true,
+    disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "oil" },
+}
+
+---@type LazySpec
+local spec = {
+    "m4xshen/hardtime.nvim",
+    --lazy = false,
+    cmd = "Hardtime",
+    dependencies = dependencies,
+    opt = opts,
+    --cond = false,
+}
+
+return spec
