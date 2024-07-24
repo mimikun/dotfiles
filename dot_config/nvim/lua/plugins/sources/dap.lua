74029f759d42887d264034e6d02dceda6370ee1c
Yuto Tanaka <karaage_nennen@icloud.com>
2024-07-24 20:47:38 +0900

    feat(nvim): add new dap adapter: python

--- dot_config/nvim/lua/plugins/sources/dap.lua
+++ dot_config/nvim/lua/plugins/sources/dap.lua
@@ -2,10 +2,28 @@ local M = {}
 
 ---@type table
 local base_adapters = {
-    -- TODO
+    "python",
+    --"cppdbg",
+    --"delve",
+    --"node2",
+    --"chrome",
+    --"firefox",
+    --"php",
+    --"coreclr",
+    --"js",
+    --"codelldb",
+    --"bash",
+    --"javadbg",
+    --"javatest",
+    --"mock",
+    --"puppet",
+    --"elixir",
+    --"kotlin",
+    --"dart",
+    --"haskell",
 }
 
--- NOTE: Include aaa
+-- NOTE: Include DAP_ADAPTER_NAME
 --[[
 if not require("core.global").is_windows then
     table.insert(base_adapters, "")
