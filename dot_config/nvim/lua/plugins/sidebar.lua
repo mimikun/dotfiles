0b218c2a482a51420b0042e812cb45412dff62f3
Yuto Tanaka <karaage_nennen@icloud.com>
2024-07-24 20:47:38 +0900

    feat(nvim): add new plugin: sections-dap
    sidebar.nvim extension

--- dot_config/nvim/lua/plugins/sidebar.lua
+++ dot_config/nvim/lua/plugins/sidebar.lua
@@ -9,29 +9,46 @@ local cmds = {
 }
 
 ---@type table
-local opts = {
-    open = false,
-    side = "right",
-    hide_statusline = false,
-    update_interval = 1000,
-    sections = { "datetime", "git", "diagnostics" },
-    section_separator = { "", "-----", "" },
-    section_title_separator = { "" },
-    containers = {
-        attach_shell = "/bin/sh",
-        show_all = true,
-        interval = 5000,
-    },
-    datetime = { format = "%a %b %d, %H:%M", clocks = { { name = "local" } } },
-    todos = { ignored_paths = { "~" } },
+local dependencies = {
+    "sidebar-nvim/sections-dap",
 }
 
 ---@type LazySpec
 local spec = {
     "sidebar-nvim/sidebar.nvim",
     --lazy = false,
+    dependencies = dependencies,
     cmd = cmds,
-    opts = opts,
+    config = function()
+        local sidebar = require("sidebar-nvim")
+        sidebar.setup({
+            open = false,
+            side = "right",
+            hide_statusline = false,
+            update_interval = 1000,
+            sections = {
+                "datetime",
+                "git",
+                "diagnostics",
+                require("dap-sidebar-nvim.breakpoints"),
+            },
+            section_separator = { "", "-----", "" },
+            section_title_separator = { "" },
+            containers = {
+                attach_shell = "/bin/sh",
+                show_all = true,
+                interval = 5000,
+            },
+            datetime = { format = "%a %b %d, %H:%M", clocks = { { name = "local" } } },
+            todos = { ignored_paths = { "~" } },
+            dap = {
+                breakpoints = {
+                    icon = "🔍",
+                },
+            },
+        }
+)
+    end,
     --cond = false,
 }
 
