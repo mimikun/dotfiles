552284c32696a5c7386cba09ce602498a554abd8
Yuto Tanaka <karaage_nennen@icloud.com>
2024-07-31 19:06:02 +0900

    chore(fish): update procs completion

--- dot_config/fish/completions/procs.fish
+++ dot_config/fish/completions/procs.fish
@@ -3,14 +3,14 @@ complete -c procs -s i -l insert -d 'Insert column to slot' -r
 complete -c procs -l only -d 'Specified column only' -r
 complete -c procs -l sorta -d 'Sort column by ascending' -r
 complete -c procs -l sortd -d 'Sort column by descending' -r
-complete -c procs -s c -l color -d 'Color mode' -r -f -a "{auto	'',always	'',disable	''}"
-complete -c procs -l theme -d 'Theme mode' -r -f -a "{auto	'',dark	'',light	''}"
-complete -c procs -s p -l pager -d 'Pager mode' -r -f -a "{auto	'',always	'',disable	''}"
+complete -c procs -s c -l color -d 'Color mode' -r -f -a "{auto\t'',always\t'',disable\t''}"
+complete -c procs -l theme -d 'Theme mode' -r -f -a "{auto\t'',dark\t'',light\t''}"
+complete -c procs -s p -l pager -d 'Pager mode' -r -f -a "{auto\t'',always\t'',disable\t''}"
 complete -c procs -l interval -d 'Interval to calculate throughput' -r
-complete -c procs -l use-config -d 'Use built-in configuration' -r -f -a "{default	'',large	''}"
+complete -c procs -l use-config -d 'Use built-in configuration' -r -f -a "{default\t'',large\t''}"
 complete -c procs -l load-config -d 'Load configuration from file' -r -F
-complete -c procs -l gen-completion -d 'Generate shell completion file' -r -f -a "{bash	'',elvish	'',fish	'',powershell	'',zsh	''}"
-complete -c procs -l gen-completion-out -d 'Generate shell completion file and write to stdout' -r -f -a "{bash	'',elvish	'',fish	'',powershell	'',zsh	''}"
+complete -c procs -l gen-completion -d 'Generate shell completion file' -r -f -a "{bash\t'',elvish\t'',fish\t'',powershell\t'',zsh\t''}"
+complete -c procs -l gen-completion-out -d 'Generate shell completion file and write to stdout' -r -f -a "{bash\t'',elvish\t'',fish\t'',powershell\t'',zsh\t''}"
 complete -c procs -s a -l and -d 'AND  logic for multi-keyword'
 complete -c procs -s o -l or -d 'OR   logic for multi-keyword'
 complete -c procs -s d -l nand -d 'NAND logic for multi-keyword'
