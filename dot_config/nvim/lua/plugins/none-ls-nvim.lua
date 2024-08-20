1de106f824eae3fdd8ec6530b7c39d2ea958e47e
Yuto Tanaka <karaage_nennen@icloud.com>
2024-08-20 19:12:43 +0900

    feat(nvim): add some none-ls.nvim sources

--- dot_config/nvim/lua/plugins/none-ls-nvim.lua
+++ dot_config/nvim/lua/plugins/none-ls-nvim.lua
@@ -1,10 +1,15 @@
+---@type table
+local opts = {
+    sources = require("plugins.configs.none-ls.sources").minimal,
+}
+
 ---@type LazySpec
 local spec = {
     "nvimtools/none-ls.nvim",
     --lazy = false,
     --event = "VeryLazy",
     dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {},
+    opts = opts,
     --cond = false,
     -- NOTE: null-ls replace plugin `pattern 2`
     --enabled = false,
