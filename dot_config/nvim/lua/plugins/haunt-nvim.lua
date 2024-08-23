d011418614611fa531f9aadf5956122d7dae6476
Yuto Tanaka <karaage_nennen@icloud.com>
2024-08-23 22:16:35 +0900

    chore(nvim): update haunt.nvim configs

--- dot_config/nvim/lua/plugins/haunt-nvim.lua
+++ dot_config/nvim/lua/plugins/haunt-nvim.lua
@@ -7,6 +7,16 @@ local cmds = {
     "HauntReset",
 }
 
+---@type table
+local opts = {
+    quit_help_with_q = true,
+    window = {
+        width_frac = 0.88,
+        height_frac = 0.88,
+        winblend = 15,
+    },
+}
+
 ---@type LazySpec
 local spec = {
     "adigitoleo/haunt.nvim",
@@ -14,7 +24,7 @@ local spec = {
     --url = "https://git.sr.ht/~adigitoleo/haunt.nvim",
     --lazy = false,
     cmd = cmds,
-    opts = {},
+    opts = opts,
     --cond = false,
 }
 
