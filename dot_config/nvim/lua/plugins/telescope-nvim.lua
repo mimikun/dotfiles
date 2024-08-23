4e5c77ed3933c1b348ef048f937c7b9086438a6f
Yuto Tanaka <karaage_nennen@icloud.com>
2024-08-23 22:27:41 +0900

    chore(nvim): update telescope.nvim configs
    add some extensions

--- dot_config/nvim/lua/plugins/telescope-nvim.lua
+++ dot_config/nvim/lua/plugins/telescope-nvim.lua
@@ -44,21 +44,21 @@ local dependencies = {
     "nvim-telescope/telescope-frecency.nvim",
     "danielfalk/smart-open.nvim",
     "rcarriga/nvim-notify",
-    --"ghassan0/telescope-glyph.nvim",
-    --"xiyaowong/telescope-emoji.nvim",
-    --"tsakirist/telescope-lazy.nvim",
-    --"fdschmidt93/telescope-egrepify.nvim",
-    --"nvim-telescope/telescope-file-browser.nvim",
-    --"vuki656/package-info.nvim",
-    --"tiagovla/scope.nvim",
-    --"nvim-lua/popup.nvim",
-    --"jvgrootveld/telescope-zoxide",
-    --"debugloop/telescope-undo.nvim",
-    --"epwalsh/pomo.nvim",
-    --"GianniBYoung/chezmoi-telescope.nvim",
-    --"roycrippen4/telescope-treesitter-info.nvim",
-    --"illia-shkroba/telescope-completion.nvim",
-    --"cljoly/telescope-repo.nvim",
+    "ghassan0/telescope-glyph.nvim",
+    "xiyaowong/telescope-emoji.nvim",
+    "tsakirist/telescope-lazy.nvim",
+    "fdschmidt93/telescope-egrepify.nvim",
+    "nvim-telescope/telescope-file-browser.nvim",
+    "vuki656/package-info.nvim",
+    "tiagovla/scope.nvim",
+    "nvim-lua/popup.nvim",
+    "jvgrootveld/telescope-zoxide",
+    "debugloop/telescope-undo.nvim",
+    "epwalsh/pomo.nvim",
+    "GianniBYoung/chezmoi-telescope.nvim",
+    "roycrippen4/telescope-treesitter-info.nvim",
+    "illia-shkroba/telescope-completion.nvim",
+    "cljoly/telescope-repo.nvim",
 }
 
 -- NOTE: Add fzf_sorter if not a Windows
@@ -68,18 +68,16 @@ if not is_windows then
         "nvim-telescope/telescope-fzf-native.nvim",
         build = "make",
     })
-    --[[
     table.insert(dependencies, {
         "dharmx/telescope-media.nvim",
     })
-    ]]
 end
 
 ---@type LazySpec
 local spec = {
     "nvim-telescope/telescope.nvim",
     --lazy = false,
-    --cmd = "CMDNAME",
+    cmd = "Telescope",
     keys = keys,
     --event = "VeryLazy",
     dependencies = dependencies,
@@ -108,25 +106,25 @@ local spec = {
         -- Very difficult to build fzf on Windows
         if not is_windows then
             telescope.load_extension("fzf")
-            --telescope.load_extension("media")
+            telescope.load_extension("media")
         end
         telescope.load_extension("frecency")
         telescope.load_extension("smart_open")
         telescope.load_extension("notify")
-        --telescope.load_extension("glyph")
-        --telescope.load_extension("emoji")
-        --telescope.load_extension("lazy")
-        --telescope.load_extension("file_browser")
-        --telescope.load_extension("egrepify")
-        --telescope.load_extension("package_info")
-        --telescope.load_extension("scope")
-        --telescope.load_extension("zoxide")
-        --telescope.load_extension("undo")
-        --telescope.load_extension("pomodori")
-        --telescope.load_extension("chezmoi")
-        --telescope.load_extension("treesitter_info")
-        --telescope.load_extension("completion")
-        --telescope.load_extension("repo")
+        telescope.load_extension("glyph")
+        telescope.load_extension("emoji")
+        telescope.load_extension("lazy")
+        telescope.load_extension("file_browser")
+        telescope.load_extension("egrepify")
+        telescope.load_extension("package_info")
+        telescope.load_extension("scope")
+        telescope.load_extension("zoxide")
+        telescope.load_extension("undo")
+        telescope.load_extension("pomodori")
+        telescope.load_extension("chezmoi")
+        telescope.load_extension("treesitter_info")
+        telescope.load_extension("completion")
+        telescope.load_extension("repo")
     end,
     --cond = false,
 }
