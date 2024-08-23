2fd130c22e251675287599b628a814af532ac76c
Yuto Tanaka <karaage_nennen@icloud.com>
2024-08-23 22:28:18 +0900

    chore(nvim): update human-rights.nvim config

--- dot_config/nvim/lua/plugins/human-rights-nvim.lua
+++ dot_config/nvim/lua/plugins/human-rights-nvim.lua
@@ -6,26 +6,29 @@ local cmds = {
     "ShowDisk",
 }
 
+---@type table
+local opts = {
+    cpu = {
+        "Ryzen 9 3900X",
+        "i5-13500H",
+    },
+    memory = {
+        -- 4GB
+        linux = 4294967296,
+        -- 9GB
+        windows = 9663676416,
+    },
+    available_parallelism = 14,
+    media_type = "SSD",
+}
+
 ---@type LazySpec
 local spec = {
     "mimikun/human-rights.nvim",
     dev = true,
     --lazy = false,
     cmd = cmds,
-    opts = {
-        cpu = {
-            "Ryzen 9 3900X",
-            "i5-13500H",
-        },
-        memory = {
-            -- 4GB
-            linux = 4294967296,
-            -- 9GB
-            windows = 9663676416,
-        },
-        available_parallelism = 14,
-        media_type = "SSD",
-    },
+    opts = opts,
     --cond = false,
 }
 
