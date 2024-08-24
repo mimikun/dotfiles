0e7311ffa7e08e459d5eb020e5db01e9f44c4613
Yuto Tanaka <karaage_nennen@icloud.com>
2024-08-24 15:54:44 +0900

    chore(nvim): sort need_servers.lua

--- dot_config/nvim/lua/plugins/configs/lspconfigs/need_servers.lua
+++ dot_config/nvim/lua/plugins/configs/lspconfigs/need_servers.lua
@@ -1,26 +1,26 @@
 ---@type table
 local need_servers = {
-    "lua_ls",
-    "marksman",
-    "efm",
-    "jsonls",
-    "typos_lsp",
     "bashls",
     "clangd",
-    "neocmake",
     "cssls",
     "denols",
-    "dockerls",
     "docker_compose_language_service",
+    "dockerls",
+    "efm",
     "eslint",
     "graphql",
     "html",
-    "tsserver",
+    "jsonls",
+    "lua_ls",
     "luau_lsp",
+    "marksman",
+    "neocmake",
     "powershell_es",
     "pyright",
     "rust_analyzer",
     "taplo",
+    "tsserver",
+    "typos_lsp",
     "vimls",
     "yamlls",
     "zls",
@@ -29,11 +29,11 @@ local need_servers = {
 -- NOTE: Include csharp_ls, gopls, jqls, esbonio, markdown_oxide
 if not require("config.global").is_windows then
     table.insert(need_servers, "csharp_ls")
+    table.insert(need_servers, "esbonio")
     table.insert(need_servers, "gopls")
     table.insert(need_servers, "jqls")
-    table.insert(need_servers, "esbonio")
-    table.insert(need_servers, "solargraph")
     table.insert(need_servers, "markdown_oxide")
+    table.insert(need_servers, "solargraph")
 end
 
 return need_servers
