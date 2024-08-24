fd501834a8efc9e7f04cf5e6a5fe4fc78a779147
Yuto Tanaka <karaage_nennen@icloud.com>
2024-08-24 23:11:58 +0900

    style(home-manager): run nixfmt

--- dot_config/home-manager/home.nix
+++ dot_config/home-manager/home.nix
@@ -19,8 +19,8 @@
   # Home Manager is pretty good at managing dotfiles.
   # The primary way to manage plain files is through 'home.file'.
   home.file = {
-  # Building this configuration will create a copy of 'dotfiles/screenrc' in the Nix store.
-  # Activating the configuration will then make '~/.screenrc' a symlink to the Nix store copy.
+    # Building this configuration will create a copy of 'dotfiles/screenrc' in the Nix store.
+    # Activating the configuration will then make '~/.screenrc' a symlink to the Nix store copy.
     #".screenrc".source = dotfiles/screenrc;
   };
 
@@ -34,9 +34,7 @@
   #
   #  /etc/profiles/per-user/mimikun/etc/profile.d/hm-session-vars.sh
   #
-  home.sessionVariables = {
-    EDITOR = "nvim";
-  };
+  home.sessionVariables = { EDITOR = "nvim"; };
 
   programs.home-manager.enable = true;
 }
