f6776560e27cb171057e1d544c9d67a1bcc75a27
Yuto Tanaka <karaage_nennen@icloud.com>
2024-08-25 23:11:08 +0900

    chore(nvim): disable silhouette.nvim
    denops error, f**k denops

--- dot_config/nvim/lua/plugins/silhouette-nvim.lua
+++ dot_config/nvim/lua/plugins/silhouette-nvim.lua
@@ -1,6 +1,8 @@
 -- Disable if there are no human rights
 ---@type boolean
 local is_human_rights = require("config.global").is_human_rights
+-- FIX: WORKAROUND
+is_human_rights = false
 
 ---@type LazySpec[]
 local dependencies = {
