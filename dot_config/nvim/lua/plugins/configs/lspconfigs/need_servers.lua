8f8107e8aeea3ec4214e12ab1a51ab2a0c11982b
Yuto Tanaka <karaage_nennen@icloud.com>
2024-08-26 19:39:37 +0900

    fix(nvim): remove nil_ls in windows

--- dot_config/nvim/lua/plugins/configs/lspconfigs/need_servers.lua
+++ dot_config/nvim/lua/plugins/configs/lspconfigs/need_servers.lua
@@ -15,7 +15,6 @@ local need_servers = {
     "luau_lsp",
     "marksman",
     "neocmake",
-    "nil_ls",
     "powershell_es",
     "pyright",
     "rust_analyzer",
@@ -27,13 +26,14 @@ local need_servers = {
     "zls",
 }
 
--- NOTE: Include csharp_ls, gopls, jqls, esbonio, markdown_oxide
+-- NOTE: Include some langservers
 if not require("config.global").is_windows then
     table.insert(need_servers, "csharp_ls")
     table.insert(need_servers, "esbonio")
     table.insert(need_servers, "gopls")
     table.insert(need_servers, "jqls")
     table.insert(need_servers, "markdown_oxide")
+    table.insert(need_servers, "nil_ls")
     table.insert(need_servers, "solargraph")
 end
 
