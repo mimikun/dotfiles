d4a8f98719a15b5b37fbc8c244ac0aeb56efabe1
Yuto Tanaka <karaage_nennen@icloud.com>
2024-08-31 11:00:05 +0900

    refactor(telescope-nvim): improved

---
+++ dot_config/nvim/lua/plugins/configs/telescope-nvim/dependencies.lua
@@ -0,0 +1,41 @@
+---@type LazySpec[]
+local dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-frecency.nvim",
+    "danielfalk/smart-open.nvim",
+    "rcarriga/nvim-notify",
+    "ghassan0/telescope-glyph.nvim",
+    "xiyaowong/telescope-emoji.nvim",
+    "tsakirist/telescope-lazy.nvim",
+    "fdschmidt93/telescope-egrepify.nvim",
+    "nvim-telescope/telescope-file-browser.nvim",
+    "vuki656/package-info.nvim",
+    "tiagovla/scope.nvim",
+    "nvim-lua/popup.nvim",
+    "jvgrootveld/telescope-zoxide",
+    "debugloop/telescope-undo.nvim",
+    "epwalsh/pomo.nvim",
+    "GianniBYoung/chezmoi-telescope.nvim",
+    "roycrippen4/telescope-treesitter-info.nvim",
+    "illia-shkroba/telescope-completion.nvim",
+    "cljoly/telescope-repo.nvim",
+    "cbochs/grapple.nvim",
+    "kdheepak/lazygit.nvim",
+    "folke/noice.nvim",
+    "nvimdev/template.nvim",
+}
+
+-- NOTE: Add fzf_sorter if not a Windows
+-- Very difficult to build fzf on Windows
+if not require("config.global").is_windows then
+    table.insert(dependencies, {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+    })
+    table.insert(dependencies, {
+        "dharmx/telescope-media.nvim",
+    })
+end
+
+return dependencies
--- dot_config/nvim/lua/plugins/configs/telescope-nvim/extensions.lua
+++ dot_config/nvim/lua/plugins/configs/telescope-nvim/extensions.lua
@@ -1,21 +1,18 @@
 local extensions = {
     frecency = require("plugins.configs.telescope-nvim.frecency"),
     smart_open = require("plugins.configs.telescope-nvim.smart-open"),
-    --glyph = require("plugins.configs.telescope-nvim.glyph"),
-    --emoji = require("plugins.configs.telescope-nvim.emoji"),
-    --lazy = require("plugins.configs.telescope-nvim.lazy"),
-    --egrepify = require("plugins.configs.telescope-nvim.egrepify"),
-    --file_browser = require("plugins.configs.telescope-nvim.file-browser"),
-    --package_info = require("plugins.configs.telescope-nvim.package-info"),
-    --zoxide = require("plugins.configs.telescope-nvim.zoxide"),
-    --undo = require("plugins.configs.telescope-nvim.undo"),
+    lazy = require("plugins.configs.telescope-nvim.lazy"),
+    file_browser = require("plugins.configs.telescope-nvim.file-browser"),
+    package_info = require("plugins.configs.telescope-nvim.package-info"),
+    zoxide = require("plugins.configs.telescope-nvim.zoxide"),
+    undo = require("plugins.configs.telescope-nvim.undo"),
 }
 
 -- NOTE: Add fzf_sorter if not a Windows
 -- Very difficult to build fzf on Windows
 if not require("config.global").is_windows then
     table.insert(extensions, { fzf = require("plugins.configs.telescope-nvim.fzf") })
-    --table.insert(extensions, { media = require("plugins.configs.telescope-nvim.media") })
+    table.insert(extensions, { media = require("plugins.configs.telescope-nvim.media") })
 end
 
 return extensions
