ff3aadbd118a4a6e29e282d09a8c28396b1f68d0
Yuto Tanaka <karaage_nennen@icloud.com>
2024-08-31 10:51:54 +0900

    refactor(dial-nvim): improved

--- dot_config/nvim/lua/plugins/dial-nvim.lua
+++ dot_config/nvim/lua/plugins/dial-nvim.lua
@@ -4,82 +4,12 @@ local cmds = {
     "DialDecrement",
 }
 
----@type LazyKeysSpec[]
-local keys = {
-    {
-        "<C-a>",
-        function()
-            require("dial.map").manipulate("increment", "normal")
-        end,
-        mode = "n",
-        desc = "normal mode increment",
-    },
-    {
-        "<C-x>",
-        function()
-            require("dial.map").manipulate("decrement", "normal")
-        end,
-        mode = "n",
-        desc = "normal mode decrement",
-    },
-    {
-        "<C-a>",
-        function()
-            require("dial.map").manipulate("increment", "visual")
-        end,
-        mode = "v",
-        desc = "visual mode increment",
-    },
-    {
-        "<C-x>",
-        function()
-            require("dial.map").manipulate("decrement", "visual")
-        end,
-        mode = "v",
-        desc = "visual mode decrement",
-    },
-    --[[
-    {
-        "g<C-a>",
-        function()
-            require("dial.map").manipulate("increment", "gnormal")
-        end,
-        mode = "gn",
-        desc = "gnormal mode increment",
-    },
-    {
-        "g<C-x>",
-        function()
-            require("dial.map").manipulate("decrement", "gnormal")
-        end,
-        mode = "gn",
-        desc = "gnormal mode decrement",
-    },
-    {
-        "g<C-a>",
-        function()
-            require("dial.map").manipulate("increment", "gvisual")
-        end,
-        mode = "gv",
-        desc = "gvisual mode increment",
-    },
-    {
-        "g<C-x>",
-        function()
-            require("dial.map").manipulate("decrement", "gvisual")
-        end,
-        mode = "gv",
-        desc = "gvisual mode decrement",
-    },
-    ]]
-}
-
 ---@type LazySpec
 local spec = {
     "monaqa/dial.nvim",
     --lazy = false,
     cmd = cmds,
-    keys = keys,
+    keys = require("plugins.configs.dial-nvim.keys"),
     event = "VeryLazy",
     --opts = {},
     --config = true,
