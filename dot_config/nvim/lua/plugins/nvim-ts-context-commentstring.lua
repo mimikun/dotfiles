db69afb474f95fbe843aae566db4d2b9db6db32d
Yuto Tanaka <karaage_nennen@icloud.com>
2024-09-17 18:28:27 +0900

    feat(nvim): use Comment.nvim and nvim-ts-context-commentstring

---
+++ dot_config/nvim/lua/plugins/nvim-ts-context-commentstring.lua
@@ -0,0 +1,12 @@
+---@type LazySpec
+local spec = {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    --lazy = false,
+    opts = {
+        enable_autocmd = false,
+    },
+    --cond = false,
+    --enabled = false,
+}
+
+return spec
