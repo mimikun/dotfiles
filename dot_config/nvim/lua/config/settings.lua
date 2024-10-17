2d811a94d80e0a0d9d7a505aa7be7209448ce1b2
Yuto Tanaka <karaage_nennen@icloud.com>
2024-10-17 19:45:40 +0900

    wip

--- dot_config/nvim/lua/config/settings.lua
+++ dot_config/nvim/lua/config/settings.lua
@@ -1,10 +1,12 @@
+local global = require("config.global")
+
 local settings = {}
 
 ---use ai assistant
 -- NOTE: AI assistants will only be enabled at home
 -- (i.e. only if it is a human right)
 ---@type boolean
-settings.use_ai = require("config.global").is_human_rights
+settings.use_ai = global.is_human_rights
 
 ---use denops
 ---@type boolean
@@ -38,5 +40,28 @@ settings.use_guard = false
 ---need all telescope extensions
 ---@type boolean
 settings.need_all_exts = false
+
+---use GitHub Copilot
+-- NOTE: only be enabled at Home-machines
+settings.use_github_copilot = global.is_home and true or false
+
+local check_codecompanion = function()
+    if global.is_azusa then
+        return true
+    else
+        if global.is_work then
+            if global.is_windows then
+                return true
+            else
+                return false
+            end
+        end
+        --return global.is_windows and true or false
+    end
+end
+
+---use Codecompanion.nvim ollama
+-- NOTE: only be enabled at Home-azusa and Work-machines
+settings.use_codecompanion = check_codecompanion()
 
 return settings
