3f634d437cd0dd86d7812d929a4798645dfc1fb2
Yuto Tanaka <karaage_nennen@icloud.com>
2024-12-11 21:10:11 +0900

    feat(yazi): add some plugins

---
+++ dot_config/yazi/init.lua
@@ -0,0 +1,8 @@
+-- https://github.com/yazi-rs/plugins/tree/main/full-border.yazi
+require("full-border"):setup({
+    --type = ui.Border.PLAIN,
+    type = ui.Border.ROUNDED,
+})
+
+-- https://github.com/yazi-rs/plugins/tree/main/git.yazi
+require("git"):setup()
