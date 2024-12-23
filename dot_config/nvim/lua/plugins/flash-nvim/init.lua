b63cf9e8085a00665980fe558f866c85a15c7ed8
Yuto Tanaka <karaage_nennen@icloud.com>
2024-12-23 19:58:47 +0900

    feat(nvim): add flash.nvim

---
+++ dot_config/nvim/lua/plugins/flash-nvim/init.lua
@@ -0,0 +1,12 @@
+---@type LazySpec
+local spec = {
+    "folke/flash.nvim",
+    --lazy = false,
+    keys = require("plugins.flash-nvim.keys"),
+    event = "VeryLazy",
+    opts = {},
+    cond = false,
+    enabled = false,
+}
+
+return spec
