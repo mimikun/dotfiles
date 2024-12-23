89ab917ac63cf29c43b6b3989ee2c6119c6c4a0f
Yuto Tanaka <karaage_nennen@icloud.com>
2024-12-23 19:58:47 +0900

    feat(nvim): add screenkey.nvim

---
+++ dot_config/nvim/lua/plugins/screenkey-nvim.lua
@@ -0,0 +1,12 @@
+---@type LazySpec
+local spec = {
+    "NStefan002/screenkey.nvim",
+    version = "*",
+    lazy = false,
+    cmd = "ScreenKey",
+    opts = {},
+    --cond = false,
+    --enabled = false,
+}
+
+return spec
