82906bd1b6e59fcd75b1f6fa4f101540f44075d9
Yuto Tanaka <karaage_nennen@icloud.com>
2024-12-24 19:39:33 +0900

    chore(codecompanion.nvim) add blink.cmp in deps

--- dot_config/nvim/lua/plugins/codecompanion-nvim/dependencies.lua
+++ dot_config/nvim/lua/plugins/codecompanion-nvim/dependencies.lua
@@ -3,6 +3,7 @@ local dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
     --{ "iguanacucumber/magazine.nvim", name = "nvim-cmp" },
+    "saghen/blink.cmp",
     "nvim-telescope/telescope.nvim",
     "stevearc/dressing.nvim",
 }
