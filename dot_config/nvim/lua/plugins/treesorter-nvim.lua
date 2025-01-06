9a515689cdda99b3ee60a0d82c408155fefe7137
Yuto Tanaka <karaage_nennen@icloud.com>
2025-01-06 22:34:13 +0900

    feat(nvim): add treesorter.nvim

---
+++ dot_config/nvim/lua/plugins/treesorter-nvim.lua
@@ -0,0 +1,11 @@
+---@type LazySpec
+local spec = {
+    "maxbol/treesorter.nvim",
+    --lazy = false,
+    cmd = "TSort",
+    opts = {},
+    --cond = false,
+    --enabled = false,
+}
+
+return spec
