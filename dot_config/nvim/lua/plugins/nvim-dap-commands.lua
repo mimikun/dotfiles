2d88dcdb09c5e7a428d8788d85cc3aba92deb4ec
Yuto Tanaka <karaage_nennen@icloud.com>
2025-01-06 22:34:13 +0900

    feat(nvim): add nvim-dap-commands

---
+++ dot_config/nvim/lua/plugins/nvim-dap-commands.lua
@@ -0,0 +1,18 @@
+local cmds = {
+    "Debugpy",
+    "DebugLldbRust",
+    "DebugGdb",
+    "DebugLldb",
+}
+
+---@type LazySpec
+local spec = {
+    "theHamsta/nvim-dap-commands",
+    --lazy = false,
+    cmd = cmds,
+    opts = {},
+    --cond = false,
+    --enabled = false,
+}
+
+return spec
