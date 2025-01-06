faa2bee774b10fd458d56db8d77c388d878b7024
Yuto Tanaka <karaage_nennen@icloud.com>
2025-01-06 22:34:13 +0900

    feat(nvim): add treewalker.nvim

---
+++ dot_config/nvim/lua/plugins/treewalker-nvim.lua
@@ -0,0 +1,12 @@
+---@type LazySpec
+local spec = {
+    "aaronik/treewalker.nvim",
+    --lazy = false,
+    cmd = "Treewalker",
+    --keys = "",
+    opts = {},
+    --cond = false,
+    --enabled = false,
+}
+
+return spec
