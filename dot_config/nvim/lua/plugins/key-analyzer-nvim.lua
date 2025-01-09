71459fafa7b91a8408e3e3cab3fcf12f1d7b0943
Yuto Tanaka <karaage_nennen@icloud.com>
2025-01-09 18:09:53 +0900

    feat(nvim): add key-analyzer.nvim

---
+++ dot_config/nvim/lua/plugins/key-analyzer-nvim.lua
@@ -0,0 +1,11 @@
+---@type LazySpec
+local spec = {
+    "meznaric/key-analyzer.nvim",
+    --lazy = false,
+    cmd = "KeyAnalyzer",
+    opts = {},
+    --cond = false,
+    --enabled = false,
+}
+
+return spec
