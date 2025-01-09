682d343af4548dfe28b225d2730cd4c9ad974646
Yuto Tanaka <karaage_nennen@icloud.com>
2025-01-09 18:09:53 +0900

    feat(nvim): add output-panel.nvim

---
+++ dot_config/nvim/lua/plugins/output-panel-nvim.lua
@@ -0,0 +1,12 @@
+---@type LazySpec
+local spec = {
+    "mhanberg/output-panel.nvim",
+    --lazy = false,
+    cmd = "OutputPanel",
+    event = "VeryLazy",
+    opts = {},
+    --cond = false,
+    --enabled = false,
+}
+
+return spec
