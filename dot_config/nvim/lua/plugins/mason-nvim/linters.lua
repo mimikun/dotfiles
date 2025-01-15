7bf67ce7b74e0d7396a06543fc78133e5e713bfe
Yuto Tanaka <karaage_nennen@icloud.com>
2025-01-15 21:09:20 +0900

    chore(mason-nvim-lint): update config
    and, fix yamllint can't install with windows

--- dot_config/nvim/lua/plugins/mason-nvim/linters.lua
+++ dot_config/nvim/lua/plugins/mason-nvim/linters.lua
@@ -1,9 +1,27 @@
 ---@type table
-local linters = {
+local need_linters = {}
+
+---@type table
+local base_linters = {
     -- fish
     "selene",
-    "yamllint",
     "textlint",
 }
 
-return linters
+---@type table
+local not_windows = {
+    "yamllint",
+}
+
+---@type table
+need_linters = vim.list_extend(need_linters, base_linters)
+
+-- NOTE: Include some linters, NOT Windows
+if not require("config.global").is_windows then
+    ---@type table
+    need_linters = vim.list_extend(need_linters, not_windows)
+end
+
+table.sort(need_linters)
+
+return need_linters
