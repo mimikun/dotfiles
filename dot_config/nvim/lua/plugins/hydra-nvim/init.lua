c4dbc7ed77df0a6cf37b63e3e482e6b4ec526c0a
Yuto Tanaka <karaage_nennen@icloud.com>
2025-01-20 20:11:30 +0900

    chore(hydra.nvim): update config

--- dot_config/nvim/lua/plugins/hydra-nvim/init.lua
+++ dot_config/nvim/lua/plugins/hydra-nvim/init.lua
@@ -1,24 +1,10 @@
----@type LazyKeysSpec[]
-local keys = {
-    -- TODO: keys
-}
-
----@type LazySpec[]
-local dependencies = {
-    -- TODO: deps
-}
-
 ---@type LazySpec
 local spec = {
     "nvimtools/hydra.nvim",
     --lazy = false,
-    --keys = keys,
-    --dependencies = dependencies,
-    config = function()
-        -- TODO: create hydras in here
-    end,
-    cond = false,
-    enabled = false,
+    opts = {},
+    --cond = false,
+    --enabled = false,
 }
 
 return spec
