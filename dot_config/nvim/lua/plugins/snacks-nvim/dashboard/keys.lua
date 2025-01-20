d57b341f85f5cfff851c7f35a4383ed82786381c
Yuto Tanaka <karaage_nennen@icloud.com>
2025-01-20 20:11:30 +0900

    chore(snacks.nvim): update dashboard keys
    use update-menu hydra head

--- dot_config/nvim/lua/plugins/snacks-nvim/dashboard/keys.lua
+++ dot_config/nvim/lua/plugins/snacks-nvim/dashboard/keys.lua
@@ -1,22 +1,103 @@
 ---@type snacks.dashboard.Item[]
 local keys = {
     -- New file
-    { icon = "", key = "n", desc = "New File", action = ":ene | startinsert" },
+    {
+        icon = "",
+        key = "n",
+        desc = "New File",
+        action = ":ene | startinsert",
+    },
     -- Utilities
-    { icon = "󰒲", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
-    -- TODO: instead telescope command
-    { icon = "󰐅", key = "T", desc = "Treesitter", action = ":TSInstallInfo" },
-    { icon = "󰭠", key = "M", desc = "Mason", action = ":Mason" },
+    {
+        icon = "󰒲",
+        key = "L",
+        desc = "Lazy",
+        action = function()
+            require("lazy").home()
+        end,
+        enabled = package.loaded.lazy ~= nil,
+    },
+    {
+        icon = "󰐅",
+        key = "T",
+        desc = "Treesitter",
+        action = ":Telescope treesitter_info",
+    },
+    {
+        icon = "󰭠",
+        key = "M",
+        desc = "Mason",
+        action = function()
+            require("mason.ui").open()
+        end,
+    },
     -- Updates
-    { icon = "", key = "l", desc = "Update Plugins", action = ":Lazy sync" },
-    { icon = "", key = "t", desc = "Update Treesitter", action = ":TSUpdate" },
-    { icon = "", key = "m", desc = "Update Mason", action = ":MasonUpdateAll" },
+    {
+        icon = "",
+        key = "u",
+        desc = "Update",
+        action = function()
+            require("plugins.hydra-nvim.update-menu"):activate()
+        end,
+    },
+    --[[
+    {
+        icon = "",
+        key = "l",
+        desc = "Update Plugins",
+        action = function()
+            require("lazy").sync()
+        end,
+    },
+    {
+        icon = "",
+        key = "t",
+        desc = "Update Treesitter",
+        action = function()
+            vim.cmd(":TSUpdate")
+        end,
+    },
+    {
+        icon = "",
+        key = "m",
+        desc = "Update Mason",
+        action = function()
+            vim.cmd(":MasonUpdateAll")
+        end,
+    },
+    ]]
+    -- telekasten
+    {
+        icon = "󰃶",
+        key = "t",
+        desc = "Open DailyNote",
+        action = function()
+            require("telekasten").goto_today()
+        end,
+    },
     -- checkhealth
-    { icon = "󰿶", key = "c", desc = "Checkhealth", action = ":checkhealth" },
+    {
+        icon = "󰿶",
+        key = "c",
+        desc = "Checkhealth",
+        action = ":checkhealth",
+    },
     -- dotfyle-metadata.nvim
-    { icon = "", key = "d", desc = "DotfyleMetaData", action = ":DotfyleGenerate --keymaps" },
+    {
+        icon = "",
+        key = "d",
+        desc = "DotfyleMetaData",
+        action = function()
+            require("dotfyle_metadata").generate({ "--keymaps" })
+        end,
+    },
     -- Quit
-    { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+    {
+        icon = " ",
+        key = "q",
+        desc = "Quit",
+        action = ":qa",
+    },
 }
 
 return keys
