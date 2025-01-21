8e0b5a703a80c8e9c093104173d5a939bd883dc6
Yuto Tanaka <karaage_nennen@icloud.com>
2025-01-21 23:14:10 +0900

    chore(snacks.nvim): update config

--- dot_config/nvim/lua/plugins/snacks-nvim/opts.lua
+++ dot_config/nvim/lua/plugins/snacks-nvim/opts.lua
@@ -1,8 +1,11 @@
 ---@class snacks.Config
 local opts = {
-    bigfile = { enabled = true },
+    bigfile = {
+        enabled = true,
+        -- 150KB, 1KB = 1024byte
+        size = 150 * 1024,
+    },
     dashboard = require("plugins.snacks-nvim.dashboard"),
-    -- TODO: want use it
     indent = { enabled = false },
     input = { enabled = false },
     notifier = { enabled = true },
@@ -10,12 +13,12 @@ local opts = {
     scope = { enabled = false },
     scroll = { enabled = false },
     statuscolumn = { enabled = false },
+    words = { enabled = true },
     styles = {
         notification = {
             wo = { wrap = true },
         },
     },
-    words = { enabled = true },
 }
 
 return opts
