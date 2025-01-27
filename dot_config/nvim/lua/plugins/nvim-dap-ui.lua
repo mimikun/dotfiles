cf9c87e14528d302377a7d5d2a222c75304cf2b8
Yuto Tanaka <karaage_nennen@icloud.com>
2025-01-27 20:51:31 +0900

    chore(nvim-dap-ui): update config
    and disabled

--- dot_config/nvim/lua/plugins/nvim-dap-ui.lua
+++ dot_config/nvim/lua/plugins/nvim-dap-ui.lua
@@ -15,33 +15,19 @@ local opts = {
 local spec = {
     "rcarriga/nvim-dap-ui",
     --lazy = false,
-    --cmd = "CMDNAME",
+    cmd = "ToggleDapUi",
     dependencies = dependencies,
     config = function()
-        local dap = require("dap")
-        local dapui = require("dapui")
-
+        local dapui = require("dap-ui")
         dapui.setup(opts)
-        dap.listeners.before.attach.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.launch.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.event_terminated.dapui_config = function()
-            dapui.close()
-        end
-        dap.listeners.before.event_exited.dapui_config = function()
-            dapui.close()
-        end
 
         -- Toggle dap-ui
         vim.api.nvim_create_user_command("ToggleDapUi", function()
             dapui.toggle()
         end, {})
     end,
-    --cond = false,
-    --enabled = false,
+    cond = false,
+    enabled = false,
 }
 
 return spec
