c140b567d51857c15e818bdfce93fdec67c4bb12
Yuto Tanaka <karaage_nennen@icloud.com>
2025-01-31 21:16:37 +0900

    chore(wezterm): update launchmenu

--- dot_config/wezterm/launchmenu.lua
+++ dot_config/wezterm/launchmenu.lua
@@ -1,56 +1,90 @@
 local wsl_domain = "WSL:Ubuntu"
 
-local M = {}
-
-M.wsl_domain = wsl_domain
-
-M.launch_menu = {
-    -- 1
-    {
+local wsl = {
+    ubuntu = {
         label = "WSL Ubuntu",
         domain = {
             DomainName = wsl_domain,
         },
     },
-    -- 2
-    {
-        label = "Windows PowerShell v7",
+    nixos = {
+        label = "WSL NixOS",
         domain = {
-            DomainName = "local",
+            DomainName = "WSL:NixOS",
         },
-        args = { "pwsh.exe" },
     },
-    -- 3
-    {
-        label = "Windows PowerShell v5",
-        domain = {
-            DomainName = "local",
+}
+
+local windows = {
+    pwsh = {
+        base = {
+            label = "Windows PowerShell v7",
+            domain = {
+                DomainName = "local",
+            },
+            args = { "pwsh.exe" },
         },
-        args = { "powershell.exe" },
+        norc = {
+            label = "Windows PowerShell v7 --norc",
+            domain = {
+                DomainName = "local",
+            },
+            args = { "pwsh.exe", "-NoProfile" },
+        },
     },
-    -- 4
-    {
+    powershell = {
+        base = {
+            label = "Windows PowerShell v5",
+            domain = {
+                DomainName = "local",
+            },
+            args = { "powershell.exe" },
+        },
+        norc = {
+            label = "Windows PowerShell v5 --norc",
+            domain = {
+                DomainName = "local",
+            },
+            args = { "powershell.exe", "-NoProfile" },
+        },
+    },
+    cmd_exe = {
         label = "Windows cmd.exe",
         domain = {
             DomainName = "local",
         },
         args = { "cmd.exe" },
     },
-    -- 5
-    {
+    nyagos = {
         label = "nyagos - Nihongo Yet Another GOing Shell",
         domain = {
             DomainName = "local",
         },
         args = { "nyagos.exe" },
     },
+}
+
+local M = {}
+
+M.wsl_domain = wsl_domain
+
+M.launch_menu = {
+    -- 1
+    wsl.ubuntu,
+    -- 2
+    windows.pwsh.base,
+    -- 3
+    windows.pwsh.norc,
+    -- 4
+    windows.powershell.base,
+    -- 5
+    windows.powershell.norc,
     -- 6
-    {
-        label = "WSL NixOS",
-        domain = {
-            DomainName = "WSL:NixOS",
-        },
-    },
+    windows.cmd_exe,
+    -- 7
+    windows.nyagos,
+    -- 8
+    wsl.nixos,
 }
 
 return M
