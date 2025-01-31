6933e73ba3f6638d5f0aeeb91d86864adc27e39b
Yuto Tanaka <karaage_nennen@icloud.com>
2025-01-31 21:16:37 +0900

    feat(feed.nvim): add deps

---
+++ dot_config/nvim/lua/plugins/feed-nvim/dependencies.lua
@@ -0,0 +1,7 @@
+---@type LazySpec[]
+local dependencies = {
+    "gregorias/coop.nvim",
+    "pysan3/pathlib.nvim",
+}
+
+return dependencies
--- dot_config/nvim/lua/plugins/feed-nvim/init.lua
+++ dot_config/nvim/lua/plugins/feed-nvim/init.lua
@@ -3,6 +3,7 @@ local spec = {
     "neo451/feed.nvim",
     --lazy = false,
     cmd = "Feed",
+    dependencies = require("plugins.feed-nvim.dependencies"),
     opts = require("plugins.feed-nvim.opts"),
     --cond = false,
     --enabled = false,
