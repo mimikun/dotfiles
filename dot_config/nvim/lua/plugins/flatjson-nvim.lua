71ab9e8ccbb70fe8da43a5e334bb5adcc45725b9
Yuto Tanaka <karaage_nennen@icloud.com>
2025-02-07 22:18:55 +0900

    feat(nvim): add flatjson.nvim

---
+++ dot_config/nvim/lua/plugins/flatjson-nvim.lua
@@ -0,0 +1,11 @@
+---@type LazySpec
+local spec = {
+    "virakkhun/flatjson.nvim",
+    --lazy = false,
+    cmd = "FlatJSON",
+    opts = {},
+    --cond = false,
+    --enabled = false,
+}
+
+return spec
