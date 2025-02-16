036822cdd1e9d21a4c2a771f07e55de60c689944
Yuto Tanaka <karaage_nennen@icloud.com>
2025-02-16 17:57:52 +0900

    chore(oil.nvim): update config

---
+++ dot_config/nvim/lua/plugins/oil-nvim/dependencies.lua
@@ -0,0 +1,11 @@
+---@type LazySpec[]
+local dependencies = {
+    {
+        "refractalize/oil-git-status.nvim",
+        ft = "oil",
+    },
+    "JezerM/oil-lsp-diagnostics.nvim",
+    "nvim-tree/nvim-web-devicons",
+}
+
+return dependencies
