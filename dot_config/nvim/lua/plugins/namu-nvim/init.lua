59713565415a940449c4712f6438f552edfd30fe
Yuto Tanaka <karaage_nennen@icloud.com>
2025-02-16 17:53:55 +0900

    feat(nvim): add namu.nvim

---
+++ dot_config/nvim/lua/plugins/namu-nvim/init.lua
@@ -0,0 +1,12 @@
+---@type LazySpec
+local spec = {
+    "bassamsdata/namu.nvim",
+    --lazy = false,
+    cmd = "Namu",
+    keys = require("plugins.namu-nvim.keys"),
+    opts = {},
+    cond = false,
+    enabled = false,
+}
+
+return spec
