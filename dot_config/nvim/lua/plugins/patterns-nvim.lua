7e44e2d9d9cd64d010a127a9779c8ce4cf06c032
Yuto Tanaka <karaage_nennen@icloud.com>
2025-02-28 13:09:34 +0900

    feat(nvim): add patterns.nvim

---
+++ dot_config/nvim/lua/plugins/patterns-nvim.lua
@@ -0,0 +1,11 @@
+---@type LazySpec
+local spec = {
+    "OXY2DEV/patterns.nvim",
+    --lazy = false,
+    cmd = "Patterns",
+    opts = {},
+    --cond = false,
+    --enabled = false,
+}
+
+return spec
