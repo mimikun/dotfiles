1cd4ac2b269973ad1f2783f2228f55ea830999fb
Yuto Tanaka <karaage_nennen@icloud.com>
2025-03-02 00:16:34 +0900

    feat(nvim): add rooter.nvim

---
+++ dot_config/nvim/lua/plugins/rooter-nvim.lua
@@ -0,0 +1,24 @@
+-- TODO: add this plugin to telescope.nvim config
+
+---@type table
+local opts = {
+    root_patterns = { ".git/" },
+    outermost = true,
+    enable_cache = true,
+    project_non_root = "",
+    --project_non_root = "home",
+    --project_non_root = "current",
+    enable_logger = true, -- enable runtime log via logger.nvim
+}
+
+---@type LazySpec
+local spec = {
+    "wsdjeg/rooter.nvim",
+    --lazy = false,
+    dependencies = { "wsdjeg/logger.nvim" },
+    opts = opts,
+    cond = false,
+    enabled = false,
+}
+
+return spec
