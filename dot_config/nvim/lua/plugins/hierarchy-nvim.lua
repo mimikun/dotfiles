a16c170d6c1a1f8805c4c4991726c13e0669367e
Yuto Tanaka <karaage_nennen@icloud.com>
2025-03-02 00:15:37 +0900

    feat(nvim): add hierarchy.nvim

---
+++ dot_config/nvim/lua/plugins/hierarchy-nvim.lua
@@ -0,0 +1,13 @@
+---@type LazySpec
+local spec = {
+    "lafarr/hierarchy.nvim",
+    --lazy = false,
+    cmd = "FunctionReferences",
+    opts = {
+        depth = 3,
+    },
+    cond = false,
+    enabled = false,
+}
+
+return spec
