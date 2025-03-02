e8bc8a24193833f238d8ed3e17a5cbbbb792f9c4
Yuto Tanaka <karaage_nennen@icloud.com>
2025-03-02 13:51:14 +0900

    feat(nvim): add neospeller.nvim

---
+++ dot_config/nvim/lua/plugins/neospeller-nvim.lua
@@ -0,0 +1,11 @@
+---@type LazySpec
+local spec = {
+    "richardhapb/neospeller.nvim",
+    --lazy = false,
+    cmd = "CheckSpell",
+    opts = {},
+    cond = false,
+    enabled = false,
+}
+
+return spec
