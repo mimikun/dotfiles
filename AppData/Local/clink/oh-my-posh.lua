0301018333dcee7e3f6ee6faf6cf998219d17edd
Yuto Tanaka <karaage_nennen@icloud.com>
2025-03-11 18:43:59 +0900

    feat(shells): use oh-my-posh

---
+++ AppData/Local/clink/oh-my-posh.lua
@@ -0,0 +1,2 @@
+--load(io.popen("oh-my-posh init cmd"):read("*a"))()
+--load(io.popen("oh-my-posh init cmd --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/refs/heads/main/themes/tokyonight_storm.omp.json'"):read("*a"))()
