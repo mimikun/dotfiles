65fe12095f770419c2b6edbe6d32c8c156dcd31f
Yuto Tanaka <karaage_nennen@icloud.com>
2025-03-12 20:22:03 +0900

    feat(nvim): add nerdy.nvim

---
+++ dot_config/nvim/lua/plugins/nerdy-nvim.lua
@@ -0,0 +1,12 @@
+---@type LazySpec
+local spec = {
+    "2kabhishek/nerdy.nvim",
+    --lazy = false,
+    cmd = "Nerdy",
+    dependencies = { "folke/snacks.nvim", optional = true },
+    opts = {},
+    cond = false,
+    enabled = false,
+}
+
+return spec
