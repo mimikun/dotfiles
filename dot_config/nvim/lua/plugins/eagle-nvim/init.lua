cd4fd78fe357d237f200c8485ad4e113a07066d0
Yuto Tanaka <karaage_nennen@icloud.com>
2025-03-24 20:04:41 +0900

    fix(eagle.nvim): module path typo

--- dot_config/nvim/lua/plugins/eagle-nvim/init.lua
+++ dot_config/nvim/lua/plugins/eagle-nvim/init.lua
@@ -2,13 +2,13 @@
 local spec = {
     "soulis-1256/eagle.nvim",
     --lazy = false,
-    cmd = require("plguins.eagle-nvim.cmds"),
-    keys = require("plguins.eagle-nvim.keys"),
+    cmd = require("plugins.eagle-nvim.cmds"),
+    keys = require("plugins.eagle-nvim.keys"),
     init = function()
         --vim.opt.mousemoveevent = true
         vim.opt.mousemoveevent = false
     end,
-    opts = require("plguins.eagle-nvim.opts"),
+    opts = require("plugins.eagle-nvim.opts"),
     cond = false,
     enabled = false,
 }
