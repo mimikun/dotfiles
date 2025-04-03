732f7a62e96c158f3cd5912a5b84e34ccf3e3909
Yuto Tanaka <karaage_nennen@icloud.com>
2025-04-03 18:53:29 +0900

    fix(kulala.nvim): vimdoc bug

--- dot_config/nvim/lua/plugins/kulala-nvim/init.lua
+++ dot_config/nvim/lua/plugins/kulala-nvim/init.lua
@@ -1,6 +1,8 @@
 ---@type LazySpec
 local spec = {
     "mistweaverco/kulala.nvim",
+    -- WORKAROUND: master branch has vimdoc bug, vimdoc 💩
+    version = "*",
     --lazy = false,
     ft = "http",
     opts = require("plugins.kulala-nvim.opts"),
