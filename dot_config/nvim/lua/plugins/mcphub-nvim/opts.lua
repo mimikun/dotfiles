a28c502622153bad1080ddc9a066f0d598582330
Yuto Tanaka <karaage_nennen@icloud.com>
2025-04-22 19:38:10 +0900

    chore(mcphub.nvim): update config

--- dot_config/nvim/lua/plugins/mcphub-nvim/opts.lua
+++ dot_config/nvim/lua/plugins/mcphub-nvim/opts.lua
@@ -1,6 +1,26 @@
+local g = require("config.global")
+
+---@type string
+local config = table.concat({ g.home, ".config", "mcphub", "servers.json" }, g.path_sep)
+
 ---@type table
 local opts = {
+    -- TODO: fix it
+    port = 37373,
+    config = config,
     native_servers = {},
+    auto_approve = false,
+    auto_toggle_mcp_servers = true,
+    extensions = {
+        avante = {
+            make_slash_commands = true,
+        },
+        codecompanion = {
+            show_result_in_chat = true,
+            make_vars = true,
+            make_slash_commands = true,
+        },
+    },
     ui = {
         window = {
             -- "none", "single", "double", "rounded", "solid", "shadow"
