79bdf6b0dcaca531aa14b4190bb48aef4813f047
Yuto Tanaka <karaage_nennen@icloud.com>
2025-04-26 00:10:43 +0900

    feat(nvim): add learn-vim.nvim

---
+++ dot_config/nvim/lua/plugins/learn-vim-nvim.lua
@@ -0,0 +1,11 @@
+---@type LazySpec
+local spec = {
+    "melkyr/learn-vim.nvim",
+    --lazy = false,
+    cmd = "LearnVim",
+    opts = {},
+    cond = false,
+    enabled = false,
+}
+
+return spec
