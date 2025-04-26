0603853689c32a2539ada35d2780252daabe2227
Yuto Tanaka <karaage_nennen@icloud.com>
2025-04-26 22:23:55 +0900

    feat(snacks.dashboard): add gaming logo

--- dot_config/nvim/lua/plugins/snacks-nvim/dashboard/terminal.lua
+++ dot_config/nvim/lua/plugins/snacks-nvim/dashboard/terminal.lua
@@ -5,16 +5,45 @@ local img_path = vim.fn.expand("~/.kabegami/banitasu.png")
 ---@type table
 local M = {}
 
+-- banitasu image
+---@type table
 local banitasu_image = {
     section = "terminal",
-    --cmd = "chafa " .. img_path .. " --symbols vhalf; sleep 10",
     cmd = "chafa " .. img_path .. " ; sleep 10",
 }
 
+-- low performance env
 local limited_env = {}
 
+---@type table
+local tte_subcommands = {
+    "middleout --center-movement-speed 0.8 --full-movement-speed 0.2",
+    "slide --merge --movement-speed 0.8",
+    "beams --beam-delay 5 --beam-row-speed-range 20-60 --beam-column-speed-range 8-12",
+}
+math.randomseed(os.time())
+local subcommand = tte_subcommands[math.random(#tte_subcommands)]
+
+local neovim_logo = require("plugins.snacks-nvim.dashboard.header").get("logo").Neovim_gaming
+
+-- gaming animation logo
+---@type table
+local gaming_logo = {
+    section = "terminal",
+    cmd = {
+        "sh",
+        "-c",
+        "echo -e "
+            .. vim.fn.shellescape(vim.trim(neovim_logo))
+            .. " | tte --anchor-canvas s "
+            .. subcommand
+            .. " --final-gradient-direction diagonal",
+    },
+}
+
 if use_dashboard_image then
-    M = banitasu_image
+    --M = banitasu_image
+    M = gaming_logo
 else
     M = limited_env
 end
