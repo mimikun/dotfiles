4bcde2662f1b6e1d098387c4abafb6752847a4bf
Yuto Tanaka <karaage_nennen@icloud.com>
2025-04-26 11:00:08 +0900

    chore(kulala.nvim): remove WORKAROUND

--- dot_config/nvim/lua/plugins/kulala-nvim/init.lua
+++ dot_config/nvim/lua/plugins/kulala-nvim/init.lua
@@ -1,8 +1,6 @@
 ---@type LazySpec
 local spec = {
     "mistweaverco/kulala.nvim",
-    -- WORKAROUND: master branch has vimdoc bug, vimdoc 💩
-    version = "*",
     --lazy = false,
     ft = "http",
     opts = require("plugins.kulala-nvim.opts"),
