872fbebda1fd3005a69336e12bad468638ed9e6c
Yuto Tanaka <karaage_nennen@icloud.com>
2025-04-26 10:53:59 +0900

    feat(nvim): add neonuget.nvim

---
+++ dot_config/nvim/lua/plugins/neonuget-nvim.lua
@@ -0,0 +1,12 @@
+---@type LazySpec
+local spec = {
+    "MonsieurTib/neonuget.nvim",
+    --lazy = false,
+    cmd = "NuGet",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+    cond = false,
+    enabled = false,
+}
+
+return spec
