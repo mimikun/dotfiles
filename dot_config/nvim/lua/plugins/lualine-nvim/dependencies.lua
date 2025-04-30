2156a5587d4637f0916480114f3e219e50b9cfd7
Yuto Tanaka <karaage_nennen@icloud.com>
2025-04-30 11:23:30 +0900

    chore(lualine.nvim): update deps

--- dot_config/nvim/lua/plugins/lualine-nvim/dependencies.lua
+++ dot_config/nvim/lua/plugins/lualine-nvim/dependencies.lua
@@ -4,14 +4,10 @@ local dependencies = {
     "lewis6991/gitsigns.nvim",
     "SmiteshP/nvim-navic",
     "pnx/lualine-lsp-status",
-    { "Dan7h3x/LazyDo", optional = true },
-    { "stevearc/aerial.nvim", optional = true },
+    "ravitemer/mcphub.nvim",
+    "Dan7h3x/LazyDo",
+    "stevearc/aerial.nvim",
+    "AndreM222/copilot-lualine",
 }
-
-if require("config.settings").use_github_copilot then
-    table.insert(dependencies, {
-        { "AndreM222/copilot-lualine" },
-    })
-end
 
 return dependencies
