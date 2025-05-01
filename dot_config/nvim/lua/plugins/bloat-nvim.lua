f4622acc64b9f58f1e36d02fae7bc11b68f2d8bd
Yuto Tanaka <karaage_nennen@icloud.com>
2025-05-01 16:42:34 +0900

    feat(nvim): add bloat.nvim

---
+++ dot_config/nvim/lua/plugins/bloat-nvim.lua
@@ -0,0 +1,11 @@
+---@type LazySpec
+local spec = {
+    "dundalek/bloat.nvim",
+    --lazy = false,
+    cmd = "Bloat",
+    opts = {},
+    --cond = false,
+    --enabled = false,
+}
+
+return spec
