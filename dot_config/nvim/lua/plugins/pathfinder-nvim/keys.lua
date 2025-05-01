32eba60a6abd667181537c927c1689824feb01e3
Yuto Tanaka <karaage_nennen@icloud.com>
2025-05-01 16:42:06 +0900

    chore(pathfinder.nvim): update config

--- dot_config/nvim/lua/plugins/pathfinder-nvim/keys.lua
+++ dot_config/nvim/lua/plugins/pathfinder-nvim/keys.lua
@@ -1,9 +1,105 @@
 ---@type LazyKeysSpec[]
 local keys = {
-    { "gf", mode = "n", silent = true, desc = "Enhanced go to file" },
-    { "gF", mode = "n", silent = true, desc = "Enhanced go to file (line)" },
-    { "<leader>gf", mode = "n", silent = true, desc = "Visual file selection" },
-    { "<leader>gF", mode = "n", silent = true, desc = "Visual file selection (line)" },
+    {
+        "gf",
+        function()
+            require("pathfinder").gf()
+        end,
+        mode = "n",
+        silent = true,
+        desc = "Enhanced go to file",
+    },
+    {
+        "gF",
+        function()
+            require("pathfinder").gF()
+        end,
+        mode = "n",
+        silent = true,
+        desc = "Enhanced go to file (line)",
+    },
+    {
+        "gx",
+        function()
+            require("pathfinder").gx()
+        end,
+        mode = "n",
+        silent = true,
+        desc = "Open URL/Git repository",
+    },
+    {
+        "]f",
+        function()
+            require("pathfinder").next_file()
+        end,
+        mode = "n",
+        silent = true,
+        desc = "Jump to next valid file name",
+    },
+    {
+        "[f",
+        function()
+            require("pathfinder").prev_file()
+        end,
+        mode = "n",
+        silent = true,
+        desc = "Jump to previous valid file name",
+    },
+    {
+        "]u",
+        function()
+            require("pathfinder").next_url()
+        end,
+        mode = "n",
+        silent = true,
+        desc = "Jump to next valid URL",
+    },
+    {
+        "[u",
+        function()
+            require("pathfinder").prev_url()
+        end,
+        mode = "n",
+        silent = true,
+        desc = "Jump to previous valid URL",
+    },
+    {
+        "<leader>gf",
+        function()
+            require("pathfinder").select_file()
+        end,
+        mode = "n",
+        silent = true,
+        desc = "Visual file selection",
+    },
+    {
+        "<leader>gF",
+        function()
+            require("pathfinder").select_file_line()
+        end,
+        mode = "n",
+        silent = true,
+        desc = "Visual file selection (line)",
+    },
+    {
+        "<leader>gx",
+        function()
+            require("pathfinder").select_url()
+        end,
+        mode = "n",
+        silent = true,
+        desc = "Visual URL/Git repository selection",
+    },
+    {
+        "<leader>h",
+        function()
+            require("pathfinder").hover_description()
+        end,
+        mode = "n",
+        noremap = true,
+        silent = true,
+        desc = "Pathfinder: Hover",
+    },
 }
 
 return keys
