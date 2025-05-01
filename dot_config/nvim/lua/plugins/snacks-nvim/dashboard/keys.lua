bde5e6099ef80adeb728f25affd9829e893ee9db
Yuto Tanaka <karaage_nennen@icloud.com>
2025-05-01 21:14:54 +0900

    refactor(snacks.dashboard): rename

--- dot_config/nvim/lua/plugins/snacks-nvim/dashboard/keys.lua
+++ dot_config/nvim/lua/plugins/snacks-nvim/dashboard/keys.lua
@@ -1,29 +1,29 @@
 ---@type snacks.dashboard.Item[]
 local keys = {
     -- New file
-    require("plugins.snacks-nvim.dashboard.items.new-file"),
+    require("plugins.snacks-nvim.dashboard.key-items.new-file"),
     -- Dashboard menu
-    require("plugins.snacks-nvim.dashboard.items.hydra-dashboard-menu"),
-    --require("plugins.snacks-nvim.dashboard.items.lazy-home"),
-    --require("plugins.snacks-nvim.dashboard.items.treesitter-info"),
-    --require("plugins.snacks-nvim.dashboard.items.mason-open"),
+    require("plugins.snacks-nvim.dashboard.key-items.hydra-dashboard-menu"),
+    --require("plugins.snacks-nvim.dashboard.key-items.lazy-home"),
+    --require("plugins.snacks-nvim.dashboard.key-items.treesitter-info"),
+    --require("plugins.snacks-nvim.dashboard.key-items.mason-open"),
     -- Update menu
-    require("plugins.snacks-nvim.dashboard.items.hydra-update-menu"),
-    --require("plugins.snacks-nvim.dashboard.items.lazy-update"),
-    --require("plugins.snacks-nvim.dashboard.items.treesitter-update"),
-    --require("plugins.snacks-nvim.dashboard.items.mason-update"),
+    require("plugins.snacks-nvim.dashboard.key-items.hydra-update-menu"),
+    --require("plugins.snacks-nvim.dashboard.key-items.lazy-update"),
+    --require("plugins.snacks-nvim.dashboard.key-items.treesitter-update"),
+    --require("plugins.snacks-nvim.dashboard.key-items.mason-update"),
     -- Generate menu
-    require("plugins.snacks-nvim.dashboard.items.hydra-generate-menu"),
-    --require("plugins.snacks-nvim.dashboard.items.generate-dotfyle"),
+    require("plugins.snacks-nvim.dashboard.key-items.hydra-generate-menu"),
+    --require("plugins.snacks-nvim.dashboard.key-items.generate-dotfyle"),
     -- Fuzzy finder
-    --require("plugins.snacks-nvim.dashboard.items.hydra-fuzzy-finder"),
+    --require("plugins.snacks-nvim.dashboard.key-items.hydra-fuzzy-finder"),
     -- Other menu
-    require("plugins.snacks-nvim.dashboard.items.hydra-other-menu"),
-    --require("plugins.snacks-nvim.dashboard.items.feed-open"),
-    --require("plugins.snacks-nvim.dashboard.items.telekasten-open"),
-    --require("plugins.snacks-nvim.dashboard.items.check-health"),
+    require("plugins.snacks-nvim.dashboard.key-items.hydra-other-menu"),
+    --require("plugins.snacks-nvim.dashboard.key-items.feed-open"),
+    --require("plugins.snacks-nvim.dashboard.key-items.telekasten-open"),
+    --require("plugins.snacks-nvim.dashboard.key-items.check-health"),
     -- Quit
-    require("plugins.snacks-nvim.dashboard.items.quit"),
+    require("plugins.snacks-nvim.dashboard.key-items.quit"),
 }
 
 return keys
