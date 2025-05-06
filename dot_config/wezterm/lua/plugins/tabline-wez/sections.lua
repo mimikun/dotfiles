60a695b0b27881dde3668c5e1baf9d316be72320
Yuto Tanaka <karaage_nennen@icloud.com>
2025-05-06 15:34:27 +0900

    Revert "DROP: Force enable tabline.wez cpu,ram feature"
    This reverts commit ab81d9bad32bff957a069943b39829dd771678ea.

--- dot_config/wezterm/lua/plugins/tabline-wez/sections.lua
+++ dot_config/wezterm/lua/plugins/tabline-wez/sections.lua
@@ -75,10 +75,16 @@ return function(config, battery)
             {
                 "ram",
                 use_pwsh = true,
+                cond = function()
+                    return is_human_rights
+                end,
             },
             {
                 "cpu",
                 use_pwsh = true,
+                cond = function()
+                    return is_human_rights
+                end,
             },
         },
         tabline_y = {
