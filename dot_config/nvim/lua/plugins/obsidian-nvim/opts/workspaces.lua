047a7582ba71dd3a3444dffe7acac73535ee8903
Yuto Tanaka <karaage_nennen@icloud.com>
2025-05-08 19:25:22 +0900

    fix(obsidian.nvim): no-vault workspace settings
    `001_DailyNotes` folder was created randomly

--- dot_config/nvim/lua/plugins/obsidian-nvim/opts/workspaces.lua
+++ dot_config/nvim/lua/plugins/obsidian-nvim/opts/workspaces.lua
@@ -1,25 +1,37 @@
 ---@type table
-local workspaces = {
-    {
-        name = "mimikun",
-        path = vim.fn.expand("$obsidian_vault_path"),
-    },
-    {
-        name = "no-vault",
-        path = function()
-            -- alternatively use the CWD:
-            -- return assert(vim.fn.getcwd())
-            return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
-        end,
-        overrides = {
-            notes_subdir = vim.NIL,
-            new_notes_location = "current_dir",
-            templates = {
-                folder = vim.NIL,
-            },
-            disable_frontmatter = true,
+local mimikun = {
+    name = "mimikun",
+    path = vim.fn.expand("$obsidian_vault_path"),
+}
+
+---@type table
+local no_vault = {
+    name = "no-vault",
+    path = function()
+        -- alternatively use the CWD:
+        -- return assert(vim.fn.getcwd())
+        return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
+    end,
+    overrides = {
+        notes_subdir = vim.NIL,
+        daily_notes = {
+            folder = vim.NIL,
+        },
+        new_notes_location = "current_dir",
+        templates = {
+            folder = vim.NIL,
+        },
+        disable_frontmatter = true,
+        attachments = {
+            img_folder = vim.NIL,
         },
     },
+}
+
+---@type table
+local workspaces = {
+    mimikun,
+    no_vault,
 }
 
 return workspaces
