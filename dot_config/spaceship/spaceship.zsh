4f228178ff616f4ed6712ebf34072b2d126bcabb
Yuto Tanaka <karaage_nennen@icloud.com>
2025-05-14 22:07:29 +0900

    feat(zsh-prompt): add spaceship config

---
+++ dot_config/spaceship/spaceship.zsh
@@ -0,0 +1,16 @@
+# Display time
+SPACESHIP_TIME_SHOW=true
+
+# Display username always
+SPACESHIP_USER_SHOW=always
+
+# Do not truncate path in repos
+SPACESHIP_DIR_TRUNC_REPO=false
+
+# Add custom Ember section
+# See: https://github.com/spaceship-prompt/spaceship-ember
+#spaceship add ember
+
+# Add a custom vi-mode section to the prompt
+# See: https://github.com/spaceship-prompt/spaceship-vi-mode
+#spaceship add --before char vi_mode
