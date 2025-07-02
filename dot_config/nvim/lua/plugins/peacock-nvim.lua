7149f17ed064096056660e496eb5abf23573137a
Yuto Tanaka <git@mimikun.dev>
2025-07-02 20:08:28 +0900

    feat(nvim): add peacock.nvim

---
+++ dot_config/nvim/lua/plugins/peacock-nvim.lua
@@ -0,0 +1,11 @@
+---@type LazySpec
+local spec = {
+    "hampushauffman/peacock.nvim",
+    --lazy = false,
+    event = "BufEnter",
+    opts = {},
+    cond = false,
+    enabled = false,
+}
+
+return spec
