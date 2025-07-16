19b2341ce9081615f64994c9217bda3e480a0d56
Yuto Tanaka <git@mimikun.dev>
2025-07-16 20:38:52 +0900

    fix(denops-shared-server.vim): incorrect with cmds.lua

--- dot_config/nvim/lua/plugins/denops-shared-server-vim/cmds.lua
+++ dot_config/nvim/lua/plugins/denops-shared-server-vim/cmds.lua
@@ -1,6 +1,3 @@
----@type boolean
-local cond = require("config.settings").use_denops
-
 ---@type table
 local cmds = {
     "DenopsSharedServerInstall",
@@ -8,30 +5,4 @@ local cmds = {
     "DenopsSharedServerRestart",
 }
 
----@type LazySpec
-local spec = {
-    "vim-denops/denops-shared-server.vim",
-    lazy = false,
-    cmd = cmds,
-    dependencies = { "vim-denops/denops.vim" },
-    init = function()
-        if not require("config.global").is_human_rights then
-            vim.g.denops_server_addr = "127.0.0.1:32123"
-        end
-    end,
-    config = function()
-        vim.api.nvim_create_user_command("DenopsSharedServerInstall", function()
-            vim.fn["denops_shared_server#install"]()
-        end, {})
-        vim.api.nvim_create_user_command("DenopsSharedServerUninstall", function()
-            vim.fn["denops_shared_server#uninstall"]()
-        end, {})
-        vim.api.nvim_create_user_command("DenopsSharedServerRestart", function()
-            vim.fn["denops_shared_server#restart"]()
-        end, {})
-    end,
-    cond = cond,
-    enabled = cond,
-}
-
-return spec
+return cmds
