f3ad0f47de04569985807e98e61b2e193b93f04d
Yuto Tanaka <git@mimikun.dev>
2025-07-21 12:49:58 +0900

    feat(nvim): add org-mouse.nvim

---
+++ dot_config/nvim/lua/plugins/org-mouse-nvim/init.lua
@@ -0,0 +1,12 @@
+---@type LazySpec
+local spec = {
+    "chipsenkbeil/org-mouse.nvim",
+    --lazy = false,
+    ft = { "org" },
+    dependencies = { "nvim-orgmode/orgmode" },
+    opts = {},
+    --cond = false,
+    --enabled = false,
+}
+
+return spec
