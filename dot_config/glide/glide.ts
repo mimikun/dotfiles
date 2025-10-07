51ce51fbed16e0c96b66c1deaf83f1ea592b7a23
Yuto Tanaka <git@mimikun.dev>
2025-10-07 19:14:59 +0900

    feat: add glide-browser configs

---
+++ dot_config/glide/glide.ts
@@ -0,0 +1,18 @@
+// Config docs:
+//
+//   https://glide-browser.app/config
+//
+// API reference:
+//
+//   https://glide-browser.app/api
+//
+// Default config files can be found here:
+//
+//   https://github.com/glide-browser/glide/tree/main/src/glide/browser/base/content/plugins
+//
+// Most default keymappings are defined here:
+//
+//   https://github.com/glide-browser/glide/blob/main/src/glide/browser/base/content/plugins/keymaps.mts
+//
+// Try typing `glide.` and see what you can do!
+
