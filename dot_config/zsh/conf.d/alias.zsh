d171989c8b7a3516afe431fca798a411ac1f87d7
Yuto Tanaka <git@mimikun.dev>
2025-12-20 14:09:25 +0900

    refactor(zsh): restructure zsh configuration templates

---
+++ dot_config/zsh/conf.d/alias.zsh
@@ -0,0 +1,22 @@
+#################
+# Shell aliases #
+#################
+
+alias ls="eza"
+alias l1="ls -1"
+alias lt="ls --tree"
+alias ll="ls -l"
+alias la="ls -la"
+alias lat="ls -l -a --tree"
+alias lta="ls --tree -a"
+
+alias mkdir="mkdir -p"
+
+alias rm="rm -i"
+
+alias pip="python3 -m pip"
+
+alias vim=nvim
+
+
+# vim:ft=zsh
